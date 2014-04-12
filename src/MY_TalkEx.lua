db91ae307c47f3fdc0d0c775b501ff4141f5d367
tinymins <tinymins@gmail.com>
2014-04-12 17:20:25 +0800

    完善TalkEx模块

--- src/MY_TalkEx.lua
+++ src/MY_TalkEx.lua
@@ -22,6 +22,12 @@ MY_TalkEx.szTrickText = '$zj麻利的拔光了$mb的羊毛。'
 MY_TalkEx.szTrickTextEnd = '$zj收拾了一下背包里的羊毛，希望今年能卖个好价钱。'
 RegisterCustomData('MY_TalkEx.tTalkChannel')
 RegisterCustomData('MY_TalkEx.szTalk')
+RegisterCustomData('MY_TalkEx.tTrickChannel')
+RegisterCustomData('MY_TalkEx.tTrickFilter')
+RegisterCustomData('MY_TalkEx.tTrickFilterForce')
+RegisterCustomData('MY_TalkEx.szTrickTextBegin')
+RegisterCustomData('MY_TalkEx.szTrickText')
+RegisterCustomData('MY_TalkEx.szTrickTextEnd')
 
 _MY_TalkEx.tForce = { [-1] = _L['all force'] } for i=0,10,1 do _MY_TalkEx.tForce[i] = GetForceTitle(i) end
 _MY_TalkEx.tFilter = { ['NEARBY'] = _L['nearby players where'], ['RAID'] = _L['teammates where'], }
@@ -37,162 +43,152 @@ _MY_TalkEx.tChannels = {
     ['CAMP'] = { szName = _L['camp channel'], tCol = GetMsgFontColor("MSG_CAMP", true) },
     ['WORLD'] = { szName = _L['world channel'], tCol = GetMsgFontColor("MSG_WORLD", true) },
 }
-_MY_TalkEx.Init = function()
+_MY_TalkEx.OnUiLoad = function(wnd)
+    local ui = MY.UI(wnd)
     -------------------------------------
     -- 喊话部分
     -------------------------------------
     -- 喊话输入框
-    MY.UI.AddEdit('TalkEx','WndEdit_TalkEx_Talk',25,25,510,210,MY_TalkEx.szTalk,true)
-    MY.UI.RegisterEvent('WndEdit_TalkEx_Talk', {
-        OnEditChanged = function() 
-            MY_TalkEx.szTalk = this:GetText()
-        end
-    })
+    MY.UI.AddEdit('TalkEx','WndEdit_TalkEx_Talk',25,25,510,210,MY_TalkEx.szTalk,true):change(function() MY_TalkEx.szTalk = this:GetText() end)
     -- 喊话频道
     local i = 22
     for szChannel, tChannel in pairs(_MY_TalkEx.tChannels) do
-        MY.UI.AddCheckBox('TalkEx','WndCheckBox_TalkEx_'..szChannel,540,i,tChannel.szName,tChannel.tCol,MY_TalkEx.tTalkChannel[szChannel])
-        MY.UI.RegisterEvent('WndCheckBox_TalkEx_'..szChannel, {
-            OnCheckBoxCheck = function() 
-                MY_TalkEx.tTalkChannel[szChannel] = true
-            end,
-            OnCheckBoxUncheck = function() 
-                MY_TalkEx.tTalkChannel[szChannel] = false
-            end
-        })
+        MY.UI.AddCheckBox('TalkEx','WndCheckBox_TalkEx_'..szChannel,540,i,tChannel.szName,tChannel.tCol,MY_TalkEx.tTalkChannel[szChannel]):check(
+            function() MY_TalkEx.tTalkChannel[szChannel] = true end,
+            function() MY_TalkEx.tTalkChannel[szChannel] = false end
+        )
         i = i + 18
     end
     -- 喊话按钮
-    MY.UI.AddButton('TalkEx','WndButton_TalkEx_Talk',540,210,_L['send'],{255,255,255})
-    MY.UI.RegisterEvent('WndButton_TalkEx_Talk', {
-        OnLButtonClick = function() 
-            for szChannel, bSend in pairs(MY_TalkEx.tTalkChannel) do
-                if bSend then MY.Talk(PLAYER_TALK_CHANNEL[szChannel],MY_TalkEx.szTalk) end
-            end
+    MY.UI.AddButton('TalkEx','WndButton_TalkEx_Talk',540,210,_L['send'],{255,255,255}):click(function() 
+        if #MY_TalkEx.szTalk == 0 then MY.Sysmsg(_L["please input something."].."\n",nil,{255,0,0}) return end
+        for szChannel, bSend in pairs(MY_TalkEx.tTalkChannel) do
+            if bSend then MY.Talk(PLAYER_TALK_CHANNEL[szChannel],MY_TalkEx.szTalk) end
         end
-    })
+    end)
     -------------------------------------
     -- 调侃部分
     -------------------------------------
     -- 文本标题
-    MY.UI.Lookup("TalkEx","","Text_TalkEx_Trick_With"):SetText(_L['have a trick with'])
+    ui:find("#Text_TalkEx_Trick_With"):text(_L['have a trick with'])
     -- 调侃对象范围过滤器
-    MY.UI.Lookup("TalkEx","","Text_TalkEx_Trick_Filter"):SetText(_MY_TalkEx.tFilter[MY_TalkEx.tTrickFilter])
-    MY.UI.RegisterEvent("Button_TalkEx_Trick_Filter", { 
-        OnLButtonClick = function()
-            local item = MY.UI.Lookup("TalkEx","","Text_TalkEx_Trick_Filter")
-            PopupMenu((function() 
-                local t = {}
-                for szFilterId,szTitle in pairs(_MY_TalkEx.tFilter) do
-                    table.insert(t,{
-                        szOption = szTitle,
-                        fnAction = function()
-                            item:SetText(szTitle)
-                            MY_TalkEx.tTrickFilter = szFilterId
-                        end,
-                    })
-                end
-                return t
-            end)())
-        end
-    })
+    ui:find("#Text_TalkEx_Trick_Filter"):text(_MY_TalkEx.tFilter[MY_TalkEx.tTrickFilter])
+    ui:find("#Button_TalkEx_Trick_Filter"):click(function()
+        PopupMenu((function() 
+            local t = {}
+            for szFilterId,szTitle in pairs(_MY_TalkEx.tFilter) do
+                table.insert(t,{
+                    szOption = szTitle,
+                    fnAction = function()
+                        ui:find("#Text_TalkEx_Trick_Filter"):text(szTitle)
+                        MY_TalkEx.tTrickFilter = szFilterId
+                    end,
+                })
+            end
+            return t
+        end)())
+    end)
     -- 调侃门派过滤器
-    MY.UI.Lookup("TalkEx","","Text_TalkEx_Trick_Force"):SetText(_MY_TalkEx.tForce[MY_TalkEx.tTrickFilterForce])
-    MY.UI.RegisterEvent("Button_TalkEx_Trick_Force", { 
-        OnLButtonClick = function()
-            local item = MY.UI.Lookup("TalkEx","","Text_TalkEx_Trick_Force")
-            PopupMenu((function() 
-                local t = {}
-                for szFilterId,szTitle in pairs(_MY_TalkEx.tForce) do
-                    table.insert(t,{
-                        szOption = szTitle,
-                        fnAction = function()
-                            item:SetText(szTitle)
-                            MY_TalkEx.tTrickFilterForce = szFilterId
-                        end,
-                    })
-                end
-                return t
-            end)())
-        end
-    })
+    ui:find("#Text_TalkEx_Trick_Force"):text(_MY_TalkEx.tForce[MY_TalkEx.tTrickFilterForce])
+    ui:find("#Button_TalkEx_Trick_Force"):click(function()
+        PopupMenu((function() 
+            local t = {}
+            for szFilterId,szTitle in pairs(_MY_TalkEx.tForce) do
+                table.insert(t,{
+                    szOption = szTitle,
+                    fnAction = function()
+                        ui:find("#Text_TalkEx_Trick_Force"):text(szTitle)
+                        MY_TalkEx.tTrickFilterForce = szFilterId
+                    end,
+                })
+            end
+            return t
+        end)())
+    end)
     -- 调侃内容输入框：第一句
-    MY.UI.AddEdit('TalkEx','WndEdit_TalkEx_TrickBegin',25,285,510,25,MY_TalkEx.szTrickTextBegin,true)
-    MY.UI.RegisterEvent('WndEdit_TalkEx_TrickBegin', {
-        OnLButtonClick = function() 
-            for szChannel, bSend in pairs(MY_TalkEx.tTalkChannel) do
-                if bSend then MY.Talk(PLAYER_TALK_CHANNEL[szChannel],MY_TalkEx.szTrickTextBegin) end
-            end
-        end
-    })
+    MY.UI.AddEdit('TalkEx','WndEdit_TalkEx_TrickBegin',25,285,510,25,MY_TalkEx.szTrickTextBegin,true):change(function() MY_TalkEx.szTrickTextBegin = this:GetText() end)
     -- 调侃内容输入框：调侃内容
-    MY.UI.AddEdit('TalkEx','WndEdit_TalkEx_Trick',25,310,510,75,MY_TalkEx.szTrickText,true)
-    MY.UI.RegisterEvent('WndEdit_TalkEx_Trick', {
-        OnLButtonClick = function() 
-            for szChannel, bSend in pairs(MY_TalkEx.tTalkChannel) do
-                if bSend then MY.Talk(PLAYER_TALK_CHANNEL[szChannel],MY_TalkEx.szTrickText) end
-            end
-        end
-    })
+    MY.UI.AddEdit('TalkEx','WndEdit_TalkEx_Trick',25,310,510,75,MY_TalkEx.szTrickText,true):change(function() MY_TalkEx.szTrickText = this:GetText() end)
     -- 调侃内容输入框：最后一句
-    MY.UI.AddEdit('TalkEx','WndEdit_TalkEx_TrickEnd',25,385,510,25,MY_TalkEx.szTrickTextEnd,true)
-    MY.UI.RegisterEvent('WndEdit_TalkEx_TrickEnd', {
-        OnLButtonClick = function() 
-            for szChannel, bSend in pairs(MY_TalkEx.tTalkChannel) do
-                if bSend then MY.Talk(PLAYER_TALK_CHANNEL[szChannel],MY_TalkEx.szTrickTextEnd) end
-            end
-        end
-    })
+    MY.UI.AddEdit('TalkEx','WndEdit_TalkEx_TrickEnd',25,385,510,25,MY_TalkEx.szTrickTextEnd,true):change(function() MY_TalkEx.szTrickTextEnd = this:GetText() end)
     -- 调侃发送频道提示框
-    MY.UI.Lookup("TalkEx","","Text_TalkEx_Trick_Sendto"):SetText(_L['send to'])
+    ui:find("#Text_TalkEx_Trick_Sendto"):text(_L['send to'])
     -- 调侃发送频道
-    MY.UI.Lookup("TalkEx","","Text_TalkEx_Trick_Sendto_Filter"):SetText(_MY_TalkEx.tChannels[MY_TalkEx.tTrickChannel].szName)
-    MY.UI.Lookup("TalkEx","","Text_TalkEx_Trick_Sendto_Filter"):SetFontColor(unpack(_MY_TalkEx.tChannels[MY_TalkEx.tTrickChannel].tCol))
-    MY.UI.RegisterEvent("Button_TalkEx_Trick_Sendto_Filter", { 
-        OnLButtonClick = function()
-            local item = MY.UI.Lookup("TalkEx","","Text_TalkEx_Trick_Sendto_Filter")
-            PopupMenu((function() 
-                local t = {}
-                for szChannel,tChannel in pairs(_MY_TalkEx.tChannels) do
-                    table.insert(t,{
-                        szOption = tChannel.szName,
-                        fnAction = function()
-                            item:SetText(tChannel.szName)
-                            item:SetFontColor(unpack(tChannel.tCol))
-                            MY_TalkEx.tTrickChannel = szChannel
-                        end,
-                        rgb = tChannel.tCol,
-                    })
-                end
-                return t
-            end)())
-        end
-    })
+    ui:find("#Text_TalkEx_Trick_Sendto_Filter"):text(_MY_TalkEx.tChannels[MY_TalkEx.tTrickChannel].szName)
+    ui:find("#Text_TalkEx_Trick_Sendto_Filter"):color(_MY_TalkEx.tChannels[MY_TalkEx.tTrickChannel].tCol)
+    ui:find("#Button_TalkEx_Trick_Sendto_Filter"):click(function()
+        PopupMenu((function() 
+            local t = {}
+            for szChannel,tChannel in pairs(_MY_TalkEx.tChannels) do
+                table.insert(t,{
+                    szOption = tChannel.szName,
+                    fnAction = function()
+                        ui:find("#Text_TalkEx_Trick_Sendto_Filter"):text(tChannel.szName):color(tChannel.tCol)
+                        MY_TalkEx.tTrickChannel = szChannel
+                    end,
+                    rgb = tChannel.tCol,
+                })
+            end
+            return t
+        end)())
+    end)
     -- 调侃按钮
-    MY.UI.AddButton('TalkEx','WndButton_TalkEx_Trick',435,415,_L['have a trick with'],{255,255,255})
-    MY.UI.RegisterEvent("WndButton_TalkEx_Trick", { 
-        OnLButtonClick = function()
-            MY.Talk(PLAYER_TALK_CHANNEL[MY_TalkEx.tTrickChannel], MY_TalkEx.szTrickTextBegin)
-            local tPlayers = {}
-            if MY_TalkEx.tTrickFilter == 'RAID' then
-                for _, i in pairs(GetClientTeam().GetTeamMemberList()) do
-                    if GetPlayer(i) then table.insert(tPlayers, GetPlayer(i)) end
-                end
-            elseif MY_TalkEx.tTrickFilter == 'NEARBY' then
-                tPlayers = MY.GetNearPlayer()
+    MY.UI.AddButton('TalkEx','WndButton_TalkEx_Trick',435,415,_L['have a trick with'],{255,255,255}):click(function()
+        if #MY_TalkEx.szTrickText == 0 then MY.Sysmsg(_L["please input something."].."\n",nil,{255,0,0}) return end
+        if #MY_TalkEx.szTrickTextBegin > 0 then MY.Talk(PLAYER_TALK_CHANNEL[MY_TalkEx.tTrickChannel], MY_TalkEx.szTrickTextBegin) end
+        local tPlayers = {}
+        if MY_TalkEx.tTrickFilter == 'RAID' then
+            for _, i in pairs(GetClientTeam().GetTeamMemberList()) do
+                if GetPlayer(i) then table.insert(tPlayers, GetPlayer(i)) end
             end
-            -- 去掉自己 _(:з」∠)_调侃自己是闹哪样
-            tPlayers[GetClientPlayer().dwID]=nil
-            for _, player in pairs(tPlayers) do
-                if MY_TalkEx.tTrickFilterForce == -1 or MY_TalkEx.tTrickFilterForce == player.dwForceID then
-                    local szText = string.gsub(MY_TalkEx.szTrickText, "%$mb", '['..player.szName..']')
-                    MY.Talk(PLAYER_TALK_CHANNEL[MY_TalkEx.tTrickChannel], szText)
-                end
-            end
-            MY.Talk(PLAYER_TALK_CHANNEL[MY_TalkEx.tTrickChannel], MY_TalkEx.szTrickTextEnd)
+        elseif MY_TalkEx.tTrickFilter == 'NEARBY' then
+            tPlayers = MY.GetNearPlayer()
         end
-    })
+        -- 去掉自己 _(:з」∠)_调侃自己是闹哪样
+        tPlayers[GetClientPlayer().dwID]=nil
+        for _, player in pairs(tPlayers) do
+            if MY_TalkEx.tTrickFilterForce == -1 or MY_TalkEx.tTrickFilterForce == player.dwForceID then
+                local szText = string.gsub(MY_TalkEx.szTrickText, "%$mb", '['..player.szName..']')
+                MY.Talk(PLAYER_TALK_CHANNEL[MY_TalkEx.tTrickChannel], szText)
+            end
+        end
+        if #MY_TalkEx.szTrickTextEnd > 0 then MY.Talk(PLAYER_TALK_CHANNEL[MY_TalkEx.tTrickChannel], MY_TalkEx.szTrickTextEnd) end
+    end)
 end
-MY.RegisterPanel( "TalkEx", _L["talk ex"], "interface\\MY\\ui\\MY_TalkEx.ini", _MY_TalkEx.Init, "UI/Image/UICommon/ScienceTreeNode.UITex", 123, {255,255,0,200} )
-MY.RegisterEvent("CUSTOM_DATA_LOADED", _MY_TalkEx.Init)
--- MY.RegisterEvent("LOADING_END", _MY_TalkEx.Init)
+_MY_TalkEx.OnDataLoad = function(wnd)
+    local ui = MY.UI(wnd)
+    -------------------------------------
+    -- 喊话部分
+    -------------------------------------
+    -- 喊话输入框
+    ui:find("#WndEdit_TalkEx_Talk"):text(MY_TalkEx.szTalk)
+    -- 喊话频道
+    for szChannel, tChannel in pairs(_MY_TalkEx.tChannels) do
+        ui:find('#WndCheckBox_TalkEx_'..szChannel):check(MY_TalkEx.tTalkChannel[szChannel])
+    end
+    -- 喊话按钮
+    ui:find('#WndButton_TalkEx_Talk'):text(_L['send'])
+    -------------------------------------
+    -- 调侃部分
+    -------------------------------------
+    -- 文本标题
+    ui:find("#Text_TalkEx_Trick_With"):text(_L['have a trick with'])
+    -- 调侃对象范围过滤器
+    ui:find("#Text_TalkEx_Trick_Filter"):text(_MY_TalkEx.tFilter[MY_TalkEx.tTrickFilter])
+    -- 调侃门派过滤器
+    ui:find("#Text_TalkEx_Trick_Force"):text(_MY_TalkEx.tForce[MY_TalkEx.tTrickFilterForce])
+    -- 调侃内容输入框：第一句
+    ui:find('#WndEdit_TalkEx_TrickBegin'):text(MY_TalkEx.szTrickTextBegin)
+    -- 调侃内容输入框：调侃内容
+    ui:find('#WndEdit_TalkEx_Trick'):text(MY_TalkEx.szTrickText)
+    -- 调侃内容输入框：最后一句
+    ui:find('#WndEdit_TalkEx_TrickEnd'):text(MY_TalkEx.szTrickTextEnd)
+    -- 调侃发送频道提示框
+    ui:find("#Text_TalkEx_Trick_Sendto"):text(_L['send to'])
+    -- 调侃发送频道
+    ui:find("#Text_TalkEx_Trick_Sendto_Filter"):text(_MY_TalkEx.tChannels[MY_TalkEx.tTrickChannel].szName)
+    ui:find("#Text_TalkEx_Trick_Sendto_Filter"):color(_MY_TalkEx.tChannels[MY_TalkEx.tTrickChannel].tCol)
+    -- 调侃按钮
+    ui:find('#WndButton_TalkEx_Trick'):text(_L['have a trick with'])
+end
+MY.RegisterPanel("TalkEx", _L["talk ex"], "interface\\MY\\ui\\MY_TalkEx.ini", "UI/Image/UICommon/ScienceTreeNode.UITex", 123, {255,255,0,200}, _MY_TalkEx.OnUiLoad, _MY_TalkEx.OnDataLoad )
