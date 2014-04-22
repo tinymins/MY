1192e4b017f8794ef20f364ce3949d62dc2deafe
Emil Zhai <root@derzh.com>
2014-04-22 20:05:36 +0800

    更新模块新增消息推送和更新日志

--- src/MY_CheckUpdate.lua
+++ src/MY_CheckUpdate.lua
@@ -1,10 +1,13 @@
 local _L = MY.LoadLangPack()
 MY_CheckUpdate = {
     szUrl = "http://j3my.sinaapp.com/interface/my/latest_version.html",
+    szTipId = nil,
 }
+RegisterCustomData('MY_CheckUpdate.szTipId')
 MY_CheckUpdate.bChecked = false
 MY_CheckUpdate.GetValue = function(szText, szKey)
-    local nPos1, nPos2 = string.find(szText, '%|'..szKey..'=[^%|]*')
+    local escape = function(s) return string.gsub(s, '([%(%)%.%%%+%-%*%?%[%^%$%]])', '%%%1') end
+    local nPos1, nPos2 = string.find(szText, '%|'..escape(szKey)..'=[^%|]*')
     if not nPos1 then
         return nil
     else
@@ -33,17 +36,41 @@ MY.RegisterEvent("LOADING_END", function()
                 local szFile = MY_CheckUpdate.GetValue(szContent, 'file')
                 local szPage = MY_CheckUpdate.GetValue(szContent, 'page')
                 local szFeature = MY_CheckUpdate.GetValue(szContent, 'feature')
+                local szAlert = MY_CheckUpdate.GetValue(szContent, 'alert')
+                local szTip = MY_CheckUpdate.GetValue(szContent, 'tip')
+                local szTipId = MY_CheckUpdate.GetValue(szContent, 'tip-id')
+                local szTipRgb = MY_CheckUpdate.GetValue(szContent, 'tip-rgb')
                 MY.Sysmsg(_L["new version found."]..'\n', nil, { 255, 0, 0})
                 MY.Sysmsg(szFeature..'\n', nil, { 255, 0, 0})
-                local tVersionInfo = {
-                    szName = "MY_VersionInfo",
-                    szMessage = string.format("[%s] %s", _L["mingyi plugins"], _L["new version found, would you want to download immediately?"]), {
-                        szOption = g_tStrings.STR_HOTKEY_SURE, fnAction = function()
-                            MY.UI.OpenInternetExplorer(szPage, true)
-                        end
-                    },{szOption = g_tStrings.STR_HOTKEY_CANCEL,fnAction = function() end},
-                }
-                MessageBox(tVersionInfo)
+                if #szTipId>0 and szTipId~=MY_CheckUpdate.szTipId then
+                    local split = function(s, p)
+                        local rt= {}
+                        string.gsub(s, '[^'..p..']+', function(w) table.insert(rt, w) end )
+                        return rt
+                    end
+                    local _rgb, rgb = { 255, 0, 0 }, split( szTipRgb, ',' )
+                    rgb[1], rgb[2], rgb[3] = tonumber(rgb[1]), tonumber(rgb[2]), tonumber(rgb[3])
+                    if rgb[1] and rgb[1]>=0 and rgb[1]<=255 then _rgb[1] = rgb[1] end
+                    if rgb[2] and rgb[2]>=0 and rgb[2]<=255 then _rgb[2] = rgb[2] end
+                    if rgb[3] and rgb[3]>=0 and rgb[3]<=255 then _rgb[3] = rgb[3] end
+                    MY.Sysmsg(szTip..'\n', nil, _rgb)
+                    MY_CheckUpdate.szTipId = szTipId
+                end
+                if szAlert~='0' then
+                    local tVersionInfo = {
+                        szName = "MY_VersionInfo",
+                        szMessage = string.format("[%s] %s", _L["mingyi plugins"], _L["new version found, would you want to download immediately?"]..((#szFeature>0 and '\n--------------------\n') or '')..szFeature), {
+                            szOption = _L['download immediately'], fnAction = function()
+                                MY.UI.OpenInternetExplorer(szFile, true)
+                            end
+                        },{
+                            szOption = _L['see new feature'], fnAction = function()
+                                MY.UI.OpenInternetExplorer(szPage, true)
+                            end
+                        }, {szOption = g_tStrings.STR_HOTKEY_CANCEL,fnAction = function() end},
+                    }
+                    MessageBox(tVersionInfo)
+                end
             end
             MY.Debug("Latest version: "..nLatestVersion..", local version: "..nVersion..' ('..szVersion..")\n",'MYVC',0)
         else
