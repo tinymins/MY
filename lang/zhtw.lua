29eebeab72e340c2bce271377a24c1c4daf7bb76
tinymins <tinymins@gmail.com>
2014-05-07 23:03:25 +0800

    繁体语言�

---
+++ lang/zhtw.lua
@@ -0,0 +1,91 @@
+-- language data (zhcn) updated at 2014-4-10 03:38:11
+data = {
+    -- common
+    ['nearby channel'] = '近聊頻道',
+    ['friend channel'] = '好友頻道',
+    ['team channel'] = '隊伍頻道',
+    ['raid channel'] = '團隊/戰場',
+    ['tong channel'] = '幫會頻道',
+    ['tong alliance channel'] = '同盟頻道',
+    ['map channel'] = '地圖頻道',
+    ['school channel'] = '門派頻道',
+    ['camp channel'] = '陣營頻道',
+    ['whisper channel'] = '密聊頻道',
+    ['world channel'] = '世界頻道',
+    ['system channel'] = '系統頻道',
+    
+    ['NEARBY'] = '近聊',
+    ['FRIENDS'] = '好友',
+    ['TEAM'] = '隊伍',
+    ['RAID'] = '團隊/戰場',
+    ['TONG'] = '幫會',
+    ['TONG_ALLIANCE'] = '同盟',
+    ['MAP'] = '地圖',
+    ['SCHOOL'] = '門派',
+    ['CAMP'] = '陣營',
+    ['WHISPER'] = '密聊',
+    ['WORLD'] = '世界',
+    ['SYSTEM'] = '系統',
+    
+    ['send'] = '發送',
+    ['send to'] = '發送到',
+    
+    ['ji guan'] = '機關',
+    ['nu jian'] = '弩箭',
+    
+    ['all force'] = '所有人',
+    ['JiangHu'] = '江湖',
+    ['ChunYang'] = '純陽',
+    ['TianCe'] = '天策',
+    ['CangJian'] = '藏劍',
+    ['GaiBang'] = '丐幫',
+    ['WuDu'] = '五毒',
+    ['MingJiao'] = '明教',
+    ['WanHua'] = '萬花',
+    ['QiXiu'] = '七秀',
+    ['ShaoLin'] = '少林',
+    ['TangMen'] = '唐門',
+    
+    ['start'] = '開始',
+    ['stop'] = '停止',
+    ['clear'] = '清空',
+    
+    ['copy'] = '複製',
+    ['paste'] = '粘貼',
+    ['whisper'] = '密聊',
+    -- MY.lua
+    ['mingyi plugin'] = '茗伊插件',
+    ['mingyi plugins'] = '茗伊插件集',
+    ['%s, welcome to use mingyi plugins!'] = '歡迎%s使用茗伊插件集！',
+    ["unable to open ini file [%s]"] = "無法打開界面 INI 文件 [%s]",
+    ["can not find wnd component [%s]"] = "找不到窗體組件 [%s]",
+    ["unable to append handle item [%s]"] = "無法追加容器組件 [%s]",
+    ["unable to append handle item from string."] = "從字符串追加容器組件失敗。",
+    -- MY_TalkEx.lua
+    ['talk ex'] = '喊話輔助',
+    ['have a trick with'] = '調侃一下',
+    ['teammates where'] = '團隊中',
+    ['nearby players where'] = '附近的',
+    ['please input something.'] = '請先輸入一些內容。',
+    ['no trick target found.'] = '找不到可以調侃的玩家，您可以嘗試更改篩選條件。',
+    -- MY_CheckUpdate.lua
+    ['new version found.'] = '發現新版本！',
+    ['new version found, would you want to download immediately?'] = '發現新版本！是否立即前往下載？',
+    ['download immediately'] = '立即下載',
+    ['see new feature'] = '更新日誌',
+    -- MY_ChatMonitor.lua
+    ['chat monitor'] = '聊天監控',
+    ['waiting...'] = '等待中…',
+    ['regexp'] = '正則',
+    ['regular expression'] = '正則表達式',
+    ['show message preview box'] = '新消息彈窗',
+    ['welcome to use mingyi chat monitor.'] = '歡迎使用茗伊聊天監控。',
+    ['key words:'] = '關鍵字：',
+    ['CHAT_MONITOR_KEYWORDS_SAMPLE'] = "10|十人,血戰天策|XZTC,!小鐵被吃了,!開宴黑鐵;大戰",
+    ['CHAT_MONITOR_TIP'] = '關鍵字過濾器\n說明：\n半角分號;分隔多個條件\n每個條件中用半角逗號,表示且\n每個條件中用半角分隔符|表示或\n每個條件中用半角感歎號!表示非\n\n例1：大明宮;DMG;血戰天策;XZTC\n-----------------\n如上例子代表匹配含有DMG或大明宮或XZTC或血戰天策的聊天記錄。\n\n例2：25,大明宮;10,血戰天策\n-----------------\n如上例子代表匹配同時含有"25"和"大明宮"或者同時含有"10"和"血戰天策"的聊天記錄。\n\n例3：\n10|十人,血戰天策|XZTC,!小鐵被吃了,!開宴黑鐵;大戰\n-----------------\n如上例子代表匹配同時含有"10"或"十人"，"XZTC"或"血戰天策"，並且不含有"小鐵被吃了"，也不含有"開宴黑鐵"的句子；或者含有"大戰"的聊天記錄。\n\n關於正則，不會正則表達式的小夥伴不要隨便打鉤哦~~會正則表達式的小夥伴(┘￣︶￣)┘└(￣︶￣└)[GiveMeFive!] \n-----------------\nPs：作者語言表達能力不佳，有什麼看不懂的，你拓麻來打我啊hhhhhhhhhhhhhhhhhhhhhhh…',
+    
+    -- MY_RescueTeam.lua
+    ['rescue team helper'] = '救場必備',
+    -- MY_RollMon.lua
+    ['roll monitor'] = '點數監控',
+}
