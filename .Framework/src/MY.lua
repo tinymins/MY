da35a5650258894b959adb7f370d432f552682e4
Zhai Yiming <tinymins@gmail.com>
2014-07-23 23:14:31 +0800

    更新版本号

--- .Framework/src/MY.lua
+++ .Framework/src/MY.lua
@@ -126,8 +126,8 @@ MY = { }
 #######################################################################################################
 ]]
 local _DEBUG_ = 4
-local _BUILD_ = "20140712"
-local _VERSION_ = 0x1000002
+local _BUILD_ = "20140723"
+local _VERSION_ = 0x1000100
 local _ADDON_ROOT_ = '\\Interface\\MY\\'
 local _FRAMEWORK_ROOT_ = '\\Interface\\MY\\.Framework\\'
 
