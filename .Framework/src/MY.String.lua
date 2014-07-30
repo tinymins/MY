c2bea40fbe0f3fa817e48b5ea010a86b3911a5e1
Zhai Yiming <tinymins@gmail.com>
2014-07-30 23:48:04 +0800

    加入MY.String.UrlEncode

--- .Framework/src/MY.String.lua
+++ .Framework/src/MY.String.lua
@@ -14,3 +14,12 @@ MY.String.Split = function(s, p)
     return rt
 end
 MY.String.PatternEscape = function(s) return (string.gsub(s, '([%(%)%.%%%+%-%*%?%[%^%$%]])', '%%%1')) end
+MY.String.UrlEncode = function(w)
+    pattern="[^%w%d%._%-%* ]"
+    local s=string.gsub(w,pattern,function(c)
+        local c=string.format("%%%02X",string.byte(c))
+        return c
+    end)
+    s=string.gsub(s," ","+")
+    return s
+end
