f893c5957398f3f5ba98b3578f5138381c48992f
Zhai YiMing <tinymins@gmail.com>
2014-10-09 11:38:45 +0800

    加入MY.String.UrlDecode()解码URL

--- .Framework/src/MY.String.lua
+++ .Framework/src/MY.String.lua
@@ -8,24 +8,46 @@
 -----------------------------------------------
 MY = MY or {}
 MY.String = MY.String or {}
+
+-- 分隔字符串
+-- (table) MY.String.Split(string szText, string szSpliter)
 MY.String.Split = function(s, p)
     local rt= {}
     string.gsub(s, '[^'..p..']+', function(w) table.insert(rt, w) end )
     return rt
 end
+
+-- 转义正则表达式特殊字符
+-- (string) MY.String.PatternEscape(string szText)
 MY.String.PatternEscape = function(s) return (string.gsub(s, '([%(%)%.%%%+%-%*%?%[%^%$%]])', '%%%1')) end
-MY.String.UrlEncode = function(w)
-    pattern="[^%w%d%._%-%* ]"
-    local s=string.gsub(w,pattern,function(c)
-        local c=string.format("%%%02X",string.byte(c))
-        return c
-    end)
-    s=string.gsub(s," ","+")
-    return s
+
+-- 清除字符串首尾的空白字符
+-- (string) MY.String.Trim(string szText)
+MY.String.Trim = function(szText)
+    if not szText or szText == "" then
+        return ""
+    end
+    return (string.gsub(szText, "^%s*(.-)%s*$", "%1"))
 end
+
+-- 转换为 URL 编码
+-- (string) MY.String.UrlEncode(string szText)
+MY.String.UrlEncode = function(szText)
+    local str = szText:gsub("([^0-9a-zA-Z ])", function (c) return string.format ("%%%02X", string.byte(c)) end)
+    str = str:gsub(" ", "+")
+    return str
+end
+
+-- 解析 URL 编码
+-- (string) MY.String.UrlDecode(string szText)
+MY.String.UrlDecode = function(szText)
+    return szText:gsub("+", " "):gsub("%%(%x%x)", function(h) return string.char(tonumber(h, 16)) end)
+end
+
 MY.String.LenW = function(str)
     return wstring.len(str)
 end
+
 MY.String.SubW = function(str,s,e)
     if s < 0 then
         s = wstring.len(str) + s
