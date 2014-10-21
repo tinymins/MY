2149cfef432394d00ab5553baaa16e456e5ea377
Zhai YiMing <tinymins@gmail.com>
2014-10-21 16:37:32 +0800

    完善MY.Chat.RenderLink()接口注释

--- .Framework/src/MY.Chat.lua
+++ .Framework/src/MY.Chat.lua
@@ -204,8 +204,8 @@ MY.Chat.LinkEventHandler = {
     end,
 }
 --[[ 绑定link事件响应
-    (userdata) MY_Farbnamen.Render(userdata link)    处理link的各种事件绑定 namelink是一个超链接Text元素
-    (string) MY_Farbnamen.Render(string szMsg)       格式化szMsg 处理里面的超链接 添加时间相应
+    (userdata) MY.Chat.RenderLink(userdata link)    处理link的各种事件绑定 namelink是一个超链接Text元素
+    (string) MY.Chat.RenderLink(string szMsg)       格式化szMsg 处理里面的超链接 添加时间相应
 ]]
 MY.Chat.RenderLink = function(argv)
     if type(argv) == 'string' then
