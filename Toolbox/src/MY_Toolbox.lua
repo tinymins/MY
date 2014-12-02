dbeb4a23872befcd29d1c4cdb9dbe7dd72ff9105
Zhai YiMing <tinymins@gmail.com>
2014-12-02 11:02:41 +0800

    常用工具新增试炼之地九宫助手

--- Toolbox/src/MY_Toolbox.lua
+++ Toolbox/src/MY_Toolbox.lua
@@ -70,10 +70,68 @@ end
 MY_ToolBox.bAvoidBlackShenxingCD = true
 RegisterCustomData("MY_ToolBox.bAvoidBlackShenxingCD")
 MY_ToolBox.ApplyConfig = function()
+    -- 好友高亮
     if MY_ToolBox.bFriendHeadTip then
         _MY_ToolBox.FriendHeadTip(true)
     end
     
+    -- 试炼之地九宫助手
+    MY.RegisterEvent('OPEN_WINDOW', 'JiugongHelper', function(arg0, szText)
+        -- 确定当前对话对象是醉逍遥（18707）
+        if GetTargetHandle(GetClientPlayer().GetTarget()).dwTemplateID ~= 18707 then
+            return
+        end
+        -- 匹配字符串
+        string.gsub(szText, "<T1916><(T%d+)><T1926><(T%d+)><T1928><(T%d+)><T1924>.+<T1918><(T%d+)><T1931><(T%d+)><T1933><(T%d+)><T1935>.+<T1920><(T%d+)><T1937><(T%d+)><T1938><(T%d+)><T1939>", function(n1,n2,n3,n4,n5,n6,n7,n8,n9)
+            local tNumList = {
+                T1925 = 1, T1927 = 2, T1929 = 3,
+                T1930 = 4, T1932 = 5, T1934 = 6,
+                T1936 = 7, T1922 = 8, T1923 = 9,
+                T1940 = false,
+            }
+            local tDefaultSolution = {
+                {8,1,6,3,5,7,4,9,2},
+                {6,1,8,7,5,3,2,9,4},
+                {4,9,2,3,5,7,8,1,6},
+                {2,9,4,7,5,3,6,1,8},
+                {6,7,2,1,5,9,8,3,4},
+                {8,3,4,1,5,9,6,7,2},
+                {2,7,6,9,5,1,4,3,8},
+                {4,3,8,9,5,1,2,7,6},
+            }
+            
+            n1,n2,n3,n4,n5,n6,n7,n8,n9 = tNumList[n1],tNumList[n2],tNumList[n3],tNumList[n4],tNumList[n5],tNumList[n6],tNumList[n7],tNumList[n8],tNumList[n9]
+            local tQuestion = {n1,n2,n3,n4,n5,n6,n7,n8,n9}
+            local tSolution
+            for _, solution in ipairs(tDefaultSolution) do
+                local bNotMatch = false
+                for i, v in ipairs(solution) do
+                    if tQuestion[i] and tQuestion[i] ~= v then
+                        bNotMatch = true
+                        break
+                    end
+                end
+                if not bNotMatch then
+                    tSolution = solution
+                    break
+                end
+            end
+            local szText = _L['The kill sequence is: ']
+            if tSolution then
+                for i, v in ipairs(tQuestion) do
+                    if not tQuestion[i] then
+                        szText = szText .. NumberToChinese(tSolution[i]) .. ' '
+                    end
+                end
+            else
+                szText = szText .. _L['failed to calc.']
+            end
+            OutputWarningMessage("MSG_WARNING_RED", szText, 10)
+            MY.Sysmsg({szText})
+        end)
+    end)
+    
+    -- 防止神行CD被吃
     if MY_ToolBox.bAvoidBlackShenxingCD then
         MY.RegisterEvent('DO_SKILL_CAST', 'MY_ToolBox_AvoidBlackShenxingCD', function()
             local dwID, dwSkillID, dwSkillLevel = arg0, arg1, arg2
