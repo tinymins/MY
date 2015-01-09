0dc551d414094c5fea13805960fc7776dab6a0a3
Emil Zhai <root@derzh.com>
2015-01-09 10:37:59 +0800

    Release V20

--- .Framework/src/MY.lua
+++ .Framework/src/MY.lua
@@ -126,8 +126,8 @@ MY = { }
 #######################################################################################################
 ]]
 local _DEBUG_ = 4
-local _BUILD_ = "20150104"
-local _VERSION_ = 0x2001900
+local _BUILD_ = "20150109"
+local _VERSION_ = 0x2002000
 local _ADDON_ROOT_ = '\\Interface\\MY\\'
 local _FRAMEWORK_ROOT_ = '\\Interface\\MY\\.Framework\\'
 
