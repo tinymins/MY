142df2d1c33dd86e16d98174acf430440a6beddd
Emil Zhai <root@derzh.com>
2015-02-12 11:51:38 +0800

    聊天监控：新增过滤帮会上下线提示和帮会公告的选项

--- Chat/src/MY_ChatLog.lua
+++ Chat/src/MY_ChatLog.lua
@@ -10,10 +10,32 @@ local Log = {}
 local tinsert = table.insert
 local tremove = table.remove
 MY_ChatLog = MY_ChatLog or {}
+MY_ChatLog.bIgnoreTongOnlineMsg    = true -- 帮会上线通知
+MY_ChatLog.bIgnoreTongMemberLogMsg = true -- 帮会成员上线下线提示
+RegisterCustomData('MY_ChatLog.bIgnoreTongOnlineMsg')
+RegisterCustomData('MY_ChatLog.bIgnoreTongMemberLogMsg')
+
+_C.TongOnlineMsg = MY.String.PatternEscape(g_tStrings.STR_TALK_HEAD_TONG .. g_tStrings.STR_GUILD_ONLINE_MSG)
+_C.TongMemberLoginMsg = MY.String.PatternEscape(g_tStrings.STR_GUILD_MEMBER_LOGIN):gsub('<link 0>', '.-')
+_C.TongMemberLogoutMsg = MY.String.PatternEscape(g_tStrings.STR_GUILD_MEMBER_LOGOUT):gsub('<link 0>', '.-')
 
 function _C.OnMsg(szMsg, szChannel, nFont, bRich, r, g, b)
-	if not bRich then
+	local szText = szMsg
+	if bRich then
+		szText = GetPureText(szMsg)
+	else
 		szMsg = GetFormatText(szMsg, nil, r, g, b)
+	end
+	-- filters
+	if szChannel == "MSG_GUILD" then
+		if MY_ChatLog.bIgnoreTongOnlineMsg and szText:find(_C.TongOnlineMsg) then
+			return
+		end
+		if MY_ChatLog.bIgnoreTongMemberLogMsg and (
+			szText:find(_C.TongMemberLoginMsg) or szText:find(_C.TongMemberLogoutMsg)
+		) then
+			return
+		end
 	end
 	-- generate rec
 	szMsg = MY.Chat.GetTimeLinkText({r=r, g=g, b=b, f=nFont, s='[MM/dd|hh:mm:ss]'}) .. szMsg
@@ -112,7 +134,7 @@ function _C.OnPanelActive(wnd)
 	end
 	
 	ui:append('WndButton_MaxLog', 'WndButton'):children('#WndButton_MaxLog')
-	  :pos(x + 400, y - 3):width(120)
+	  :pos(w - 26 - 120, y - 3):width(120)
 	  :text(_L('Max log: %d', Log.nMax))
 	  :click(function()
 	  	local me = this
@@ -127,6 +149,32 @@ function _C.OnPanelActive(wnd)
 	  		function() end
 	  	)
 	  end)
+	
+	ui:append('Image_Setting','Image'):item('#Image_Setting')
+	  :pos(w - 26, y - 6):size(30, 30):alpha(200)
+	  :image('UI/Image/UICommon/Commonpanel.UITex',18)
+	  :hover(function(bIn) this:SetAlpha((bIn and 255) or 200) end)
+	  :click(function()
+	  	PopupMenu((function()
+	  		local t = {}
+	  		table.insert(t, {
+	  			szOption = _L['filter tong member log message'],
+	  			bCheck = true, bChecked = MY_ChatLog.bIgnoreTongMemberLogMsg,
+	  			fnAction = function()
+	  				MY_ChatLog.bIgnoreTongMemberLogMsg = not MY_ChatLog.bIgnoreTongMemberLogMsg
+	  			end,
+	  		})
+	  		table.insert(t, {
+	  			szOption = _L['filter tong online message'],
+	  			bCheck = true, bChecked = MY_ChatLog.bIgnoreTongOnlineMsg,
+	  			fnAction = function()
+	  				MY_ChatLog.bIgnoreTongOnlineMsg = not MY_ChatLog.bIgnoreTongOnlineMsg
+	  			end,
+	  		})
+	  		return t
+	  	end)())
+	end)
+
 end
 
 MY.RegisterPanel( "ChatLog", _L["chat log"], _L['Chat'], "ui/Image/button/SystemButton.UITex|43", {255,127,0,200}, {
