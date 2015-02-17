9a9a9255852be2f1a0557906fbdf0165ef5b0c42
Zhai YiMing <tinymins@gmail.com>
2015-02-17 12:45:59 +0800

    库：Text:size()后没有Format的问题

--- .Framework/src/MY.UI.lua
+++ .Framework/src/MY.UI.lua
@@ -4,7 +4,7 @@
 -- @Date  : 2014-11-24 08:40:30
 -- @Email : admin@derzh.com
 -- @Last Modified by:   µÔÒ»Ãù @tinymins
--- @Last Modified time: 2015-02-16 23:57:32
+-- @Last Modified time: 2015-02-17 12:45:19
 -----------------------------------------------
 MY = MY or {}
 local _MY = {
@@ -1903,6 +1903,7 @@ function _MY.UI:size(nWidth, nHeight)
 				ele.hdl:FormatAllItemPos()
 			elseif ele.type == "Text" then
 				ele.txt:SetSize(nWidth, nHeight)
+				ele.txt:GetParent():FormatAllItemPos()
 				ele.raw.bAutoSize = false
 			elseif ele.type == "WndListBox" then
 				ele.raw:Lookup('Scroll_Default'):SetRelPos(nWidth - 15, 10)
