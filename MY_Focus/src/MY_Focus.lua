b81efdbe7c67df11e6c432086c6e6a40179342c6
Zhai YiMing <tinymins@gmail.com>
2015-04-19 16:57:01 +0800

    焦点列表：不能屏蔽某些死掉的NPC的问题

--- MY_Focus/src/MY_Focus.lua
+++ MY_Focus/src/MY_Focus.lua
@@ -4,7 +4,7 @@
 -- @Date  : 2014-07-30 19:22:10
 -- @Email : admin@derzh.com
 -- @Last Modified by:   µÔÒ»Ãù @tinymins
--- @Last Modified time: 2015-03-26 16:11:28
+-- @Last Modified time: 2015-04-19 16:53:25
 --------------------------------------------
 local _L = MY.LoadLangPack(MY.GetAddonInfo().szRoot.."MY_Focus/lang/")
 local _C = {}
@@ -618,11 +618,12 @@ end
 MY_Focus.OnFrameBreathe = function()
 	if MY_Focus.bHideDeath then
 		for _, p in ipairs(MY_Focus.GetFocusList()) do
-			if p.dwType == TARGET.NPC or p.dwType == TARGET.PLAYER then
-				local tar = MY.GetObject(p.dwType, p.dwID)
-				if not tar or tar.nMoveState == MOVE_STATE.ON_DEATH then
-					MY_Focus.DelFocus(p.dwType, p.dwID)
-				end
+			local tar = MY.GetObject(p.dwType, p.dwID)
+			if not tar or (
+				((p.dwType == TARGET.NPC or p.dwType == TARGET.PLAYER) and tar.nMoveState == MOVE_STATE.ON_DEATH)
+				or (p.dwType == TARGET.DOODAD and tar.nKind == DOODAD_KIND.CORPSE)
+			) then
+				MY_Focus.DelFocus(p.dwType, p.dwID)
 			end
 		end
 	end
