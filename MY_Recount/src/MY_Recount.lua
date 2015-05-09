6532edd78832100fd36bf738c81da61e1ca3d8dd
Zhai YiMing <tinymins@gmail.com>
2015-05-09 22:15:12 +0800

    战斗统计：微调配色。。。

--- MY_Recount/src/MY_Recount.lua
+++ MY_Recount/src/MY_Recount.lua
@@ -65,20 +65,36 @@ local _C = {
 				[-1                  ] = { r = 255, g = 255, b = 255, a = 150 }, -- NPC
 				[FORCE_TYPE.JIANG_HU ] = { r = 255, g = 255, b = 255, a = 255 }, -- 江湖
 				[FORCE_TYPE.SHAO_LIN ] = { r = 210, g = 180, b = 0  , a = 144 }, -- 少林
-				[FORCE_TYPE.WAN_HUA  ] = { r = 62 , g = 22 , b = 155, a = 144 }, -- 万花
-				[FORCE_TYPE.TIAN_CE  ] = { r = 160, g = 0  , b = 0  , a = 180 }, -- 天策
+				[FORCE_TYPE.WAN_HUA  ] = { r = 127, g = 31 , b = 223, a = 180 }, -- 万花
+				[FORCE_TYPE.TIAN_CE  ] = { r = 160, g = 0  , b = 0  , a = 200 }, -- 天策
 				[FORCE_TYPE.CHUN_YANG] = { r = 0  , g = 146, b = 235, a = 180 }, -- 纯阳
-				[FORCE_TYPE.QI_XIU   ] = { r = 239, g = 61 , b = 171, a = 144 }, -- 七秀
-				[FORCE_TYPE.WU_DU    ] = { r = 132, g = 23 , b = 152, a = 144 }, -- 五毒
-				[FORCE_TYPE.TANG_MEN ] = { r = 0  , g = 46 , b = 188, a = 180 }, -- 唐门
+				[FORCE_TYPE.QI_XIU   ] = { r = 255, g = 127, b = 255, a = 128 }, -- 七秀
+				[FORCE_TYPE.WU_DU    ] = { r = 63 , g = 31 , b = 159, a = 128 }, -- 五毒
+				[FORCE_TYPE.TANG_MEN ] = { r = 0  , g = 46 , b = 188, a = 144 }, -- 唐门
 				[FORCE_TYPE.CANG_JIAN] = { r = 255, g = 255, b = 0  , a = 144 }, -- 藏剑
 				[FORCE_TYPE.GAI_BANG ] = { r = 205, g = 133, b = 63 , a = 180 }, -- 丐帮
-				[FORCE_TYPE.MING_JIAO] = { r = 253, g = 84 , b = 0  , a = 180 }, -- 明教
+				[FORCE_TYPE.MING_JIAO] = { r = 253, g = 84 , b = 0  , a = 144 }, -- 明教
 				[FORCE_TYPE.CANG_YUN ] = { r = 180, g = 60 , b = 0  , a = 255 }, -- 苍云
 			},
 		}, {
 			['Bar'] = {
-				[-1] = { image = "ui/Image/Common/Money.UITex", frame = 215 }, -- NPC
+				[-1                  ] = { r = 255, g = 255, b = 255, a = 150 }, -- NPC
+				[FORCE_TYPE.JIANG_HU ] = { r = 255, g = 255, b = 255, a = 255 }, -- 江湖
+				[FORCE_TYPE.SHAO_LIN ] = { r = 210, g = 180, b = 0  , a = 144 }, -- 少林
+				[FORCE_TYPE.WAN_HUA  ] = { r = 100, g = 0  , b = 150, a = 96  }, -- 万花
+				[FORCE_TYPE.TIAN_CE  ] = { r = 0  , g = 128, b = 0  , a = 255 }, -- 天策
+				[FORCE_TYPE.CHUN_YANG] = { r = 0  , g = 175, b = 230, a = 112 }, -- 纯阳
+				[FORCE_TYPE.QI_XIU   ] = { r = 240, g = 80 , b = 240, a = 96  }, -- 七秀
+				[FORCE_TYPE.WU_DU    ] = { r = 0  , g = 128, b = 255, a = 144 }, -- 五毒
+				[FORCE_TYPE.TANG_MEN ] = { r = 121, g = 183, b = 54 , a = 144 }, -- 唐门
+				[FORCE_TYPE.CANG_JIAN] = { r = 215, g = 241, b = 74 , a = 144 }, -- 藏剑
+				[FORCE_TYPE.GAI_BANG ] = { r = 205, g = 133, b = 63 , a = 180 }, -- 丐帮
+				[FORCE_TYPE.MING_JIAO] = { r = 240, g = 70 , b = 96 , a = 180 }, -- 明教
+				[FORCE_TYPE.CANG_YUN ] = { r = 180, g = 60 , b = 0  , a = 255 }, -- 苍云
+			},
+		}, {
+			['Bar'] = {
+				[-1                  ] = { image = "ui/Image/Common/Money.UITex", frame = 215 }, -- NPC
 				[FORCE_TYPE.JIANG_HU ] = { image = "ui/Image/Common/Money.UITex", frame = 210 }, -- 大侠
 				[FORCE_TYPE.SHAO_LIN ] = { image = "ui/Image/Common/Money.UITex", frame = 203 }, -- 少林
 				[FORCE_TYPE.WAN_HUA  ] = { image = "ui/Image/Common/Money.UITex", frame = 205 }, -- 万花
@@ -94,7 +110,7 @@ local _C = {
 			},
 		}, {
 			['Bar'] = {
-				[-1] = { image = "ui/Image/Common/Money.UITex", frame = 220 }, -- NPC
+				[-1                  ] = { image = "ui/Image/Common/Money.UITex", frame = 220 }, -- NPC
 				[FORCE_TYPE.JIANG_HU ] = { image = "ui/Image/Common/Money.UITex", frame = 220 }, -- 大侠
 				[FORCE_TYPE.SHAO_LIN ] = { image = "ui/Image/Common/Money.UITex", frame = 216 }, -- 少林
 				[FORCE_TYPE.WAN_HUA  ] = { image = "ui/Image/Common/Money.UITex", frame = 212 }, -- 万花
