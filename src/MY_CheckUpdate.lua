28e4d974fcaeb4d7616cab2b8b9711cea44d4ef1
Zhai YiMing <tinymins@gmail.com>
2015-07-29 12:07:25 +0800

    转换为网页

--- src/MY_CheckUpdate.lua
+++
@@ -1,4 +0,0 @@
-MY.RegisterEvent("LOADING_END", function() MY.RemoteRequest("https://raw.githubusercontent.com/tinymins/Jx3Interface/master/list.json", function(szTitle,szContent)
-    local szVersion, nVersion = MY.GetVersion()
-    Output(szTitle) Output(szContent) 
-end)end)
