af192450a0366fa71546ba45fc4dfcec6b8533b7
Zhai YiMing <tinymins@gmail.com>
2015-08-05 11:16:01 +0800

    简化字体设置 去掉大小和各种复选

--- MY_Font/src/MY_Font.lua
+++ MY_Font/src/MY_Font.lua
@@ -11,16 +11,9 @@ local C = {
 	tFontList = Font.GetFontPathList() or {},
 	aFontPath = {},
 	tFontType = {
-		{ dwID = 0, szName = _L['title'] },
-		{ dwID = 1, szName = _L['normal\ncontent'] },
-		{ dwID = 3, szName = _L['small\ncontent'] },
-		{ dwID = 2, szName = _L['large\ncontent'] },
-		{ dwID = 4, szName = _L['huge\ncontent'] },
-		{ dwID = 6, szName = _L['vertical\ncontent'] },
-		{ dwID = 5, szName = _L['chat'] },
-		{ dwID = 7, szName = _L['fight'] },
-		-- { dwID = 8, szName = _L['8'] },
-		-- { dwID = Font.GetChatFontID(), szName = _L['chat'] },
+		{ tIDs = {0, 1, 2, 3, 4, 6}, szName = _L['content'] },
+		{ tIDs = {Font.GetChatFontID()}, szName = _L['chat'] },
+		{ tIDs = {7}, szName = _L['fight'] },
 	},
 }
 local LUA_DATA_PATH = MY.GetAddonInfo().szRoot .. "MY_Font/font/$lang.jx3dat"
@@ -32,8 +25,8 @@ for _, p in ipairs(C.tFontList) do
 end
 local OBJ = {}
 
-function OBJ.SetFont(dwID, szName, szPath, nSize, tStyle)
-	-- dwID  : 要改变字体的类型（标题/文本/姓名 等）
+function OBJ.SetFont(tIDs, szName, szPath, nSize, tStyle)
+	-- tIDs  : 要改变字体的类型组（标题/文本/姓名 等）
 	-- szName: 字体名称
 	-- szPath: 字体路径
 	-- nSize : 字体大小
@@ -45,11 +38,14 @@ function OBJ.SetFont(dwID, szName, szPath, nSize, tStyle)
 	--     ["mipmap"  ] = (bool),
 	-- }
 	-- Ex: SetFont(Font.GetChatFontID(), "黑体", "\\UI\\Font\\方正黑体_GBK.ttf", 16, {["shadow"] = true})
-	Font.SetFont(dwID, szName, szPath, nSize, tStyle)
-	Station.SetUIScale(Station.GetUIScale(), true)
-	if dwID == Font.GetChatFontID() then
-		Wnd.OpenWindow("ChatSettingPanel")
-		OutputWarningMessage("MSG_REWARD_GREEN", _L['please click apply or sure button to save change!'], 10)
+	for _, dwID in ipairs(tIDs) do
+		local szName1, szFile1, nSize1, tStyle1 = Font.GetFont(dwID)
+		Font.SetFont(dwID, szName or szName1, szPath or szPath1, nSize or nSize1, tStyle or tStyle1)
+		Station.SetUIScale(Station.GetUIScale(), true)
+		if dwID == Font.GetChatFontID() then
+			Wnd.OpenWindow("ChatSettingPanel")
+			OutputWarningMessage("MSG_REWARD_GREEN", _L['please click apply or sure button to save change!'], 10)
+		end
 	end
 end
 
@@ -58,34 +54,24 @@ MY.RegisterPanel(
 "ui/Image/UICommon/CommonPanel7.UITex|36", {255,127,0,200}, {
 OnPanelActive = function(wnd)
 	local ui = MY.UI(wnd)
-	local x, y = 10, 5
+	local x, y = 10, 30
 	local w, h = ui:size()
 	
 	for _, p in ipairs(C.tFontType) do
-		local szName, szFile, nSize, tStyle = Font.GetFont(p.dwID)
+		local szName, szFile, nSize, tStyle = Font.GetFont(p.tIDs[1])
 		if tStyle then
 			-- local ui = ui:append("WndWindow", { w = w, h = 60 }, true)
-			local autocomplete, editname, editsize, chkshadow, chkvertical, chkborder, chkmono, chkmipmap, btn
+			local autocomplete, editname, btn
 			local function UpdateBtnEnable()
-				local nSize1 = tonumber(editsize:text())
-				btn:enable(IsFileExist(autocomplete:text()) and not not nSize1 and nSize1 > 0 and (
-					autocomplete:text() ~= szFile or
-					editname:text() ~= szName or
-					nSize1 ~= nSize or
-					not chkshadow:check()   ~= not tStyle.shadow   or
-					not chkvertical:check() ~= not tStyle.vertical or
-					not chkborder:check()   ~= not tStyle.border   or
-					not chkmono:check()     ~= not tStyle.mono     or
-					not chkmipmap:check()   ~= not tStyle.mipmap
-				))
+				btn:enable(IsFileExist(autocomplete:text()) and autocomplete:text() ~= szFile)
 			end
-			x, y = 10, y + 10
-			ui:append("Text", { text = p.szName, x = x, y = y, multiline = true })
-			x = x + 40
+			x = 10
+			ui:append("Text", { text = _L[" * "] .. p.szName, x = x, y = y })
+			x = 10
+			y = y + 40
 			-- line 1
-			y = y - 10
 			autocomplete = ui:append("WndAutoComplete", {
-				x = x, y = y, w = w - 200,
+				x = x, y = y, w = w - 180,
 				text = szFile,
 				onchange = function(szText)
 					UpdateBtnEnable()
@@ -106,34 +92,18 @@ OnPanelActive = function(wnd)
 			  	end,
 				autocomplete = {{"option", "source", C.aFontPath}},
 			}, true)
-			x = w - 200 + x
-			editname  = ui:append("WndEditBox" , { w = 100, h = 25, x = x, y = y, text = szName, onchange = function() UpdateBtnEnable() end }, true)
+			x = w - 180 + x
+			editname  = ui:append("WndEditBox" , { w = 100, h = 25, x = x, y = y, text = szName, onchange = function() UpdateBtnEnable() end, enable = false }, true)
 			x = x + 100
-			editsize  = ui:append("WndEditBox" , { w = 50, h = 25, x = x, y = y, text = nSize, onchange = function() UpdateBtnEnable() end }, true)
 			-- line 2
 			x = 50
-			y = y + 25
-			chkshadow   = ui:append("WndCheckBox", { w = 100, h = 25, x = x, y = y, text = _L['shadow'], checked = tStyle.shadow, oncheck = function() UpdateBtnEnable() end }, true)
-			x = x + 90
-			chkvertical = ui:append("WndCheckBox", { w = 100, h = 25, x = x, y = y, text = _L['vertical'], checked = tStyle.vertical, oncheck = function() UpdateBtnEnable() end }, true)
-			x = x + 90
-			chkborder   = ui:append("WndCheckBox", { w = 100, h = 25, x = x, y = y, text = _L['border'], checked = tStyle.border, oncheck = function() UpdateBtnEnable() end }, true)
-			x = x + 90
-			chkmono     = ui:append("WndCheckBox", { w = 100, h = 25, x = x, y = y, text = _L['mono'  ], checked = tStyle.mono  , oncheck = function() UpdateBtnEnable() end }, true)
-			x = x + 90
-			chkmipmap   = ui:append("WndCheckBox", { w = 100, h = 25, x = x, y = y, text = _L['mipmap'], checked = tStyle.mipmap, oncheck = function() UpdateBtnEnable() end }, true)
 			x = w - 60
 			btn         = ui:append("WndButton"  , { w = 60, h = 25, x = x, y = y, text = _L['apply' ], enable = false, onclick = function()
-				MY_Font.SetFont(p.dwID, editname:text(), autocomplete:text(), tonumber(editsize:text()), {
-					shadow   = chkshadow:check(),
-					vertical = chkvertical:check(),
-					border   = chkborder:check(),
-					mono     = chkmono:check(),
-					mipmap   = chkmipmap:check(),
-				})
-				szName, szFile, nSize, tStyle = Font.GetFont(p.dwID)
+				MY_Font.SetFont(p.tIDs, editname:text(), autocomplete:text())
+				szName, szFile, nSize, tStyle = Font.GetFont(p.tIDs[1])
+				UpdateBtnEnable()
 			end }, true)
-			y = y + 30
+			y = y + 60
 		end
 	end
 end})
