da002c507db2ed40f8d64d407d052fd1ff6ba61b
Zhai YiMing <tinymins@gmail.com>
2016-02-02 18:04:03 +0800

    支持重复监控BUFF

--- MY_BuffMon/src/MY_BuffMon.lua
+++ MY_BuffMon/src/MY_BuffMon.lua
@@ -68,9 +68,15 @@ local function RedrawBuffList(hFrame, aBuffMon, OBJ)
 			hItem.mon = mon
 			hItem.dwIcon = mon[2]
 			if mon[4] then
-				hFrame.tItem[mon[3] .. mon[4]] = hItem
+				if not hFrame.tItem[mon[3] .. mon[4]] then
+					hFrame.tItem[mon[3] .. mon[4]] = {}
+				end
+				hFrame.tItem[mon[3] .. mon[4]][hItem] = true
 			else
-				hFrame.tItem[mon[3]] = hItem
+				if not hFrame.tItem[mon[3]] then
+					hFrame.tItem[mon[3]] = {}
+				end
+				hFrame.tItem[mon[3]][hItem] = true
 			end
 			hBox:SetObject(UI_OBJECT.BUFF, mon[2], 1, 1)
 			hBox:SetObjectIcon(hItem.dwIcon or -1)
@@ -153,81 +159,86 @@ local function UpdateBuffList(hFrame, KTarget, bTargetNotChanged, bHideOthers, b
 		for _, buff in ipairs(MY.Player.GetBuffList(KTarget)) do
 			if not bHideOthers or buff.dwSkillSrcID == UI_GetClientPlayerID() then
 				local szName = Table_GetBuffName(buff.dwID, buff.nLevel)
-				local hItem = hFrame.tItem[szName .. buff.dwID] or hFrame.tItem[szName]
-				if hItem then
-					if not (hItem.mon[4] and hItem.mon[4] ~= buff.dwID) then
-						if bHideVoidBuff and not hItem:IsVisible() then
-							_needFormatItemPos = true
-							hItem:Show()
-						end
-						local hBox = hItem.hBox
-						-- 计算BUFF时间
-						local nBuffTime = GetBuffTime(buff.dwID, buff.nLevel) / 16
-						local nTimeLeft = ("%.1f"):format(math.max(0, buff.nEndFrame - GetLogicFrameCount()) / 16)
-						if not _tBuffTime[KTarget.dwID] then
-							_tBuffTime[KTarget.dwID] = {}
-							hFrame.tBuffTime = _tBuffTime[KTarget.dwID] -- 防止CD过程中table被GC回收
-						end
-						nBuffTime = math.max(nBuffTime, nTimeLeft)
-						if _tBuffTime[KTarget.dwID][buff.dwID] then
-							nBuffTime = math.max(_tBuffTime[KTarget.dwID][buff.dwID], nBuffTime)
-						end
-						_tBuffTime[KTarget.dwID][buff.dwID] = nBuffTime
-						
-						if not hItem.dwIcon or hItem.dwIcon == 13 then
-							if hFrame.tItem[szName .. buff.dwID] == hFrame.tItem[szName] then
-								hFrame.tItem[szName] = nil
+				local tItems = hFrame.tItem[szName .. buff.dwID] or hFrame.tItem[szName]
+				if tItems then
+					for hItem, _ in pairs(tItems) do
+						if not (hItem.mon[4] and hItem.mon[4] ~= buff.dwID) then
+							if bHideVoidBuff and not hItem:IsVisible() then
+								_needFormatItemPos = true
+								hItem:Show()
 							end
-							hFrame.tItem[szName .. buff.dwID] = hItem
-							hItem.dwIcon = Table_GetBuffIconID(buff.dwID, buff.nLevel)
-							hBox:SetObjectIcon(hItem.dwIcon)
-							hItem.mon[2] = hItem.dwIcon
-							hItem.mon[4] = buff.dwID
-						end
-						
-						if hItem.hProcessTxt then
-							hItem.hProcessTxt:SetText(nTimeLeft .. "'")
-						end
-						hBox:SetOverText(1, nTimeLeft .. "'")
-						
-						if buff.nStackNum == 1 then
-							hBox:SetOverText(0, "")
-						else
-							hBox:SetOverText(0, buff.nStackNum)
-						end
+							local hBox = hItem.hBox
+							-- 计算BUFF时间
+							local nBuffTime = GetBuffTime(buff.dwID, buff.nLevel) / 16
+							local nTimeLeft = ("%.1f"):format(math.max(0, buff.nEndFrame - GetLogicFrameCount()) / 16)
+							if not _tBuffTime[KTarget.dwID] then
+								_tBuffTime[KTarget.dwID] = {}
+								hFrame.tBuffTime = _tBuffTime[KTarget.dwID] -- 防止CD过程中table被GC回收
+							end
+							nBuffTime = math.max(nBuffTime, nTimeLeft)
+							if _tBuffTime[KTarget.dwID][buff.dwID] then
+								nBuffTime = math.max(_tBuffTime[KTarget.dwID][buff.dwID], nBuffTime)
+							end
+							_tBuffTime[KTarget.dwID][buff.dwID] = nBuffTime
+							
+							if not hItem.dwIcon or hItem.dwIcon == 13 then
+								if hFrame.tItem[szName] then
+									hFrame.tItem[szName][hItem] = nil
+								end
+								if not hFrame.tItem[szName .. buff.dwID] then
+									hFrame.tItem[szName .. buff.dwID] = {}
+								end
+								hFrame.tItem[szName .. buff.dwID][hItem] = true
+								hItem.dwIcon = Table_GetBuffIconID(buff.dwID, buff.nLevel)
+								hBox:SetObjectIcon(hItem.dwIcon)
+								hItem.mon[2] = hItem.dwIcon
+								hItem.mon[4] = buff.dwID
+							end
+							
+							if hItem.hProcessTxt then
+								hItem.hProcessTxt:SetText(nTimeLeft .. "'")
+							end
+							hBox:SetOverText(1, nTimeLeft .. "'")
+							
+							if buff.nStackNum == 1 then
+								hBox:SetOverText(0, "")
+							else
+								hBox:SetOverText(0, buff.nStackNum)
+							end
 
-						local dwPercent = nTimeLeft / nBuffTime
-						if hItem.hProcessImg then
-							hItem.hProcessImg:SetPercentage(dwPercent)
-						end
-						hBox:SetCoolDownPercentage(dwPercent)
+							local dwPercent = nTimeLeft / nBuffTime
+							if hItem.hProcessImg then
+								hItem.hProcessImg:SetPercentage(dwPercent)
+							end
+							hBox:SetCoolDownPercentage(dwPercent)
 
-						if dwPercent < 0.5 and dwPercent > 0.3 then
-							if hBox.dwPercent ~= 0.5 then
-								hBox.dwPercent = 0.5
-								hBox:SetObjectStaring(true)
+							if dwPercent < 0.5 and dwPercent > 0.3 then
+								if hBox.dwPercent ~= 0.5 then
+									hBox.dwPercent = 0.5
+									hBox:SetObjectStaring(true)
+								end
+							elseif dwPercent < 0.3 and dwPercent > 0.1 then
+								if hBox.dwPercent ~= 0.3 then
+									hBox.dwPercent = 0.3
+									hBox:SetExtentAnimate("ui\\Image\\Common\\Box.UITex", 17)
+								end
+							elseif dwPercent < 0.1 then
+								if hBox.dwPercent ~= 0.1 then
+									hBox.dwPercent = 0.1
+									hBox:SetExtentAnimate("ui\\Image\\Common\\Box.UITex", 20)
+								end
+							else
+								hBox:SetObjectStaring(false)
+								hBox:ClearExtentAnimate()
 							end
-						elseif dwPercent < 0.3 and dwPercent > 0.1 then
-							if hBox.dwPercent ~= 0.3 then
-								hBox.dwPercent = 0.3
-								hBox:SetExtentAnimate("ui\\Image\\Common\\Box.UITex", 17)
-							end
-						elseif dwPercent < 0.1 then
-							if hBox.dwPercent ~= 0.1 then
-								hBox.dwPercent = 0.1
-								hBox:SetExtentAnimate("ui\\Image\\Common\\Box.UITex", 20)
-							end
-						else
-							hBox:SetObjectStaring(false)
-							hBox:ClearExtentAnimate()
+							hItem.nRenderFrame = nCurrentFrame
 						end
-						hItem.nRenderFrame = nCurrentFrame
-					end
-					if not hItem.mon[5] then
-						hItem.mon[5] = {}
-					end
-					if not hItem.mon[5][buff.dwID] then
-						hItem.mon[5][buff.dwID] = Table_GetBuffIconID(buff.dwID, buff.nLevel) or 13
+						if not hItem.mon[5] then
+							hItem.mon[5] = {}
+						end
+						if not hItem.mon[5][buff.dwID] then
+							hItem.mon[5][buff.dwID] = Table_GetBuffIconID(buff.dwID, buff.nLevel) or 13
+						end
 					end
 				end
 			end
