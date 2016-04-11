a36d59fec7de39ae5757ca934c0d8723049f8494
Emil Zhai <root@derzh.com>
2016-04-11 09:39:02 +0800

    Release V55

--- .Framework/src/MY.lua
+++ .Framework/src/MY.lua
@@ -117,8 +117,8 @@ MY = {}
 --------------------------------------------------------------------------------------------------------------------------------------------
 local _DEBUG_ = tonumber(LoadLUAData('interface/my.debug.level') or nil) or 4
 local _LOGLV_ = tonumber(LoadLUAData('interface/my.delog.level') or nil) or 4
-local _BUILD_ = "20160330"
-local _VERSION_ = 0x2005400
+local _BUILD_ = "20160408"
+local _VERSION_ = 0x2005500
 local _ADDON_ROOT_ = '/Interface/MY/'
 local _FRAMEWORK_ROOT_ = '/Interface/MY/.Framework/'
 
