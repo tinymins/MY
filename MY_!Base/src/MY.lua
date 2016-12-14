e6d289d09a494c0d519d5581e9fee6e8f7e0e6b7
一名宅。 <tinymins@gmail.com>
2016-12-14 15:40:06 +0800

    Release V64

--- MY_!Base/src/MY.lua
+++ MY_!Base/src/MY.lua
@@ -4,7 +4,7 @@
 -- @Date  : 2014-11-24 08:40:30
 -- @Email : admin@derzh.com
 -- @Last modified by:   Zhai Yiming
--- @Last modified time: 2016-12-13 15:32:43
+-- @Last modified time: 2016-12-14 15:14:02
 -- @Ref: 借鉴大量海鳗源码 @haimanchajian.com
 --------------------------------------------
 -- #################################################################################################################################### --
@@ -115,8 +115,8 @@ MY = {}
 --------------------------------------------------------------------------------------------------------------------------------------------
 -- 本地函数变量
 --------------------------------------------------------------------------------------------------------------------------------------------
-local _BUILD_ = "20161016"
-local _VERSION_ = 0x2006300
+local _BUILD_ = "20161214"
+local _VERSION_ = 0x2006400
 local _DEBUGLV_ = tonumber(LoadLUAData('interface/my.debug.level') or nil) or 4
 local _DELOGLV_ = tonumber(LoadLUAData('interface/my.delog.level') or nil) or 4
 local _NORESTM_ = tonumber(LoadLUAData('interface/my.nrtim.level') or nil) or -1
