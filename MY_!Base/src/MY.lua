e2524023a6d005159846799c6c9d06a108bc0cd3
Emil Zhai <root@derzh.com>
2017-02-08 20:09:42 +0800

    Release V70

--- MY_!Base/src/MY.lua
+++ MY_!Base/src/MY.lua
@@ -115,8 +115,8 @@ MY = {}
 --------------------------------------------------------------------------------------------------------------------------------------------
 -- 本地函数变量
 --------------------------------------------------------------------------------------------------------------------------------------------
-local _BUILD_ = "20170120"
-local _VERSION_ = 0x2006900
+local _BUILD_ = "20170208"
+local _VERSION_ = 0x2007000
 local _DEBUGLV_ = tonumber(LoadLUAData('interface/my.debug.level') or nil) or 4
 local _DELOGLV_ = tonumber(LoadLUAData('interface/my.delog.level') or nil) or 4
 local _NORESTM_ = tonumber(LoadLUAData('interface/my.nrtim.level') or nil) or -1
