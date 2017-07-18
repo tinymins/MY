9b5a3865d698ab405786de7ad0ec5f1abb9be98c
Emil Zhai <root@derzh.com>
2017-07-19 00:18:17 +0800

    新的自动打包脚本 自动判断文件修改

--- 7zip.py
+++ 7zip.py
@@ -1,19 +1,51 @@
 # -*- coding: GBK -*-
-import time, os
+import time, os, re
+
 # 读取MY.lua文件中的插件版本号
-szVersion = "0x0000000"
+str_version = "0x0000000"
 for line in open("MY_!Base/src/MY.lua"):
 	if line[6:15] == "_VERSION_":
-		szVersion = line[23:25]
+		str_version = line[23:25]
+
+# 读取Git中最大的版本号
+version_list = os.popen('git tag').read().strip().split("\n")
+max_version, git_tag = 0, ''
+for version in version_list:
+	if max_version < int(version[1:]):
+		git_tag = version
+		max_version = int(version[1:])
+
+# 判断是否忘记提升版本号
+if int(str_version) <= max_version:
+	print 'Error: current version(%s) is smaller than or equals with last git published version(%d)!' % (str_version, max_version)
+	exit()
+
+# 读取可能需要打包的文件夹列表
+whitelist = {}
+for line in open("7zippathlist.txt"):
+	whitelist[line.strip()] = True
+
+# 读取Git中最大的版本号 到最新版修改文件
+paths = {}
+if git_tag != '':
+	filelist = os.popen('git diff ' + git_tag + ' --name-status').read().strip().split("\n")
+	for file in filelist:
+		file = re.sub('["/].*$', '', re.sub('^[^\t]\t"*', '', file))
+		if file in whitelist:
+			paths[file] = True
 
 # 获取并格式化当前时间字符串
-szTime = time.strftime("%Y%m%d%H%M%S", time.localtime())
+str_time = time.strftime("%Y%m%d%H%M%S", time.localtime())
 
 # 拼接字符串开始压缩文件
-szFile = "!src-dist/releases/MY." + szTime + "v" + szVersion + ".7z"
+dst_file = "!src-dist/releases/MY." + str_time + "v" + str_version + ".7z"
 print "zippping..."
-os.system("7z a -t7z " + szFile + " -xr!manifest.dat -xr!manifest.key -xr!publisher.key -x@7zipignore.txt")
+cmd = "7z a -t7z " + dst_file + " -xr!manifest.dat -xr!manifest.key -xr!publisher.key -x@7zipignore.txt"
+for path in paths:
+	cmd = cmd + ' "' + path + '"'
+os.system(cmd)
+print "Based on git tag " + git_tag + "."
 print "File(s) compressing acomplete!"
-print "Url" + szFile
+print "Url: " + dst_file
 
 time.sleep(5)
