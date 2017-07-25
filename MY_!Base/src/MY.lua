9362ded8735e2b060ff085e1ff92b1b23ff0cb8d
一名宅。 <root@derzh.com>
2017-07-25 09:19:09 +0800

    Release V79

--- MY_!Base/src/MY.lua
+++ MY_!Base/src/MY.lua
@@ -115,8 +115,8 @@ MY = {}
 --------------------------------------------------------------------------------------------------------------------------------------------
 -- 本地函数变量
 --------------------------------------------------------------------------------------------------------------------------------------------
-local _BUILD_ = "20170721"
-local _VERSION_ = 0x2007800
+local _BUILD_ = "20170722"
+local _VERSION_ = 0x2007900
 local _DEBUGLV_ = tonumber(LoadLUAData('interface/my.debug.level') or nil) or 4
 local _DELOGLV_ = tonumber(LoadLUAData('interface/my.delog.level') or nil) or 4
 local _NORESTM_ = tonumber(LoadLUAData('interface/my.nrtim.level') or nil) or -1
