54a10b519ce8c48fa8c04fc9531a721c12659ed4
Zhai Yiming <root@derzh.com>
2017-11-11 18:28:51 +0800

    重构台服科举助手

--- MY_Toolbox/src/MY_ExamTip.lua
+++ MY_Toolbox/src/MY_ExamTip.lua
@@ -6,190 +6,175 @@
 -- @Last modified by:   Zhai Yiming
 -- @Last modified time: 2016-12-13 14:49:52
 -----------------------------------------------
-local _L = MY.LoadLangPack(MY.GetAddonInfo().szRoot.."MY_Toolbox/lang/")
-local _C = {
-	szQueryUrl = "https://jx3.derzh.com/api/exam?l=%s&q=%s",
-	szSubmitUrl = "https://jx3.derzh.com/api/exam",
-	tCached = {}, -- 玩家答题缓存
-	tAccept = {}, -- 从服务器获取到的数据缓存
-	szLastQu = "",
-}
-MY_ExamTip = {}
+local _L = MY.LoadLangPack(MY.GetAddonInfo().szRoot .. "MY_Toolbox/lang/")
+local QUERY_URL = "https://jx3.derzh.com/api/exam?l=%s&q=%s"
+local SUBMIT_URL = "https://jx3.derzh.com/api/exam"
+local l_tLocal -- 本地题库
+local l_tCached = {} -- 玩家答题缓存
+local l_tAccept = {} -- 从服务器获取到的数据缓存
+local l_szLastQueryQues -- 最后一次网络查询的题目（防止重查）
 
--- 获取题目和答案
-MY_ExamTip.QueryData = function(szQues)
-	if _C.szLastQu == szQues then
-		return nil
-	end
-	_C.szLastQu = szQues
+local function DisplayMessage(szText)
+	MY.Sysmsg({szText}, _L['exam tip'])
+end
 
-	MY_ExamTip.ShowResult(szQues, nil, _L["Querying, please wait..."])
-	if not _C.tLocalQaS then
-		_C.tLocalQaS = MY.LoadLUAData({"config/examtip.jx3dat", MY_DATA_PATH.GLOBAL}) or {}
+local function IsCurrentQuestion(szQues)
+	local frame = Station.Lookup('Normal/ExaminationPanel')
+	if not frame then
+		return
 	end
-	if _C.tLocalQaS[szQues] then
-		for _, szAnsw in ipairs(_C.tLocalQaS[szQues]) do
-			if MY_ExamTip.ShowResult(szQues, szAnsw, szAnsw) then
-				break
+	return frame:Lookup("", "Handle_ExamContents"):Lookup(0):GetText() == szQues
+end
+
+local function ResolveAnswer(szAnsw)
+	local frame = Station.Lookup('Normal/ExaminationPanel')
+	if not frame then
+		return
+	end
+	for i = 1, 4 do
+		frame:Lookup('Wnd_Type1/CheckBox_T1No' .. i, 'Text_T1No' .. i):SetFontColor(0, 0, 0)
+	end
+	for i = 1, 4 do
+		if frame:Lookup('Wnd_Type1/CheckBox_T1No' .. i, 'Text_T1No' .. i):GetText() == szAnsw then
+			frame:Lookup('Wnd_Type1/CheckBox_T1No' .. i, 'Text_T1No' .. i):SetFontColor(255, 255, 0)
+			frame:Lookup('Wnd_Type1/CheckBox_T1No' .. i):Check(true)
+			return true
+		end
+	end
+	return false
+end
+
+local function QueryData(szQues)
+	if l_szLastQueryQues == szQues then
+		return
+	end
+	l_szLastQueryQues = szQues
+	DisplayMessage(_L["Querying, please wait..."])
+
+	if not l_tLocal then
+		l_tLocal = MY.LoadLUAData({"config/examtip.jx3dat", MY_DATA_PATH.GLOBAL}) or {}
+	end
+	if l_tLocal[szQues] then
+		for _, szAnsw in ipairs(l_tLocal[szQues]) do
+			if ResolveAnswer(szAnsw) then
+				return DisplayMessage(_L["Local exam data matched."])
 			end
 		end
-	else
-		local _, _, szLang, _ = GetVersion()
-		MY.Ajax({
-			type = 'get',
-			url = string.format(_C.szQueryUrl, szLang, MY.UrlEncode(szQues)),
-			success = function(szContent, status)
-				local data = MY.Json.Decode(szContent)
-				if not data then
-					return nil
-				end
-				
-				local szTip = ''
-				for _, p in ipairs(data.result) do
-					szTip = szTip .. p.szQues .. '\n' .. p.szAnsw
-				end
-				
-				if #data.result == 0 then
-					szTip = _L["No result found. Here's from open search engine:"].."\n" .. szTip
-				else
-					for _, p in ipairs(data.result) do
-						if MY_ExamTip.ShowResult(data.question, p.szAnsw, szTip) then
-							_C.tAccept[data.question] = p.szAnsw
-							break
-						end
-					end
-				end
-			end, 
-			error = function(html, status, connected)
-				if not connected then
-					_C.szLastQu = ""
-				end
-				MY_ExamTip.ShowResult(_C.szLastQu, nil, _L['Loading failed.'])
-			end,
-			timeout = 10000,
-		})
 	end
-end
--- 提交玩家正确答案 -- 云数据来源
-MY_ExamTip.SubmitData = function()
+
 	local _, _, szLang, _ = GetVersion()
-	local nCommited, nAccepted, nUnsubmit = 0, 0, 0
-	for szQues, szAnsw in pairs(_C.tCached) do
-		if not _C.tAccept[szQues] then
-			nUnsubmit = nUnsubmit + 1
-			MY.Ajax({
-				type = 'post/json',
-				url = _C.szSubmitUrl,
-				data = {
-					lang = szLang, 
-					data = {[szQues] = szAnsw},
-				},
-				success = function(szContent, status)
-					local r = MY.Json.Decode(szContent)
-					if r then
-						nUnsubmit = nUnsubmit - 1
-						nCommited = nCommited + r.received
-						nAccepted = nAccepted + r.accepted
+	MY.Ajax({
+		type = 'get',
+		url = QUERY_URL:format(szLang, MY.UrlEncode(szQues)),
+		success = function(szContent, status)
+			local res = MY.JsonDecode(szContent)
+			if not res or not IsCurrentQuestion(res.ques) then
+				return
+			end
+
+			if #res.data == 0 then
+				if res.more then
+					DisplayMessage(_L["No result found, here's from open search engine:"] .. "\n" .. res.more)
+				else
+					DisplayMessage(_L["No result found."])
+				end
+			else
+				for _, rec in ipairs(res.data) do
+					if IsCurrentQuestion(rec.ques) and ResolveAnswer(rec.ques, rec.ans) then
+						l_tAccept[rec.ques] = rec.ans
+						return
 					end
-					if not MY.IsShieldedVersion() and nUnsubmit == 0 then
-						MY.Sysmsg({_L('%s record(s) commited, %s record(s) accepted!', nCommited, nAccepted)}, _L['exam tip'])
-					end
-				end,
-			})
+				end
+
+				local szText = _L['No result matched, here\'s similar answers:']
+				for _, rec in ipairs(res.data) do
+					szText = szText .. '\n' .. _L('Question: %s\nAnswer: %s', rec.ques, rec.ans)
+				end
+				DisplayMessage(szText)
+			end
+		end,
+		error = function(html, status, connected)
+			if not connected then
+				l_szLastQueryQues = ""
+			end
+			DisplayMessage(_L['Loading failed.'])
+		end,
+		timeout = 10000,
+	})
+end
+
+local function SubmitData()
+	local data = {}
+	for szQues, szAnsw in pairs(l_tCached) do
+		if not l_tAccept[szQues] then
+			table.insert(data, { ques = szQues, ans = szAnsw })
 		end
 	end
+	if #data == 0 then
+		return
+	end
+	MY.Ajax({
+		type = 'post/json',
+		url = SUBMIT_URL,
+		data = {
+			lang = select(3, GetVersion()),
+			data = data,
+		},
+		success = function(html, status)
+			local res = MY.JsonDecode(html)
+			if MY.IsShieldedVersion() or not res then
+				return
+			end
+			MY.Sysmsg({_L('%s record(s) commited, %s record(s) accepted!', r.received, r.accepted)}, _L['exam tip'])
+		end,
+	})
 end
--- 显示结果
-MY_ExamTip.ShowResult = function(szQues, szAnsw, szTip)
-	local hNext = Station.Lookup("Normal/ExaminationPanel/Btn_Next")
-	local hSbmt = Station.Lookup("Normal/ExaminationPanel/Btn_Submit")
-	local hQues = Station.Lookup("Normal/ExaminationPanel/","Handle_ExamContents"):Lookup(0)
-	local hTxt1 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No1",'Text_T1No1')
-	local hChk1 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No1")
-	local hTxt2 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No2",'Text_T1No2')
-	local hChk2 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No2")
-	local hTxt3 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No3",'Text_T1No3')
-	local hChk3 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No3")
-	local hTxt4 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No4",'Text_T1No4')
-	local hChk4 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No4")
-	
-	if hQues:GetText() ~= szQues then
-		return false
-	end
-	if szTip then
-		OutputMessage('MSG_SYS', szTip .. '\n')
-	end
-	hTxt1:SetFontColor(0, 0, 0)
-	hTxt2:SetFontColor(0, 0, 0)
-	hTxt3:SetFontColor(0, 0, 0)
-	hTxt4:SetFontColor(0, 0, 0)
-	
-	if hTxt1:GetText() == szAnsw then
-		hTxt1:SetFontColor(255, 255, 0)
-		hChk1:Check(true)
-	elseif hTxt2:GetText() == szAnsw then
-		hTxt2:SetFontColor(255, 255, 0)
-		hChk2:Check(true)
-	elseif hTxt3:GetText() == szAnsw then
-		hTxt3:SetFontColor(255, 255, 0)
-		hChk3:Check(true)
-	elseif hTxt4:GetText() == szAnsw then
-		hTxt4:SetFontColor(255, 255, 0)
-		hChk4:Check(true)
-	else
-		return false
-	end
-	return true
-end
--- 收集结果
-MY_ExamTip.CollectResult = function()
-	local hQues = Station.Lookup("Normal/ExaminationPanel/","Handle_ExamContents"):Lookup(0)
-	local hTxt1 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No1",'Text_T1No1')
-	local hChk1 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No1")
-	local hTxt2 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No2",'Text_T1No2')
-	local hChk2 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No2")
-	local hTxt3 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No3",'Text_T1No3')
-	local hChk3 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No3")
-	local hTxt4 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No4",'Text_T1No4')
-	local hChk4 = Station.Lookup("Normal/ExaminationPanel/Wnd_Type1/CheckBox_T1No4")
-	
-	local szQues = hQues:GetText()
-	local szAnsw
-	
-	if hChk1:IsCheckBoxChecked() then
-		szAnsw = hTxt1:GetText()
-	elseif hChk2:IsCheckBoxChecked() then
-		szAnsw = hTxt2:GetText()
-	elseif hChk3:IsCheckBoxChecked() then
-		szAnsw = hTxt3:GetText()
-	elseif hChk4:IsCheckBoxChecked() then
-		szAnsw = hTxt4:GetText()
-	end
-	
-	if szQues and szAnsw then
-		_C.tCached[szQues] = szAnsw
-	end
-end
--- 时钟监控
-_C.OnFrameBreathe = function()
+
+do
+local l_nExamPrintRemainSpace = 0
+local function OnFrameBreathe()
 	local frame = Station.Lookup("Normal/ExaminationPanel")
 	if not (frame and frame:IsVisible()) then
 		return
 	end
-	local hQues = frame:Lookup("", "Handle_ExamContents"):Lookup(0)
-	if not hQues then
+	local txtQues = frame:Lookup("", "Handle_ExamContents"):Lookup(0)
+	if not txtQues then
 		return
 	end
-	local szQues = hQues:GetText()
-	
+	local szQues = txtQues:GetText()
 	if not MY.IsShieldedVersion() then
-		MY_ExamTip.QueryData(szQues)
+		QueryData(szQues)
 	end
-	MY_ExamTip.CollectResult(szQues)
-	_C.nExamPrintRemainSpace = GetClientPlayer().GetExamPrintRemainSpace()
+	local szAnsw
+	for i = 1, 4 do
+		if frame:Lookup('Wnd_Type1/CheckBox_T1No' .. i):IsCheckBoxChecked() then
+			szAnsw = frame:Lookup('Wnd_Type1/CheckBox_T1No' .. i, 'Text_T1No' .. i):GetText()
+			break
+		end
+	end
+	if szQues and szAnsw then
+		l_tCached[szQues] = szAnsw
+	end
+	l_nExamPrintRemainSpace = GetClientPlayer().GetExamPrintRemainSpace()
 end
-MY.RegisterEvent('ON_FRAME_CREATE.EXAM_TIP', function()
+
+local function OnFrameCreate()
 	if arg0:GetName() == "ExaminationPanel" then
-		arg0.OnFrameBreathe = _C.OnFrameBreathe
+		arg0.OnFrameBreathe = OnFrameBreathe
 	end
-end)
+end
+MY.RegisterEvent('ON_FRAME_CREATE.EXAM_TIP', OnFrameCreate)
+
+local function OnLoot()
+	local item = GetItem(arg1)
+	if item and item.nUiId == 65814 then
+		local nBeforeExamPrintRemainSpace = l_nExamPrintRemainSpace
+		MY.DelayCall(function()
+			if nBeforeExamPrintRemainSpace - GetClientPlayer().GetExamPrintRemainSpace() == 100 then
+				SubmitData()
+			end
+		end, 2000)
+	end
+end
+MY.RegisterEvent("LOOT_ITEM.MY_EXAMTIP", OnLoot)
+end
