7c9c96460ed88852edad1acbd5e0ebb57888db51
Zhai Yiming <root@derzh.com>
2018-01-07 20:39:30 +0800

    在XGUI增加Handle原件池管理

--- MY_!Base/src/MY.UI.lua
+++ MY_!Base/src/MY.UI.lua
@@ -3226,13 +3226,85 @@ function XGUI:blur(fnOnKillFocus)
 	end
 end
 
--- OnGetFocus 获取焦点
+---------------------------------------------------------------------
+-- 可重复利用的简易 Handle 元件缓存池
+---------------------------------------------------------------------
+local HandlePool = {}
+HandlePool.__index = HandlePool
+-- construct
+function HandlePool:ctor(handle, xml)
+	local oo = {}
+	setmetatable(oo, self)
+	oo.handle, oo.xml = handle, xml
+	handle.nFreeCount = 0
+	handle:Clear()
+	return oo
+end
+
+-- clear
+function HandlePool:Clear()
+	self.handle:Clear()
+	self.handle.nFreeCount = 0
+end
+
+-- new item
+function HandlePool:New()
+	local handle = self.handle
+	local nCount = handle:GetItemCount()
+	if handle.nFreeCount > 0 then
+		for i = nCount - 1, 0, -1 do
+			local item = handle:Lookup(i)
+			if item.bFree then
+				item.bFree = false
+				handle.nFreeCount = handle.nFreeCount - 1
+				return item
+			end
+		end
+		handle.nFreeCount = 0
+	else
+		handle:AppendItemFromString(self.xml)
+		local item = handle:Lookup(nCount)
+		item.bFree = false
+		return item
+	end
+end
+
+-- remove item
+function HandlePool:Remove(item)
+	if item:IsValid() then
+		self.handle:RemoveItem(item)
+	end
+end
+
+-- free item
+function HandlePool:Free(item)
+	if item:IsValid() then
+		self.handle.nFreeCount = self.handle.nFreeCount + 1
+		item.bFree = true
+		item:SetName("")
+		item:Hide()
+	end
+end
+
+function HandlePool:GetAllItem(bShow)
+	local t = {}
+	for i = self.handle:GetItemCount() - 1, 0, -1 do
+		local item = self.handle:Lookup(i)
+		if bShow and item:IsVisible() or not bShow then
+			table.insert(t, item)
+		end
+	end
+	return t
+end
+-- public api, create pool
+-- (class) XGUI.HandlePool(userdata handle, string szXml)
+XGUI.HandlePool = setmetatable({}, { __call = function(me, ...) return HandlePool:ctor( ... ) end, __metatable = true, __newindex = function() end })
 
 -----------------------------------------------------------
--- XGUI
+-- 枚举
 -----------------------------------------------------------
 
-XGUI = XGUI or {}
+MY = MY or {}
 MY.Const = MY.Const or {}
 MY.Const.Event = MY.Const.Event or {}
 MY.Const.Event.Mouse = MY.Const.Event.Mouse or {}
