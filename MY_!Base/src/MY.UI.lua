52e8cb698416b0df1779e0762ac5a702b3eae37f
Emil Zhai <root@derzh.com>
2018-02-22 14:46:41 +0800

    XGUI支持初始化events

--- MY_!Base/src/MY.UI.lua
+++ MY_!Base/src/MY.UI.lua
@@ -116,6 +116,8 @@ local function ApplyUIArguments(ui, arg)
 		if arg.oncheck            ~= nil then ui:check          (arg.oncheck    ) end
 		if arg.onchange           ~= nil then ui:change         (arg.onchange   ) end
 		if arg.ondragging or arg.ondrag  then ui:drag(arg.ondragging, arg.ondrag) end
+		if arg.events             ~= nil then for _, v in ipairs(arg.events) do ui:event(unpack(v)) end end
+		if arg.uievents           ~= nil then for _, v in ipairs(arg.uievents) do ui:uievent(unpack(v)) end end
 		if arg.autocomplete       ~= nil then for _, v in ipairs(arg.autocomplete) do ui:autocomplete(unpack(v)) end end
 	end
 	return ui
