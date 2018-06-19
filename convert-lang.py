46324c698fc0fac2e495922ccb6244df649974a9
Emil Zhai <root@derzh.com>
2018-06-19 11:53:30 +0800

    更新语言转换脚本

--- convert-lang.py
+++ convert-lang.py
@@ -1,10 +1,6 @@
 # -*- coding: GBK -*-
 from lang_mapping import map_zhcn2zhtw
-import sys, os
-import os.path # 遍历文件所需库
-import codecs  # 保存UTF-8编码所需库
-import re      # 正则匹配
-import time    # 获取时间
+import sys, os, codecs, re, time
 
 def zhcn2zhtw(source):
 	dest = ""
@@ -17,26 +13,24 @@ def zhcn2zhtw(source):
 			dest = dest + result
 	return dest
 
-rootdir = os.path.dirname(os.path.abspath(__file__))    # 指明被遍历的文件夹
-for parent, dirnames, filenames in os.walk(rootdir):    # 三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
-	if parent == "@DATA" or parent == ".git":
-		continue
-	#for dirname in  dirnames:                      #输出文件夹信息
-	#    print "parent is:" + parent
+root = os.path.dirname(os.path.abspath(__file__))
+excludes = [".git", "@DATA"]
+
+for cwd, dirs, files in os.walk(root):
+	dirs[:] = [d for d in dirs if d not in excludes]
+
+	#for dirname in  dirs:
+	#    print "cwd is:" + cwd
 	#    print  "dirname is" + dirname
 
-	for filename in filenames:                      #输出文件信息
+	for filename in files:
 		if filename == "zhcn.jx3dat":
-			#print "parent is:" + parent
-			#print "filename is:" + filename
-			#print "the full name of the file is:" + os.path.join(parent,filename) #输出文件路径信息
-			print 'file loading: ' + os.path.join(parent,filename)
+			print 'file loading: ' + os.path.join(cwd,filename)
 			# all_the_text = "-- language data (zhtw) updated at " + time.strftime('%Y-%m-%d %H:%I:%M',time.localtime(time.time())) + "\r\n"
 			all_the_text = ""
-			for count, line in enumerate(codecs.open(os.path.join(parent,filename),'r',encoding='gbk')):
+			for count, line in enumerate(codecs.open(os.path.join(cwd,filename),'r',encoding='gbk')):
 				if count == 0 and line.find('-- language data') == 0:
 					all_the_text = line.replace('zhcn', 'zhtw')
-					# pass
 				else:
 					all_the_text = all_the_text + line
 
@@ -45,7 +39,7 @@ for parent, dirnames, filenames in os.walk(rootdir):    # 三个参数：分别返回1.父
 			all_the_text = zhcn2zhtw(all_the_text)
 
 			print 'file saving...'
-			with codecs.open(os.path.join(parent,"zhtw.jx3dat"),'w',encoding='utf8') as f:
+			with codecs.open(os.path.join(cwd,"zhtw.jx3dat"),'w',encoding='utf8') as f:
 				f.write(all_the_text)
 				print 'file saved: zhtw.jx3dat'
 			print '-----------------------'
