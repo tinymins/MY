5ecbd040cb255598377c5134a1236776de9c6f6e
Emil Zhai <root@derzh.com>
2018-06-20 02:08:52 +0800

    修复试衣间在不同缩放下的问题

--- MY_Toolbox/src/MY_PlayerViewMagnifier.lua
+++ MY_Toolbox/src/MY_PlayerViewMagnifier.lua
@@ -2,28 +2,29 @@
 -- @Author: Emil Zhai (root@derzh.com)
 -- @Date:   2018-06-19 23:58:06
 -- @Last Modified by:   Emil Zhai (root@derzh.com)
--- @Last Modified time: 2018-06-20 01:50:25
+-- @Last Modified time: 2018-06-20 02:07:03
 ---------------------------------------------------
 local _L = MY.LoadLangPack(MY.GetAddonInfo().szRoot .. 'MY_Toolbox/lang/')
 
 local function onFrameCreate()
 	local config
 	if arg0:GetName() == 'PlayerView' then
-		config = { x = 35, y = 8, w = 30, h = 30, coefficient = 2.3 }
+		config = { x = 35, y = 8, w = 30, h = 30, coefficient = 2.45 }
 	elseif arg0:GetName() == 'ExteriorView' then
-		config = { x = 20, y = 15, w = 40, h = 40, coefficient = 2.25 }
+		config = { x = 20, y = 15, w = 40, h = 40, coefficient = 2.5 }
 	end
 	if config then
-		local frame, ui = arg0, XGUI(arg0)
+		local frame, ui, coefficient, posx, posy = arg0, XGUI(arg0), config.coefficient / Station.GetUIScale()
 		ui:append(MY.GetAddonInfo().szRoot .. 'MY_Toolbox/ui/Btn_MagnifierUp.ini:WndButton', {
 			name = 'Btn_MY_MagnifierUp',
 			x = config.x, y = config.y, w = config.w, h = config.h,
 			onclick = function()
+				posx, posy = ui:pos()
 				frame:EnableDrag(true)
 				frame:SetDragArea(0, 0, frame:GetW(), 50)
-				frame:Scale(config.coefficient, config.coefficient)
+				frame:Scale(coefficient, coefficient)
 				frame:SetPoint('CENTER', 0, 0, 'CENTER', 0, 0)
-				ui:find('.Text'):fontScale(config.coefficient)
+				ui:find('.Text'):fontScale(coefficient)
 				ui:children('#Btn_MY_MagnifierUp'):hide()
 				ui:children('#Btn_MY_MagnifierDown'):show()
 			end,
@@ -33,7 +34,8 @@ local function onFrameCreate()
 			name = 'Btn_MY_MagnifierDown',
 			x = config.x, y = config.y, w = config.w, h = config.h, visible = false,
 			onclick = function()
-				frame:Scale(1 / config.coefficient, 1 / config.coefficient)
+				frame:Scale(1 / coefficient, 1 / coefficient)
+				ui:pos(posx, posy)
 				ui:find('.Text'):fontScale(1)
 				ui:children('#Btn_MY_MagnifierUp'):show()
 				ui:children('#Btn_MY_MagnifierDown'):hide()
