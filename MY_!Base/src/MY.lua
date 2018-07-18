d0245f926c646ce814b6fd4523f79c3dafc5e351
Emil Zhai <root@derzh.com>
2018-07-19 01:46:56 +0800

    Release V108

--- MY_!Base/src/MY.lua
+++ MY_!Base/src/MY.lua
@@ -157,8 +157,8 @@ MY.IsNumber, MY.IsString, MY.IsTable, MY.IsFunction = IsNumber, IsString, IsTabl
 ---------------------------------------------------------------------------------------------
 -- 本地函数变量
 ---------------------------------------------------------------------------------------------
-local _BUILD_ = '20180713'
-local _VERSION_ = 0x2010700
+local _BUILD_ = '20180718'
+local _VERSION_ = 0x2010800
 local _DEBUGLV_ = tonumber(LoadLUAData('interface/my.debug.level') or nil) or 4
 local _DELOGLV_ = tonumber(LoadLUAData('interface/my.delog.level') or nil) or 4
 local _NORESTM_ = tonumber(LoadLUAData('interface/my.nrtim.level') or nil) or -1
