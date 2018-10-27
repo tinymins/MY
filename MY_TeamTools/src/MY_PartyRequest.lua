8d9aafc3609ebba9351c1a1d532624a30d7ab755
Emil Zhai <root@derzh.com>
2018-10-27 13:28:40 +0800

    修复组队助手报错的问题

--- MY_TeamTools/src/MY_PartyRequest.lua
+++ MY_TeamTools/src/MY_PartyRequest.lua
@@ -122,7 +122,7 @@ function MY_PartyRequest.OnMouseEnter()
 	elseif this.info then
 		local x, y = this:GetAbsPos()
 		local w, h = this:GetSize()
-		local szTip = MY_Farbnamen.GetTip(this.info.szName)
+		local szTip = MY_Farbnamen and MY_Farbnamen.GetTip(this.info.szName)
 		if szTip then
 			OutputTip(szTip, 450, {x, y, w, h}, MY_TIP_POSTYPE.TOP_BOTTOM)
 		end
