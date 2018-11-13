395ab856034ccede9f13b59adbfc5ea06ca997fe
Emil Zhai <root@derzh.com>
2018-11-13 12:47:24 +0800

    修复基础库一处变量作用域问题

--- MY_!Base/src/MY.UI.lua
+++ MY_!Base/src/MY.UI.lua
@@ -15,7 +15,7 @@ local ipairs, pairs, next, pcall = ipairs, pairs, next, pcall
 local sub, len, format, rep = string.sub, string.len, string.format, string.rep
 local find, byte, char, gsub = string.find, string.byte, string.char, string.gsub
 local type, tonumber, tostring = type, tonumber, tostring
-local huge, pi, random = math.huge, math.pi, math.random
+local huge, pi, random, abs = math.huge, math.pi, math.random, math.abs
 local min, max, floor, ceil = math.min, math.max, math.floor, math.ceil
 local pow, sqrt, sin, cos, tan = math.pow, math.sqrt, math.sin, math.cos, math.tan
 local insert, remove, concat, sort = table.insert, table.remove, table.concat, table.sort
@@ -25,9 +25,11 @@ local wsub, wlen, wfind = wstring.sub, wstring.len, wstring.find
 local GetTime, GetLogicFrameCount = GetTime, GetLogicFrameCount
 local GetClientTeam, UI_GetClientPlayerID = GetClientTeam, UI_GetClientPlayerID
 local GetClientPlayer, GetPlayer, GetNpc, IsPlayer = GetClientPlayer, GetPlayer, GetNpc, IsPlayer
-local IsNil, IsBoolean, IsEmpty, RandomChild = MY.IsNil, MY.IsBoolean, MY.IsEmpty, MY.RandomChild
-local IsNumber, IsString, IsTable, IsFunction = MY.IsNumber, MY.IsString, MY.IsTable, MY.IsFunction
+local UI, Get, RandomChild = MY.UI, MY.Get, MY.RandomChild
+local IsNil, IsBoolean, IsNumber, IsFunction = MY.IsNil, MY.IsBoolean, MY.IsNumber, MY.IsFunction
+local IsEmpty, IsString, IsTable, IsUserdata = MY.IsEmpty, MY.IsString, MY.IsTable, MY.IsUserdata
 ---------------------------------------------------------------------------------------------------
+local _L = MY.LoadLangPack()
 
 -------------------------------------
 -- UI object class
@@ -42,7 +44,7 @@ local function createInstance(c, ins, ...)
 	end
 	return c
 end
-local UI = setmetatable({}, {
+UI = setmetatable({}, {
 	__index = {},
 	__tostring = function(t) return 'MY_UI (class prototype)' end,
 	__call = function (...)
@@ -70,7 +72,6 @@ local UI = setmetatable({}, {
 	end,
 })
 end
-local _L = MY.LoadLangPack()
 
 -----------------------------------------------------------
 -- my ui common functions
