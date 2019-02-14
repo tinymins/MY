45f466078b893c86c655e876072f564cdbc55de1
Emil Zhai <root@derzh.com>
2019-02-14 16:10:37 +0800

    修复剑心导致玩家头像越来越大从而影响锚定位的问题

--- MY_!Base/src/MY.LoadingEnd.lua
+++ MY_!Base/src/MY.LoadingEnd.lua
@@ -20,3 +20,31 @@ MY = setmetatable({}, {
 })
 
 FireUIEvent('MY_BASE_LOADING_END')
+
+-- 修复剑心导致玩家头像越来越大的问题。。。
+do
+local nInitWidth, nFinalWidth
+MY.RegisterEvent('ON_FRAME_CREATE.FixJXPlayer', function()
+	if arg0:GetName() == 'Player' then
+		nInitWidth = arg0:GetW()
+	end
+end)
+local function RevertWidth()
+	local frame = Station.Lookup('Normal/Player')
+	if not frame or not nInitWidth then
+		return
+	end
+	nFinalWidth = frame:GetW()
+	frame:SetW(nInitWidth)
+end
+local function ApplyWidth()
+	local frame = Station.Lookup('Normal/Player')
+	if not frame or not nFinalWidth then
+		return
+	end
+	frame:SetW(nFinalWidth)
+end
+MY.RegisterReload('FixJXPlayer', RevertWidth)
+MY.RegisterEvent('ON_ENTER_CUSTOM_UI_MODE.FixJXPlayer', RevertWidth)
+MY.RegisterEvent('ON_LEAVE_CUSTOM_UI_MODE.FixJXPlayer', ApplyWidth)
+end
