c8102ef2716aed1cd35d831c1ac5631a0772668a
Emil Zhai <root@derzh.com>
2019-07-05 11:55:58 +0800

    修复聊天记录集群节点自我修复遇到大节点包裹小节点时的问题

--- MY_ChatLog/src/MY_ChatLog_DS.lua
+++ MY_ChatLog/src/MY_ChatLog_DS.lua
@@ -127,39 +127,52 @@ function DS:InitDB(bFixProblem)
 			end
 		end
 		-- 修复覆盖区域不连续的节点（覆盖区中断问题、分段冲突问题）
-		for i = 1, #aDB - 1 do
-			local db1, db2 = aDB[i], aDB[i + 1]
-			-- 检测中间节点最大值
-			if IsHugeNumber(db1:GetMaxTime()) then
-				LIB.Debug({'Unexpected huge MaxTime: ' .. db1:ToString()}, _L['MY_ChatLog'], DEBUG_LEVEL.WARNING)
-				if not bFixProblem then
-					return false
-				end
-				db1:SetMaxTime(db1:GetMaxRecTime())
-				LIB.Debug({'Fix unexpected huge MaxTime: ' .. db1:ToString()}, _L['MY_ChatLog'], DEBUG_LEVEL.WARNING)
-			end
-			-- 检测区域连续性
-			if db1:GetMaxTime() ~= db2:GetMinTime() then
-				LIB.Debug({'Unexpected noncontinuously time between ' .. db1:ToString() .. ' and ' .. db2:ToString()}, _L['MY_ChatLog'], DEBUG_LEVEL.WARNING)
-				if not bFixProblem then
-					return false
-				end
-				if db1:GetMaxRecTime() <= db2:GetMinTime() then -- 覆盖区中断 扩充左侧区域
-					db1:SetMaxTime(db2:GetMinTime())
-					LIB.Debug({'Fix noncontinuously time by modify ' .. db1:ToString()}, _L['MY_ChatLog'], DEBUG_LEVEL.LOG)
-				elseif db1:GetMaxTime() <= db2:GetMinRecTime() then -- 覆盖区中断 扩充右侧区域
-					db2:SetMinTime(db1:GetMaxTime())
-					LIB.Debug({'Fix noncontinuously time by modify ' .. db2:ToString()}, _L['MY_ChatLog'], DEBUG_LEVEL.LOG)
-				else -- 覆盖区域冲突 将右侧节点的冲突区域数据移动到左侧节点中
-					db1:SetMaxTime(db1:GetMaxRecTime())
-					for _, rec in ipairs(db2:SelectMsgByTime('<=', db1:GetMaxTime())) do
-						db1:InsertMsg(rec.nChannel, rec.szText, rec.szMsg, rec.szTalker, rec.nTime, rec.szHash)
+		do
+			local i = 1
+			while i < #aDB do
+				local db1, db2 = aDB[i], aDB[i + 1]
+				-- 检测中间节点最大值
+				if IsHugeNumber(db1:GetMaxTime()) then
+					LIB.Debug({'Unexpected huge MaxTime: ' .. db1:ToString()}, _L['MY_ChatLog'], DEBUG_LEVEL.WARNING)
+					if not bFixProblem then
+						return false
 					end
-					db1:Flush()
-					db2:DeleteMsgByTime('<=', db1:GetMaxTime())
-					db2:SetMinTime(db1:GetMaxTime())
-					LIB.Debug({'Fix noncontinuously time by moving data from ' .. db2:ToString() .. ' to ' .. db1:ToString()}, _L['MY_ChatLog'], DEBUG_LEVEL.LOG)
+					db1:SetMaxTime(db1:GetMaxRecTime())
+					LIB.Debug({'Fix unexpected huge MaxTime: ' .. db1:ToString()}, _L['MY_ChatLog'], DEBUG_LEVEL.WARNING)
 				end
+				-- 检测区域连续性
+				if db1:GetMaxTime() ~= db2:GetMinTime() then
+					LIB.Debug({'Unexpected noncontinuously time between ' .. db1:ToString() .. ' and ' .. db2:ToString()}, _L['MY_ChatLog'], DEBUG_LEVEL.WARNING)
+					if not bFixProblem then
+						return false
+					end
+					if db1:GetMaxRecTime() <= db2:GetMinTime() then -- 覆盖区中断 扩充左侧区域
+						db1:SetMaxTime(db2:GetMinTime())
+						LIB.Debug({'Fix noncontinuously time by modify ' .. db1:ToString()}, _L['MY_ChatLog'], DEBUG_LEVEL.LOG)
+					elseif db1:GetMaxTime() <= db2:GetMinRecTime() then -- 覆盖区中断 扩充右侧区域
+						db2:SetMinTime(db1:GetMaxTime())
+						LIB.Debug({'Fix noncontinuously time by modify ' .. db2:ToString()}, _L['MY_ChatLog'], DEBUG_LEVEL.LOG)
+					elseif db1:GetMaxTime() >= db2:GetMaxTime() then -- 覆盖区冲突 右侧区域完全被左侧区域包裹 将右侧节点并入左侧节点中
+						for _, rec in ipairs(db2:SelectMsg()) do
+							db1:InsertMsg(rec.nChannel, rec.szText, rec.szMsg, rec.szTalker, rec.nTime, rec.szHash)
+						end
+						db1:Flush()
+						db2:DeleteDB()
+						LIB.Debug({'Fix noncontinuously time by merge ' .. db2:ToString() .. ' to ' .. db1:ToString()}, _L['MY_ChatLog'], DEBUG_LEVEL.LOG)
+						remove(aDB, i + 1)
+						i = i - 1
+					else -- 覆盖区域冲突 将右侧节点的冲突区域数据移动到左侧节点中
+						db1:SetMaxTime(db1:GetMaxRecTime())
+						for _, rec in ipairs(db2:SelectMsgByTime('<=', db1:GetMaxTime())) do
+							db1:InsertMsg(rec.nChannel, rec.szText, rec.szMsg, rec.szTalker, rec.nTime, rec.szHash)
+						end
+						db1:Flush()
+						db2:DeleteMsgByTime('<=', db1:GetMaxTime())
+						db2:SetMinTime(db1:GetMaxTime())
+						LIB.Debug({'Fix noncontinuously time by moving data from ' .. db2:ToString() .. ' to ' .. db1:ToString()}, _L['MY_ChatLog'], DEBUG_LEVEL.LOG)
+					end
+				end
+				i = i + 1
 			end
 		end
 		-- 检查集群最新活跃节点是否存在
