03f3918852f463e523f46640e998959f9892d6cb
Emil Zhai <root@derzh.com>
2019-07-16 15:49:07 +0800

    恢复递归版本的格式化检查用户数据函数

--- MY_!Base/src/Sys.lua
+++ MY_!Base/src/Sys.lua
@@ -475,171 +475,132 @@ end
 -- Format data's structure as struct descripted.
 do
 local defaultParams = { keepNewChild = false }
-function LIB.FormatDataStructure(data, struct, assign, metaSymbol)
-	if not assign then
-		data = FullClone(data)
-	end
+local function FormatDataStructure(data, struct, assign, metaSymbol)
 	if metaSymbol == nil then
 		metaSymbol = '__META__'
 	end
-	local ret = {data = data}
-	local aParentStack = {ret}
-	local aKeyStack = {'data'}
-	local aModeStack = {'value'}
-	local aStructStack = {struct}
-	local nStack, tParent, oKey, oValue, eMode = 1
-	while nStack ~= 0 do
-		-- 取出下一个待处理的
-		tParent = aParentStack[nStack]
-		eMode = aModeStack[nStack]
-		oKey = aKeyStack[nStack]
-		oValue = tParent[oKey]
-		struct = aStructStack[nStack]
-		data = eMode == 'key' and oKey or tParent[oKey]
-		nStack = nStack - 1
-		-- 标准化参数
-		local params = setmetatable({}, defaultParams)
-		local structTypes, defaultData, defaultDataType
-		local keyTemplate, childTemplate, arrayTemplate, dictionaryTemplate
-		if type(struct) == 'table' and struct[1] == metaSymbol then
-			-- 处理有META标记的数据项
-			-- 允许类型和默认值
-			structTypes = struct[2] or { type(struct.__VALUE__) }
-			defaultData = struct[3] or struct.__VALUE__
-			defaultDataType = type(defaultData)
-			-- 表模板相关参数
-			if defaultDataType == 'table' then
-				keyTemplate = struct.__KEY_TEMPLATE__
-				childTemplate = struct.__CHILD_TEMPLATE__
-				arrayTemplate = struct.__ARRAY_TEMPLATE__
-				dictionaryTemplate = struct.__DICTIONARY_TEMPLATE__
-			end
-			-- 附加参数
-			if struct.__PARAMS__ then
-				for k, v in pairs(struct.__PARAMS__) do
-					params[k] = v
-				end
-			end
-		else
-			-- 处理普通数据项
-			structTypes = { type(struct) }
-			defaultData = struct
-			defaultDataType = type(defaultData)
+	-- 标准化参数
+	local params = setmetatable({}, defaultParams)
+	local structTypes, defaultData, defaultDataType
+	local keyTemplate, childTemplate, arrayTemplate, dictionaryTemplate
+	if type(struct) == 'table' and struct[1] == metaSymbol then
+		-- 处理有META标记的数据项
+		-- 允许类型和默认值
+		structTypes = struct[2] or { type(struct.__VALUE__) }
+		defaultData = struct[3] or struct.__VALUE__
+		defaultDataType = type(defaultData)
+		-- 表模板相关参数
+		if defaultDataType == 'table' then
+			keyTemplate = struct.__KEY_TEMPLATE__
+			childTemplate = struct.__CHILD_TEMPLATE__
+			arrayTemplate = struct.__ARRAY_TEMPLATE__
+			dictionaryTemplate = struct.__DICTIONARY_TEMPLATE__
 		end
-		-- 计算结构和数据的类型
-		local dataType = type(data)
-		local dataTypeExists = false
-		if not dataTypeExists then
-			for _, v in ipairs(structTypes) do
-				if dataType == v then
-					dataTypeExists = true
-					break
-				end
+		-- 附加参数
+		if struct.__PARAMS__ then
+			for k, v in pairs(struct.__PARAMS__) do
+				params[k] = v
 			end
 		end
-		-- 分别处理类型匹配与不匹配的情况
-		if dataTypeExists then
-			local keys, skipKeys = {}, {}
-			-- 数据类型是表且默认数据也是表 则递归检查子元素与默认子元素
-			if dataType == 'table' and defaultDataType == 'table' then
-				for k, v in pairs(defaultData) do
-					keys[k], skipKeys[k] = true, true
-					nStack = nStack + 1
-					aParentStack[nStack] = data
-					aKeyStack[nStack] = k
-					aModeStack[nStack] = 'value'
-					aStructStack[nStack] = defaultData[k]
-				end
+	else
+		-- 处理普通数据项
+		structTypes = { type(struct) }
+		defaultData = struct
+		defaultDataType = type(defaultData)
+	end
+	-- 计算结构和数据的类型
+	local dataType = type(data)
+	local dataTypeExists = false
+	if not dataTypeExists then
+		for _, v in ipairs(structTypes) do
+			if dataType == v then
+				dataTypeExists = true
+				break
 			end
-			-- 数据类型是表且META信息中定义了子元素KEY模板 则递归检查子元素KEY与子元素KEY模板
-			if dataType == 'table' and keyTemplate then
-				for k, v in pairs(data) do
-					if not skipKeys[k] then
-						nStack = nStack + 1
-						aParentStack[nStack] = data
-						aKeyStack[nStack] = k
-						aModeStack[nStack] = 'key'
-						aStructStack[nStack] = keyTemplate
-					end
-				end
-			end
-			-- 数据类型是表且META信息中定义了子元素模板 则递归检查子元素与子元素模板
-			if dataType == 'table' and childTemplate then
-				for k, v in pairs(data) do
-					if not skipKeys[k] then
-						keys[k] = true
-						nStack = nStack + 1
-						aParentStack[nStack] = data
-						aKeyStack[nStack] = k
-						aModeStack[nStack] = 'value'
-						aStructStack[nStack] = childTemplate
-					end
-				end
-			end
-			-- 数据类型是表且META信息中定义了列表子元素模板 则递归检查子元素与列表子元素模板
-			if dataType == 'table' and arrayTemplate then
-				for i, v in pairs(data) do
-					if type(i) == 'number' then
-						if not skipKeys[k] then
-							keys[i] = true
-							nStack = nStack + 1
-							aParentStack[nStack] = data
-							aKeyStack[nStack] = i
-							aModeStack[nStack] = 'value'
-							aStructStack[nStack] = arrayTemplate
-						end
-					end
-				end
-			end
-			-- 数据类型是表且META信息中定义了哈希子元素模板 则递归检查子元素与哈希子元素模板
-			if dataType == 'table' and dictionaryTemplate then
-				for k, v in pairs(data) do
-					if type(k) ~= 'number' then
-						if not skipKeys[k] then
-							keys[k] = true
-							nStack = nStack + 1
-							aParentStack[nStack] = data
-							aKeyStack[nStack] = k
-							aModeStack[nStack] = 'value'
-							aStructStack[nStack] = dictionaryTemplate
-						end
-					end
-				end
-			end
-			-- 数据类型是表且默认数据也是表 则递归检查子元素是否需要保留
-			if dataType == 'table' and defaultDataType == 'table' then
-				if not params.keepNewChild then
-					for k, v in pairs(data) do
-						if defaultData[k] == nil and not keys[k] then -- 默认中没有且没有通过过滤器函数的则删除
-							data[k] = nil
-						end
-					end
-				end
-			end
-		else -- 类型不匹配的情况
-			if type(defaultData) == 'table' then
-				-- 默认值为表 需要递归检查子元素
-				data = {}
-				for k, v in pairs(defaultData) do
-					nStack = nStack + 1
-					aParentStack[nStack] = data
-					aKeyStack[nStack] = k
-					aModeStack[nStack] = 'value'
-					aStructStack[nStack] = v
-				end
-			else -- 默认值不是表 直接克隆数据
-				data = FullClone(defaultData)
-			end
-		end
-		if eMode == 'key' then
-			tParent[oKey] = nil
-			tParent[data] = oValue
-		else
-			tParent[oKey] = data
 		end
 	end
-	return ret.data
+	-- 分别处理类型匹配与不匹配的情况
+	if dataTypeExists then
+		if not assign then
+			data = FullClone(data)
+		end
+		local keys, skipKeys = {}, {}
+		-- 数据类型是表且默认数据也是表 则递归检查子元素与默认子元素
+		if dataType == 'table' and defaultDataType == 'table' then
+			for k, v in pairs(defaultData) do
+				keys[k], skipKeys[k] = true, true
+				data[k] = FormatDataStructure(data[k], defaultData[k], true, metaSymbol)
+			end
+		end
+		-- 数据类型是表且META信息中定义了子元素KEY模板 则递归检查子元素KEY与子元素KEY模板
+		if dataType == 'table' and keyTemplate then
+			for k, v in pairs(data) do
+				if not skipKeys[k] then
+					local k1 = FormatDataStructure(k, keyTemplate, true, metaSymbol)
+					if k1 ~= k then
+						if k1 ~= nil then
+							data[k1] = data[k]
+						end
+						data[k] = nil
+					end
+				end
+			end
+		end
+		-- 数据类型是表且META信息中定义了子元素模板 则递归检查子元素与子元素模板
+		if dataType == 'table' and childTemplate then
+			for k, v in pairs(data) do
+				if not skipKeys[k] then
+					keys[k] = true
+					data[k] = FormatDataStructure(data[k], childTemplate, true, metaSymbol)
+				end
+			end
+		end
+		-- 数据类型是表且META信息中定义了列表子元素模板 则递归检查子元素与列表子元素模板
+		if dataType == 'table' and arrayTemplate then
+			for i, v in pairs(data) do
+				if type(i) == 'number' then
+					if not skipKeys[k] then
+						keys[i] = true
+						data[i] = FormatDataStructure(data[i], arrayTemplate, true, metaSymbol)
+					end
+				end
+			end
+		end
+		-- 数据类型是表且META信息中定义了哈希子元素模板 则递归检查子元素与哈希子元素模板
+		if dataType == 'table' and dictionaryTemplate then
+			for k, v in pairs(data) do
+				if type(k) ~= 'number' then
+					if not skipKeys[k] then
+						keys[k] = true
+						data[k] = FormatDataStructure(data[k], dictionaryTemplate, true, metaSymbol)
+					end
+				end
+			end
+		end
+		-- 数据类型是表且默认数据也是表 则递归检查子元素是否需要保留
+		if dataType == 'table' and defaultDataType == 'table' then
+			if not params.keepNewChild then
+				for k, v in pairs(data) do
+					if defaultData[k] == nil and not keys[k] then -- 默认中没有且没有通过过滤器函数的则删除
+						data[k] = nil
+					end
+				end
+			end
+		end
+	else -- 类型不匹配的情况
+		if type(defaultData) == 'table' then
+			-- 默认值为表 需要递归检查子元素
+			data = {}
+			for k, v in pairs(defaultData) do
+				data[k] = FormatDataStructure(nil, v, true, metaSymbol)
+			end
+		else -- 默认值不是表 直接克隆数据
+			data = FullClone(defaultData)
+		end
+	end
+	return data
 end
+LIB.FormatDataStructure = FormatDataStructure
 end
 
 function LIB.SetGlobalValue(szVarPath, Val)
