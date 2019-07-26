a256781b04f75b7bf610735f58fab08174403582
Emil Zhai <root@derzh.com>
2019-07-26 13:29:52 +0800

    完善团队监控设置界面

---
+++ MY_TeamMon/src/MY_TeamMon.PS.lua
@@ -0,0 +1,219 @@
+--------------------------------------------------------
+-- This file is part of the JX3 Mingyi Plugin.
+-- @link     : https://jx3.derzh.com/
+-- @desc     : 团队监控设置界面
+-- @author   : 茗伊 @双梦镇 @追风蹑影
+-- @ref      : William Chan (Webster)
+-- @modifier : Emil Zhai (root@derzh.com)
+-- @copyright: Copyright (c) 2013 EMZ Kingsoft Co., Ltd.
+--------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------
+-- these global functions are accessed all the time by the event handler
+-- so caching them is worth the effort
+-----------------------------------------------------------------------------------------------------------
+local setmetatable = setmetatable
+local ipairs, pairs, next, pcall, select = ipairs, pairs, next, pcall, select
+local sub, len, format, rep = string.sub, string.len, string.format, string.rep
+local find, byte, char, gsub = string.find, string.byte, string.char, string.gsub
+local type, tonumber, tostring = type, tonumber, tostring
+local HUGE, PI, random, abs = math.huge, math.pi, math.random, math.abs
+local min, max, floor, ceil = math.min, math.max, math.floor, math.ceil
+local pow, sqrt, sin, cos, tan, atan = math.pow, math.sqrt, math.sin, math.cos, math.tan, math.atan
+local insert, remove, concat, sort = table.insert, table.remove, table.concat, table.sort
+local pack, unpack = table.pack or function(...) return {...} end, table.unpack or unpack
+-- jx3 apis caching
+local wsub, wlen, wfind, wgsub = wstring.sub, wstring.len, wstring.find, StringReplaceW
+local GetTime, GetLogicFrameCount, GetCurrentTime = GetTime, GetLogicFrameCount, GetCurrentTime
+local GetClientTeam, UI_GetClientPlayerID = GetClientTeam, UI_GetClientPlayerID
+local GetClientPlayer, GetPlayer, GetNpc, IsPlayer = GetClientPlayer, GetPlayer, GetNpc, IsPlayer
+local LIB = MY
+local UI, DEBUG_LEVEL, PATH_TYPE, PACKET_INFO = LIB.UI, LIB.DEBUG_LEVEL, LIB.PATH_TYPE, LIB.PACKET_INFO
+local spairs, spairs_r, sipairs, sipairs_r = LIB.spairs, LIB.spairs_r, LIB.sipairs, LIB.sipairs_r
+local ipairs_r, count_c, pairs_c, ipairs_c = LIB.ipairs_r, LIB.count_c, LIB.pairs_c, LIB.ipairs_c
+local IsNil, IsBoolean, IsUserdata, IsFunction = LIB.IsNil, LIB.IsBoolean, LIB.IsUserdata, LIB.IsFunction
+local IsString, IsTable, IsArray, IsDictionary = LIB.IsString, LIB.IsTable, LIB.IsArray, LIB.IsDictionary
+local IsNumber, IsHugeNumber, IsEmpty, IsEquals = LIB.IsNumber, LIB.IsHugeNumber, LIB.IsEmpty, LIB.IsEquals
+local Call, XpCall, GetTraceback, RandomChild = LIB.Call, LIB.XpCall, LIB.GetTraceback, LIB.RandomChild
+local Get, Set, Clone, GetPatch, ApplyPatch = LIB.Get, LIB.Set, LIB.Clone, LIB.GetPatch, LIB.ApplyPatch
+local EncodeLUAData, DecodeLUAData, CONSTANT = LIB.EncodeLUAData, LIB.DecodeLUAData, LIB.CONSTANT
+-----------------------------------------------------------------------------------------------------------
+local _L = LIB.LoadLangPack(PACKET_INFO.ROOT .. 'MY_TeamMon/lang/')
+if not LIB.AssertVersion('MY_TeamMon', _L['MY_TeamMon'], 0x2013500) then
+	return
+end
+
+local PS = {}
+
+function PS.OnPanelActive(wnd)
+	local ui = UI(wnd)
+	local X, Y = 20, 20
+	local nX, nY = X, Y
+
+	-- ui:append('WndButton2', {
+	-- 	x = 400, y = 20, text = g_tStrings.HELP_PANEL,
+	-- 	onclick = function()
+	-- 		OpenInternetExplorer('https://github.com/luckyyyyy/JH/blob/dev/JH_DBM/README.md')
+	-- 	end,
+	-- })
+
+	nX, nY = ui:append('Text', { x = X, y = Y, text = _L['Master switch'], font = 27 }, true):autoWidth():pos('BOTTOMRIGHT')
+	nX, nY = ui:append('WndCheckBox', {
+		x = X + 10, y = nY, text = _L['Enable MY_TeamMon Plugin'],
+		checked = MY_TeamMon.bEnable,
+		oncheck = function(bCheck)
+			MY_TeamMon.Enable(bCheck, true)
+			MY_TeamMon.bEnable = bCheck
+		end,
+	}, true):autoWidth():pos('BOTTOMRIGHT')
+	if Circle then
+		nY = 17
+		nX = ui:append('WndCheckBox', {
+			x = nX + 5, y = nY, text = _L['Circle Enable'],
+			checked = Circle.bEnable,
+			oncheck = function(bCheck)
+				Circle.Enable(bCheck)
+				if bCheck then
+					FireUIEvent('CIRCLE_RELOAD')
+				end
+			end,
+		}, true):autoWidth():pos('BOTTOMRIGHT')
+		nX, nY = ui:append('WndCheckBox', {
+			x = nX + 5, y = nY + 10, text = _L['Circle Border'],
+			checked = Circle.bBorder,
+			oncheck = function(bCheck)
+				Circle.bBorder = bCheck
+				FireUIEvent('CIRCLE_RELOAD')
+			end,
+		}, true):autoWidth():pos('BOTTOMRIGHT')
+	end
+	nX, nY = ui:append('Text', { x = X, y = nY + 5, text = _L['Enable alarm (master switch)'], font = 27 }, true):autoWidth():pos('BOTTOMRIGHT')
+	nX = ui:append('WndCheckBox', {
+		x = X + 10, y = nY,
+		text = _L['Team Channel Alarm'],
+		color = GetMsgFontColor('MSG_TEAM', true),
+		checked = MY_TeamMon.bPushTeamChannel,
+		oncheck = function(bCheck)
+			MY_TeamMon.bPushTeamChannel = bCheck
+		end,
+	}, true):autoWidth():pos('BOTTOMRIGHT')
+	nX = ui:append('WndCheckBox', {
+		x = nX + 5, y = nY,
+		text = _L['Whisper Channel Alarm'], color = GetMsgFontColor('MSG_WHISPER', true),
+		checked = MY_TeamMon.bPushWhisperChannel,
+		oncheck = function(bCheck)
+			MY_TeamMon.bPushWhisperChannel = bCheck
+		end,
+	}, true):autoWidth():pos('BOTTOMRIGHT')
+	nX = ui:append('WndCheckBox', {
+		x = nX + 5, y = nY, text = _L['Buff List'],
+		checked = MY_TeamMon.bPushBuffList,
+		oncheck = function(bCheck)
+			MY_TeamMon.bPushBuffList = bCheck
+		end,
+	}, true):autoWidth():pos('BOTTOMRIGHT')
+	nX, nY = ui:append('WndCheckBox', {
+		x = nX + 5, y = nY, text = _L['Center Alarm'],
+		checked = MY_TeamMon.bPushCenterAlarm,
+		oncheck = function(bCheck)
+			MY_TeamMon.bPushCenterAlarm = bCheck
+		end,
+	}, true):autoWidth():pos('BOTTOMRIGHT')
+	nX = X + 5
+	if not LIB.IsShieldedVersion() then
+		nX = ui:append('WndCheckBox', {
+			x = nX + 5, y = nY, text = _L['Big Font Alarm'],
+			checked = MY_TeamMon.bPushBigFontAlarm,
+			oncheck = function(bCheck)
+				MY_TeamMon.bPushBigFontAlarm = bCheck
+			end,
+		}, true):autoWidth():pos('BOTTOMRIGHT')
+		nX = ui:append('WndCheckBox', {
+			x = nX + 5, y = nY, text = _L['Full Screen Alarm'],
+			checked = MY_TeamMon.bPushFullScreen,
+			oncheck = function(bCheck)
+				MY_TeamMon.bPushFullScreen = bCheck
+			end,
+		}, true):autoWidth():pos('BOTTOMRIGHT')
+	end
+	nX = ui:append('WndCheckBox', {
+		x = nX + 5, y = nY, text = _L['Party Buff List'],
+		checked = MY_TeamMon.bPushPartyBuffList,
+		oncheck = function(bCheck)
+			MY_TeamMon.bPushPartyBuffList = bCheck
+		end,
+	}, true):autoWidth():pos('BOTTOMRIGHT')
+	nX, nY = ui:append('WndCheckBox', {
+		x = nX + 5, y = nY, text = _L['Screen Head Alarm'],
+		tip = _L['Requires MY_LifeBar loaded.'],
+		tippostype = MY_TIP_POSTYPE.BOTTOM_TOP,
+		checked = MY_TeamMon.bPushScreenHead,
+		oncheck = function(bCheck)
+			MY_TeamMon.bPushScreenHead = bCheck
+		end,
+	}, true):autoWidth():pos('BOTTOMRIGHT')
+	nX, nY = ui:append('Text', { x = X, y = nY + 5, text = _L['Team Panel Bind Show Buff'], font = 27 }, true):autoWidth():pos('BOTTOMRIGHT')
+	nX, nY = ui:append('WndCheckBox', {
+		x = X + 10, y = nY, text = _L['Team Panel Bind Show Buff'],
+		checked = MY_TeamMon.bPushTeamPanel,
+		oncheck = function(bCheck)
+			MY_TeamMon.bPushTeamPanel = bCheck
+			FireUIEvent('MY_TM_CREATE_CACHE')
+		end,
+	}, true):autoWidth():pos('BOTTOMRIGHT')
+	nX, nY = ui:append('Text', { x = X, y = nY + 5, text = _L['Buff List'], font = 27 }, true):autoWidth():pos('BOTTOMRIGHT')
+	nX = ui:append('WndComboBox', {
+		x = X + 10, y = nY, text = _L['Max Buff Count'],
+		menu = function()
+			local menu = {}
+			for k, v in ipairs({ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }) do
+				table.insert(menu, { szOption = v, bMCheck = true, bChecked = MY_TeamMon_BL.nCount == v, fnAction = function()
+					FireUIEvent('MY_TM_BL_RESIZE', nil, v)
+				end })
+			end
+			return menu
+		end,
+	}, true):autoWidth():pos('BOTTOMRIGHT')
+	nX, nY = ui:append('WndComboBox', {
+		x = nX + 5, y = nY, text = _L['Buff Size'],
+		menu = function()
+			local menu = {}
+			for k, v in ipairs({ 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 100 }) do
+				table.insert(menu, { szOption = v, bMCheck = true, bChecked = MY_TeamMon_BL.fScale == v / 55, fnAction = function()
+					FireUIEvent('MY_TM_BL_RESIZE', v / 55)
+				end })
+			end
+			return menu
+		end,
+	}, true):autoWidth():pos('BOTTOMRIGHT')
+	nX, nY = ui:append('Text', { x = X, y = nY + 5, text = _L['Data save mode'], font = 27 }, true):autoWidth():pos('BOTTOMRIGHT')
+	nX, nY = ui:append('WndCheckBox', {
+		x = X + 10, y = nY, text = _L['Use common data'],
+		checked = MY_TeamMon.bCommon,
+		oncheck = function(bCheck)
+			MY_TeamMon.bCommon = bCheck
+		end,
+	}, true):autoWidth():pos('BOTTOMRIGHT')
+	nX = ui:append('WndButton2', {
+		x = X + 5, y = nY + 15, text = _L['Data Panel'],
+		onclick = MY_TeamMon_UI.TogglePanel,
+	}, true):autoWidth():pos('BOTTOMRIGHT')
+	nX = ui:append('WndButton2', {
+		x = nX + 5, y = nY + 15, text = _L['Export Data'],
+		onclick = MY_TeamMon_UI.OpenExportPanel,
+	}, true):autoWidth():pos('BOTTOMRIGHT')
+	nX = ui:append('WndButton2', {
+		x = nX + 5, y = nY + 15, text = _L['Import Data'],
+		menu = function()
+			local szLang = select(3, GetVersion())
+			local menu = {}
+			table.insert(menu, { szOption = _L['Import Data (local)'], fnAction = function() MY_TeamMon.OpenImportPanel() end }) -- 有传参 不要改
+			-- local szLang = select(3, GetVersion())
+			-- if szLang == 'zhcn' or szLang == 'zhtw' then
+			-- 	table.insert(menu, { szOption = _L['Import Data (web)'], fnAction = MY_TeamMon_RemoteRequest.TogglePanel })
+			-- end
+			return menu
+		end,
+	}, true):autoWidth():pos('BOTTOMRIGHT')
+end
+
+LIB.RegisterPanel('MY_TeamMon', _L['MY_TeamMon'], _L['Raid'], 'ui/Image/UICommon/FBlist.uitex|34', PS)
