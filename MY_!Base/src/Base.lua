deca4802576524540584675748684776fc6e5e27
Emil Zhai <root@derzh.com>
2019-07-30 18:31:30 +0800

    补充蓬莱心法图标

--- MY_!Base/src/Base.lua
+++ MY_!Base/src/Base.lua
@@ -920,7 +920,7 @@ local CONSTANT = setmetatable({}, {
 			{ dwID = 10026, szUITex = 'ui/Image/icon/skill_tiance02.UITex',     nFrame = 0 }, -- ∞¡—©
 			{ dwID = 10268, szUITex = 'ui/Image/icon/skill_GB_30.UITex',        nFrame = 0 }, -- –¶≥æ
 			{ dwID = 10464, szUITex = 'ui/Image/icon/daoj_16_8_25_16.UITex',    nFrame = 0 }, -- ∞‘µ∂
-			{ dwID = 10533, szUITex = '',                                       nFrame = 0 }, -- ≈Ó¿≥
+			{ dwID = 10533, szUITex = 'ui/image/icon/jnpl_18_10_30_27.uitex',   nFrame = 45 }, -- ≈Ó¿≥
 		},
 		EQUIPMENT_SUIT_COUNT = EQUIPMENT_SUIT_COUNT or 4,
 		INVENTORY_GUILD_BANK = INVENTORY_GUILD_BANK or INVENTORY_INDEX.TOTAL + 1, --∞Ôª·≤÷ø‚ΩÁ√Ê–Èƒ‚“ª∏ˆ±≥∞¸Œª÷√
