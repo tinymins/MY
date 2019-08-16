22ed3b94f33be9a6835395a617d8df55cd2a32d6
Emil Zhai <root@derzh.com>
2019-08-16 18:46:06 +0800

    重构自动隐藏聊天栏功能

--- MY_Toolbox/src/MY_AutoHideChat.lua
+++ MY_Toolbox/src/MY_AutoHideChat.lua
@@ -36,231 +36,275 @@ local Call, XpCall, GetTraceback, RandomChild = LIB.Call, LIB.XpCall, LIB.GetTra
 local Get, Set, Clone, GetPatch, ApplyPatch = LIB.Get, LIB.Set, LIB.Clone, LIB.GetPatch, LIB.ApplyPatch
 local EncodeLUAData, DecodeLUAData, CONSTANT = LIB.EncodeLUAData, LIB.DecodeLUAData, LIB.CONSTANT
 -----------------------------------------------------------------------------------------------------------
+local _L = LIB.LoadLangPack(PACKET_INFO.ROOT..'MY_Toolbox/lang/')
+
 local STATE = {
-    SHOW    = 1, -- 已显示
-    HIDE    = 2, -- 已隐藏
-    SHOWING = 3, -- 渐变显示中
-    HIDDING = 4, -- 渐变隐藏中
+	SHOW    = 1, -- 已显示
+	HIDE    = 2, -- 已隐藏
+	SHOWING = 3, -- 渐变显示中
+	HIDDING = 4, -- 渐变隐藏中
 }
 local m_nState = STATE.SHOW
-local _L = LIB.LoadLangPack(PACKET_INFO.ROOT..'MY_Toolbox/lang/')
-local _Cache = {}
-MY_AutoHideChat = {}
-MY_AutoHideChat.bAutoHideChatPanel = false
-RegisterCustomData('MY_AutoHideChat.bAutoHideChatPanel')
+local D = {}
+local O = {
+	bEnable = false,
+}
+RegisterCustomData('MY_AutoHideChat.bEnable')
 
 -- get sys chat bg alpha
-MY_AutoHideChat.GetBgAlpha = function()
-    return Station.Lookup('Lowest2/ChatPanel1'):Lookup('Wnd_Message', 'Shadow_Back'):GetAlpha() / 255
+function D.GetBgAlpha()
+	return Station.Lookup('Lowest2/ChatPanel1'):Lookup('Wnd_Message', 'Shadow_Back'):GetAlpha() / 255
 end
 
 -- show panel
-MY_AutoHideChat.ShowChatPanel = function(nShowFrame, nDelayFrame, callback)
-    nShowFrame  = nShowFrame  or GLOBAL.GAME_FPS / 4  -- 渐变出现帧数
-    nDelayFrame = nDelayFrame or 0                    -- 隐藏延迟帧数
-    -- switch case
-    if m_nState == STATE.SHOW then
-        -- return when chat panel is visible
-        if callback then
-            Call(callback)
-        end
-        return
-    elseif m_nState == STATE.SHOWING then
-        return
-    elseif m_nState == STATE.HIDE then
-        -- show each
-        for i = 1, 10 do
-            local hFrame = Station.Lookup('Lowest2/ChatPanel' .. i)
-            if hFrame then
-                hFrame:SetMousePenetrable(false)
-            end
-        end
-    elseif m_nState == STATE.HIDDING then
-        -- unregister hide animate
-        LIB.BreatheCall('MY_AutoHideChat_Hide', false)
-    end
-    m_nState = STATE.SHOWING
+function D.ShowChatPanel(nShowFrame, nDelayFrame, callback)
+	-- 渐变出现帧数
+	if not nShowFrame then
+		nShowFrame = GLOBAL.GAME_FPS / 4
+	end
+	-- 隐藏延迟帧数
+	if not nDelayFrame then
+		nDelayFrame = 0
+	end
+	-- switch case
+	if m_nState == STATE.SHOW then
+		-- return when chat panel is visible
+		if callback then
+			Call(callback)
+		end
+		return
+	elseif m_nState == STATE.SHOWING then
+		return
+	elseif m_nState == STATE.HIDE then
+		-- show each
+		for i = 1, 10 do
+			local hFrame = Station.Lookup('Lowest2/ChatPanel' .. i)
+			if hFrame then
+				hFrame:SetMousePenetrable(false)
+			end
+		end
+	elseif m_nState == STATE.HIDDING then
+		-- unregister hide animate
+		LIB.BreatheCall('MY_AutoHideChat_Hide', false)
+	end
+	m_nState = STATE.SHOWING
 
-    -- get start alpha
-    local nStartAlpha = Station.Lookup('Lowest1/ChatTitleBG'):GetAlpha()
-    local nStartFrame = GetLogicFrameCount()
-    -- register animate breathe call
-    LIB.BreatheCall('MY_AutoHideChat_Show', function()
-        local nFrame = GetLogicFrameCount()
-        if nFrame - nDelayFrame < nStartFrame then
-            _Cache.fAhBgAlpha = MY_AutoHideChat.GetBgAlpha()
-            return
-        end
-        -- calc new alpha
-        local nAlpha = math.min(math.ceil((nFrame - nDelayFrame - nStartFrame) / nShowFrame * (255 - nStartAlpha) + nStartAlpha), 255)
-        -- alpha each panel
-        for i = 1, 10 do
-            local hFrame = Station.Lookup('Lowest2/ChatPanel' .. i)
-            if hFrame then
-                hFrame:SetAlpha(nAlpha)
-                hFrame:Lookup('Wnd_Message', 'Shadow_Back'):SetAlpha(nAlpha * _Cache.fAhBgAlpha)
-            end
-        end
-        Station.Lookup('Lowest1/ChatTitleBG'):SetAlpha(nAlpha)
-        Station.Lookup('Lowest1/ChatTitleBG', 'Image_BG'):SetAlpha(nAlpha * _Cache.fAhBgAlpha)
-        if nAlpha == 255 then
-            m_nState = STATE.SHOW
-            if callback then
-                Call(callback)
-            end
-            return 0
-        end
-    end)
+	-- get start alpha
+	local nStartAlpha = Station.Lookup('Lowest1/ChatTitleBG'):GetAlpha()
+	local nStartFrame = GetLogicFrameCount()
+	-- register animate breathe call
+	LIB.BreatheCall('MY_AutoHideChat_Show', function()
+		local nFrame = GetLogicFrameCount()
+		if nFrame - nDelayFrame < nStartFrame then
+			O.fAhBgAlpha = D.GetBgAlpha()
+			return
+		end
+		-- calc new alpha
+		local nAlpha = math.min(math.ceil((nFrame - nDelayFrame - nStartFrame) / nShowFrame * (255 - nStartAlpha) + nStartAlpha), 255)
+		-- alpha each panel
+		for i = 1, 10 do
+			local hFrame = Station.Lookup('Lowest2/ChatPanel' .. i)
+			if hFrame then
+				hFrame:SetAlpha(nAlpha)
+				hFrame:Lookup('Wnd_Message', 'Shadow_Back'):SetAlpha(nAlpha * O.fAhBgAlpha)
+			end
+		end
+		Station.Lookup('Lowest1/ChatTitleBG'):SetAlpha(nAlpha)
+		Station.Lookup('Lowest1/ChatTitleBG', 'Image_BG'):SetAlpha(nAlpha * O.fAhBgAlpha)
+		if nAlpha == 255 then
+			m_nState = STATE.SHOW
+			if callback then
+				Call(callback)
+			end
+			return 0
+		end
+	end)
 end
 
 -- hide panel
-MY_AutoHideChat.HideChatPanel = function(nHideFrame, nDelayFrame, callback)
-    nHideFrame  = nHideFrame  or GLOBAL.GAME_FPS / 2  -- 渐变消失帧数
-    nDelayFrame = nDelayFrame or GLOBAL.GAME_FPS * 5  -- 隐藏延迟帧数
-    -- switch case
-    if m_nState == STATE.SHOW then
-        -- get bg alpha
-        _Cache.fAhBgAlpha = MY_AutoHideChat.GetBgAlpha()
-    elseif m_nState == STATE.SHOWING then
-        return
-    elseif m_nState == STATE.HIDE then
-        -- return when chat panel is not visible
-        if callback then
-            Call(callback)
-        end
-        return
-    elseif m_nState == STATE.HIDDING then
-        -- unregister hide animate
-        LIB.BreatheCall('MY_AutoHideChat_Hide', false)
-    end
-    m_nState = STATE.HIDDING
+function D.HideChatPanel(nHideFrame, nDelayFrame, callback)
+	-- 渐变消失帧数
+	if not nHideFrame then
+		nHideFrame = GLOBAL.GAME_FPS / 2
+	end
+	-- 隐藏延迟帧数
+	if not nDelayFrame then
+		nDelayFrame = GLOBAL.GAME_FPS * 5
+	end
+	-- switch case
+	if m_nState == STATE.SHOW then
+		-- get bg alpha
+		O.fAhBgAlpha = D.GetBgAlpha()
+	elseif m_nState == STATE.SHOWING then
+		return
+	elseif m_nState == STATE.HIDE then
+		-- return when chat panel is not visible
+		if callback then
+			Call(callback)
+		end
+		return
+	elseif m_nState == STATE.HIDDING then
+		-- unregister hide animate
+		LIB.BreatheCall('MY_AutoHideChat_Hide', false)
+	end
+	m_nState = STATE.HIDDING
 
-    -- get start alpha
-    local nStartAlpha = Station.Lookup('Lowest1/ChatTitleBG'):GetAlpha()
-    local nStartFrame = GetLogicFrameCount()
-    -- register animate breathe call
-    LIB.BreatheCall('MY_AutoHideChat_Hide', function()
-        local nFrame = GetLogicFrameCount()
-        if nFrame - nDelayFrame < nStartFrame then
-            _Cache.fAhBgAlpha = MY_AutoHideChat.GetBgAlpha()
-            return
-        end
-        -- calc new alpha
-        local nAlpha = math.max(math.ceil((1 - (nFrame - nDelayFrame - nStartFrame) / nHideFrame) * nStartAlpha), 0)
-        -- if panel setting panel is opened then delay again
-        local hPanelSettingFrame = Station.Lookup('Normal/ChatSettingPanel')
-        if hPanelSettingFrame and hPanelSettingFrame:IsVisible() then
-            nStartFrame = GetLogicFrameCount()
-            return
-        end
-        -- if mouse over chat panel then delay again
-        local hMouseOverWnd = Station.GetMouseOverWindow()
-        if hMouseOverWnd and hMouseOverWnd:GetRoot():GetName():sub(1, 9) == 'ChatPanel' then
-            nStartFrame = GetLogicFrameCount()
-            nAlpha = 255
-        end
-        -- alpha each panel
-        for i = 1, 10 do
-            local hFrame = Station.Lookup('Lowest2/ChatPanel' .. i)
-            if hFrame then
-                hFrame:SetAlpha(nAlpha)
-                hFrame:Lookup('Wnd_Message', 'Shadow_Back'):SetAlpha(nAlpha * _Cache.fAhBgAlpha)
-                -- hide if alpha turns to zero
-                if nAlpha == 0 then
-                    hFrame:SetMousePenetrable(true)
-                end
-            end
-        end
-        Station.Lookup('Lowest1/ChatTitleBG'):SetAlpha(nAlpha)
-        Station.Lookup('Lowest1/ChatTitleBG', 'Image_BG'):SetAlpha(nAlpha * _Cache.fAhBgAlpha)
-        if nAlpha == 0 then
-            m_nState = STATE.HIDE
-            if callback then
-                Call(callback)
-            end
-            return 0
-        end
-    end)
+	-- get start alpha
+	local nStartAlpha = Station.Lookup('Lowest1/ChatTitleBG'):GetAlpha()
+	local nStartFrame = GetLogicFrameCount()
+	-- register animate breathe call
+	LIB.BreatheCall('MY_AutoHideChat_Hide', function()
+		local nFrame = GetLogicFrameCount()
+		if nFrame - nDelayFrame < nStartFrame then
+			O.fAhBgAlpha = D.GetBgAlpha()
+			return
+		end
+		-- calc new alpha
+		local nAlpha = math.max(math.ceil((1 - (nFrame - nDelayFrame - nStartFrame) / nHideFrame) * nStartAlpha), 0)
+		-- if panel setting panel is opened then delay again
+		local hPanelSettingFrame = Station.Lookup('Normal/ChatSettingPanel')
+		if hPanelSettingFrame and hPanelSettingFrame:IsVisible() then
+			nStartFrame = GetLogicFrameCount()
+			return
+		end
+		-- if mouse over chat panel then delay again
+		local hMouseOverWnd = Station.GetMouseOverWindow()
+		if hMouseOverWnd and hMouseOverWnd:GetRoot():GetName():sub(1, 9) == 'ChatPanel' then
+			nStartFrame = GetLogicFrameCount()
+			nAlpha = 255
+		end
+		-- alpha each panel
+		for i = 1, 10 do
+			local hFrame = Station.Lookup('Lowest2/ChatPanel' .. i)
+			if hFrame then
+				hFrame:SetAlpha(nAlpha)
+				hFrame:Lookup('Wnd_Message', 'Shadow_Back'):SetAlpha(nAlpha * O.fAhBgAlpha)
+				-- hide if alpha turns to zero
+				if nAlpha == 0 then
+					hFrame:SetMousePenetrable(true)
+				end
+			end
+		end
+		Station.Lookup('Lowest1/ChatTitleBG'):SetAlpha(nAlpha)
+		Station.Lookup('Lowest1/ChatTitleBG', 'Image_BG'):SetAlpha(nAlpha * O.fAhBgAlpha)
+		if nAlpha == 0 then
+			m_nState = STATE.HIDE
+			if callback then
+				Call(callback)
+			end
+			return 0
+		end
+	end)
 end
 
 -- 初始化/生效 设置
-MY_AutoHideChat.ApplyConfig = function()
-    if MY_AutoHideChat.bAutoHideChatPanel then
-        -- get bg alpha
-        if not _Cache.fAhBgAlpha then
-            _Cache.fAhBgAlpha = Station.Lookup('Lowest2/ChatPanel1'):Lookup('Wnd_Message', 'Shadow_Back'):GetAlpha() / 255
-            _Cache.bAhAnimate = _Cache.bAhAnimate or false
-        end
-        -- hook chat panel as event listener
-        LIB.HookChatPanel('AFTER.MY_AutoHideChat', function(h)
-            -- if input box get focus then return
-            local focus = Station.GetFocusWindow()
-            if focus and focus:GetTreePath() == 'Lowest2/EditBox/Edit_Input/' then
-                return
-            end
-            -- show when new msg
-            MY_AutoHideChat.ShowChatPanel(GLOBAL.GAME_FPS / 4, 0, function()
-                -- hide after 5 sec
-                MY_AutoHideChat.HideChatPanel(GLOBAL.GAME_FPS / 2, GLOBAL.GAME_FPS * 5)
-            end)
-        end)
+function D.Apply()
+	if O.bEnable then
+		-- get bg alpha
+		if not O.fAhBgAlpha then
+			O.fAhBgAlpha = Station.Lookup('Lowest2/ChatPanel1'):Lookup('Wnd_Message', 'Shadow_Back'):GetAlpha() / 255
+			O.bAhAnimate = O.bAhAnimate or false
+		end
+		-- hook chat panel as event listener
+		LIB.HookChatPanel('AFTER.MY_AutoHideChat', function(h)
+			-- if input box get focus then return
+			local focus = Station.GetFocusWindow()
+			if focus and focus:GetTreePath() == 'Lowest2/EditBox/Edit_Input/' then
+				return
+			end
+			-- show when new msg
+			D.ShowChatPanel(GLOBAL.GAME_FPS / 4, 0, function()
+				-- hide after 5 sec
+				D.HideChatPanel(GLOBAL.GAME_FPS / 2, GLOBAL.GAME_FPS * 5)
+			end)
+		end)
 
-        -- hook chat edit box
-        local hEditInput = Station.Lookup('Lowest2/EditBox/Edit_Input')
-        -- save org
-        if hEditInput._MY_T_AHCP_OnSetFocus == nil then
-            hEditInput._MY_T_AHCP_OnSetFocus = hEditInput.OnSetFocus or false
-        end
-        -- show when chat panel get focus
-        hEditInput.OnSetFocus  = function()
-            MY_AutoHideChat.ShowChatPanel(GLOBAL.GAME_FPS / 4, 0)
-            if this._MY_T_AHCP_OnSetFocus then
-                this._MY_T_AHCP_OnSetFocus()
-            end
-        end
-        -- save org
-        if hEditInput._MY_T_AHCP_OnKillFocus == nil then
-            hEditInput._MY_T_AHCP_OnKillFocus = hEditInput.OnKillFocus or false
-        end
-        -- hide after input box lost focus for 5 sec
-        hEditInput.OnKillFocus = function()
-            MY_AutoHideChat.HideChatPanel(GLOBAL.GAME_FPS / 2, GLOBAL.GAME_FPS * 5)
-            if this._MY_T_AHCP_OnKillFocus then
-                this._MY_T_AHCP_OnKillFocus()
-            end
-        end
-    else
-        local hEditInput = Station.Lookup('Lowest2/EditBox/Edit_Input')
-        if hEditInput._MY_T_AHCP_OnSetFocus then
-            hEditInput.OnSetFocus = hEditInput._MY_T_AHCP_OnSetFocus
-        else
-            hEditInput.OnSetFocus = nil
-        end
-        hEditInput._MY_T_AHCP_OnSetFocus = nil
+		-- hook chat edit box
+		local hEditInput = Station.Lookup('Lowest2/EditBox/Edit_Input')
+		-- save org
+		if hEditInput._MY_T_AHCP_OnSetFocus == nil then
+			hEditInput._MY_T_AHCP_OnSetFocus = hEditInput.OnSetFocus or false
+		end
+		-- show when chat panel get focus
+		hEditInput.OnSetFocus = function()
+			D.ShowChatPanel(GLOBAL.GAME_FPS / 4, 0)
+			if this._MY_T_AHCP_OnSetFocus then
+				this._MY_T_AHCP_OnSetFocus()
+			end
+		end
+		-- save org
+		if hEditInput._MY_T_AHCP_OnKillFocus == nil then
+			hEditInput._MY_T_AHCP_OnKillFocus = hEditInput.OnKillFocus or false
+		end
+		-- hide after input box lost focus for 5 sec
+		hEditInput.OnKillFocus = function()
+			D.HideChatPanel(GLOBAL.GAME_FPS / 2, GLOBAL.GAME_FPS * 5)
+			if this._MY_T_AHCP_OnKillFocus then
+				this._MY_T_AHCP_OnKillFocus()
+			end
+		end
+	else
+		local hEditInput = Station.Lookup('Lowest2/EditBox/Edit_Input')
+		if hEditInput._MY_T_AHCP_OnSetFocus then
+			hEditInput.OnSetFocus = hEditInput._MY_T_AHCP_OnSetFocus
+		else
+			hEditInput.OnSetFocus = nil
+		end
+		hEditInput._MY_T_AHCP_OnSetFocus = nil
 
-        if hEditInput._MY_T_AHCP_OnKillFocus then
-            hEditInput.OnKillFocus = hEditInput._MY_T_AHCP_OnKillFocus
-        else
-            hEditInput.OnKillFocus = nil
-        end
-        hEditInput._MY_T_AHCP_OnKillFocus = nil
-        LIB.HookChatPanel('AFTER.MY_AutoHideChat', false)
+		if hEditInput._MY_T_AHCP_OnKillFocus then
+			hEditInput.OnKillFocus = hEditInput._MY_T_AHCP_OnKillFocus
+		else
+			hEditInput.OnKillFocus = nil
+		end
+		hEditInput._MY_T_AHCP_OnKillFocus = nil
+		LIB.HookChatPanel('AFTER.MY_AutoHideChat', false)
 
-        MY_AutoHideChat.ShowChatPanel()
-    end
+		D.ShowChatPanel()
+	end
 end
-LIB.RegisterInit('MY_AUTOHIDECHAT', MY_AutoHideChat.ApplyConfig)
+LIB.RegisterInit('MY_AutoHideChat', D.Apply)
 
-function MY_AutoHideChat.OnPanelActivePartial(ui, X, Y, W, H, x, y)
+function D.OnPanelActivePartial(ui, X, Y, W, H, x, y)
 	ui:append('WndCheckBox', {
 		x = x, y = y, w = 'auto',
 		text = _L['auto hide chat panel'],
-		checked = MY_AutoHideChat.bAutoHideChatPanel,
+		checked = MY_AutoHideChat.bEnable,
 		oncheck = function(bChecked)
-			MY_AutoHideChat.bAutoHideChatPanel = bChecked
-			MY_AutoHideChat.ApplyConfig()
+			MY_AutoHideChat.bEnable = bChecked
 		end,
 	})
 	y = y + 30
 	return x, y
+end
+
+-- Global exports
+do
+local settings = {
+	exports = {
+		{
+			fields = {
+				OnPanelActivePartial = D.OnPanelActivePartial,
+			},
+		},
+		{
+			fields = {
+				bEnable = true,
+			},
+			root = O,
+		},
+	},
+	imports = {
+		{
+			fields = {
+				bEnable = true,
+			},
+			triggers = {
+				bEnable = D.Apply,
+			},
+			root = O,
+		},
+	},
+}
+MY_AutoHideChat = LIB.GeneGlobalNS(settings)
 end
