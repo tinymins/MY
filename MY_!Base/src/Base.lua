9926959816d535f38f45e66f9c6064530ca61f22
Emil Zhai <root@derzh.com>
2019-09-12 14:10:46 +0800

    Release V141

--- MY_!Base/src/Base.lua
+++ MY_!Base/src/Base.lua
@@ -119,8 +119,8 @@ local GetClientTeam, UI_GetClientPlayerID = GetClientTeam, UI_GetClientPlayerID
 ---------------------------------------------------------------------------------------------
 -- 本地函数变量
 ---------------------------------------------------------------------------------------------
-local _BUILD_                = '20190904'
-local _VERSION_              = 0x2014000
+local _BUILD_                = '20190912'
+local _VERSION_              = 0x2014100
 local _MENU_COLOR_           = {255, 165, 79}
 local _MAX_PLAYER_LEVEL_     = 100
 local _INTERFACE_ROOT_       = './Interface/'
