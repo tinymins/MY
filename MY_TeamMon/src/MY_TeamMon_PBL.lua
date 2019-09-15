726528c7cf083985417b2c53c8bac4ad41c0c745
Emil Zhai <root@derzh.com>
2019-09-15 18:53:38 +0800

    修复团队监控气劲显示翻译问题

--- MY_TeamMon/src/MY_TeamMon_PBL.lua
+++ MY_TeamMon/src/MY_TeamMon_PBL.lua
@@ -37,7 +37,7 @@ local Call, XpCall, GetTraceback, RandomChild = LIB.Call, LIB.XpCall, LIB.GetTra
 local Get, Set, Clone, GetPatch, ApplyPatch = LIB.Get, LIB.Set, LIB.Clone, LIB.GetPatch, LIB.ApplyPatch
 local EncodeLUAData, DecodeLUAData, CONSTANT = LIB.EncodeLUAData, LIB.DecodeLUAData, LIB.CONSTANT
 -----------------------------------------------------------------------------------------------------------
-local PLUGIN_NAME = 'MY_TeamMon_PBL'
+local PLUGIN_NAME = 'MY_TeamMon'
 local PLUGIN_ROOT = PACKET_INFO.ROOT .. PLUGIN_NAME
 local MODULE_NAME = 'MY_TeamMon_PBL'
 local _L = LIB.LoadLangPack(PLUGIN_ROOT .. '/lang/')
