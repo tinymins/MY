4f788b8f9d95e4ef85ebdd945779540d4534cccc
Emil Zhai <root@derzh.com>
2019-12-26 17:50:03 +0800

    合并门派特殊选项逻辑

---
+++ MY_Toolbox/src/MY_Force.lua
@@ -0,0 +1,470 @@
+--------------------------------------------------------
+-- This file is part of the JX3 Mingyi Plugin.
+-- @link     : https://jx3.derzh.com/
+-- @desc     : 职业特色增强
+-- @author   : 茗伊 @双梦镇 @追风蹑影
+-- @modifier : Emil Zhai (root@derzh.com)
+-- @copyright: Copyright (c) 2013 EMZ Kingsoft Co., Ltd.
+--------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------
+-- these global functions are accessed all the time by the event handler
+-- so caching them is worth the effort
+-----------------------------------------------------------------------------------------------------------
+local setmetatable = setmetatable
+local ipairs, pairs, next, pcall, select = ipairs, pairs, next, pcall, select
+local sub, len, format, rep = string.sub, string.len, string.format, string.rep
+local find, byte, char, gsub = string.find, string.byte, string.char, string.gsub
+local type, tonumber, tostring = type, tonumber, tostring
+local HUGE, PI, random, abs = math.huge, math.pi, math.random, math.abs
+local min, max, floor, ceil = math.min, math.max, math.floor, math.ceil
+local pow, sqrt, sin, cos, tan, atan = math.pow, math.sqrt, math.sin, math.cos, math.tan, math.atan
+local insert, remove, concat, sort = table.insert, table.remove, table.concat, table.sort
+local pack, unpack = table.pack or function(...) return {...} end, table.unpack or unpack
+-- jx3 apis caching
+local wsub, wlen, wfind, wgsub = wstring.sub, wstring.len, wstring.find, StringReplaceW
+local GetTime, GetLogicFrameCount, GetCurrentTime = GetTime, GetLogicFrameCount, GetCurrentTime
+local GetClientTeam, UI_GetClientPlayerID = GetClientTeam, UI_GetClientPlayerID
+local GetClientPlayer, GetPlayer, GetNpc, IsPlayer = GetClientPlayer, GetPlayer, GetNpc, IsPlayer
+local LIB = MY
+local UI, DEBUG_LEVEL, PATH_TYPE, PACKET_INFO = LIB.UI, LIB.DEBUG_LEVEL, LIB.PATH_TYPE, LIB.PACKET_INFO
+local spairs, spairs_r, sipairs, sipairs_r = LIB.spairs, LIB.spairs_r, LIB.sipairs, LIB.sipairs_r
+local ipairs_r, count_c, pairs_c, ipairs_c = LIB.ipairs_r, LIB.count_c, LIB.pairs_c, LIB.ipairs_c
+local IsNil, IsBoolean, IsUserdata, IsFunction = LIB.IsNil, LIB.IsBoolean, LIB.IsUserdata, LIB.IsFunction
+local IsString, IsTable, IsArray, IsDictionary = LIB.IsString, LIB.IsTable, LIB.IsArray, LIB.IsDictionary
+local IsNumber, IsHugeNumber, IsEmpty, IsEquals = LIB.IsNumber, LIB.IsHugeNumber, LIB.IsEmpty, LIB.IsEquals
+local Call, XpCall, GetTraceback, RandomChild = LIB.Call, LIB.XpCall, LIB.GetTraceback, LIB.RandomChild
+local Get, Set, Clone, GetPatch, ApplyPatch = LIB.Get, LIB.Set, LIB.Clone, LIB.GetPatch, LIB.ApplyPatch
+local EncodeLUAData, DecodeLUAData, CONSTANT = LIB.EncodeLUAData, LIB.DecodeLUAData, LIB.CONSTANT
+-----------------------------------------------------------------------------------------------------------
+local PLUGIN_NAME = 'MY_Toolbox'
+local PLUGIN_ROOT = PACKET_INFO.ROOT .. PLUGIN_NAME
+local MODULE_NAME = 'MY_Force'
+local _L = LIB.LoadLangPack(PLUGIN_ROOT .. '/lang/')
+--------------------------------------------------------------------------
+if not LIB.AssertVersion(MODULE_NAME, _L[MODULE_NAME], 0x2013900) then
+	return
+end
+--------------------------------------------------------------------------
+
+local D = {}
+local O = {
+	-- 导出设置
+	bAlertPet      = true , -- 五毒宠物消失提醒
+	bMarkPet       = false, -- 五毒宠物标记
+	bFeedHorse     = true , -- 提示喂马
+	bWarningDebuff = false, -- 警告 debuff 类型
+	nDebuffNum     = 3    , -- debuff 类型达到几个时警告
+	bAlertWanted   = false, -- 在线被悬赏时提醒自己
+	-- 本地变量
+	nFrameXJ = 0, -- 献祭、各种召唤跟宠的帧数
+}
+RegisterCustomData('MY_Force.bAlertPet')
+RegisterCustomData('MY_Force.bMarkPet')
+RegisterCustomData('MY_Force.bFeedHorse')
+RegisterCustomData('MY_Force.bWarningDebuff')
+RegisterCustomData('MY_Force.nDebuffNum')
+RegisterCustomData('MY_Force.bAlertWanted')
+
+-- check pet of 5D （XJ：2226）
+function D.OnAlertPetChange(_, bAlertPet)
+	if bAlertPet then
+		LIB.RegisterEvent('NPC_LEAVE_SCENE.MY_Force__AlertPet', function()
+			local me = GetClientPlayer()
+			if me then
+				local pet = me.GetPet()
+				if pet and pet.dwID == arg0 and (GetLogicFrameCount() - O.nFrameXJ) >= 32 then
+					OutputWarningMessage('MSG_WARNING_YELLOW', _L('Your pet [%s] disappeared!',  pet.szName))
+					PlaySound(SOUND.UI_SOUND, g_sound.CloseAuction)
+				end
+			end
+		end)
+		LIB.RegisterEvent('DO_SKILL_CAST.MY_Force__AlertPet', function()
+			if arg0 == UI_GetClientPlayerID() then
+				-- 献祭、各种召唤：2965，2221 ~ 2226
+				if arg1 == 2965 or (arg1 >= 2221 and arg1 <= 2226) then
+					O.nFrameXJ = GetLogicFrameCount()
+				end
+			end
+		end)
+	else
+		LIB.RegisterEvent('NPC_LEAVE_SCENE.MY_Force__AlertPet', false)
+		LIB.RegisterEvent('DO_SKILL_CAST.MY_Force__AlertPet', false)
+	end
+end
+
+-- check to mark pet
+do
+local function UpdatePetMark(bMark)
+	local me = GetClientPlayer()
+	if not me then
+		return
+	end
+	local pet = me.GetPet()
+	if pet then
+		local dwEffect = 13
+		if not bMark then
+			dwEffect = 0
+		end
+		SceneObject_SetTitleEffect(TARGET.NPC, pet.dwID, dwEffect)
+	end
+end
+function D.OnMarkPetChange(_, bMarkPet)
+	if bMarkPet then
+		LIB.RegisterEvent({'NPC_ENTER_SCENE.MY_Force__MarkPet', 'NPC_DISPLAY_DATA_UPDATE.MY_Force__MarkPet'}, function()
+			local pet = GetClientPlayer().GetPet()
+			if pet and arg0 == pet.dwID then
+				LIB.DelayCall(500, function()
+					UpdatePetMark(true)
+				end)
+			else
+				local npc = GetNpc(arg0)
+				if npc.dwTemplateID == 46297 and npc.dwEmployer == UI_GetClientPlayerID() then
+					SceneObject_SetTitleEffect(TARGET.NPC, npc.dwID, 13)
+				end
+			end
+		end)
+	else
+		LIB.RegisterEvent({'NPC_ENTER_SCENE.MY_Force__MarkPet', 'NPC_DISPLAY_DATA_UPDATE.MY_Force__MarkPet'}, false)
+	end
+	UpdatePetMark(bMarkPet)
+end
+end
+
+-- check feed horse
+function D.OnFeedHorseChange(_, bFeedHorse)
+	if bFeedHorse then
+		LIB.RegisterEvent('SYS_MSG.MY_Force__FeedHorse', function()
+			local me = GetClientPlayer()
+			-- 读条技能
+			if arg0 == 'UI_OME_SKILL_CAST_LOG' then
+				-- on prepare 骑乘
+				if O.bFeedHorse and arg1 == me.dwID and (arg2 == 433 or arg2 == 53 or Table_GetSkillName(arg2, 1) == Table_GetSkillName(53, 1)) then
+					local it = me.GetItem(INVENTORY_INDEX.EQUIP, EQUIPMENT_INVENTORY.HORSE)
+					if it then
+						OutputItemTip(UI_OBJECT_ITEM, INVENTORY_INDEX.EQUIP, EQUIPMENT_INVENTORY.HORSE)
+						local hM = Station.Lookup('Topmost1/TipPanel_Normal', 'Handle_Message')
+						for i = 0, hM:GetItemCount() - 1, 1 do
+							local hT = hM:Lookup(i)
+							if hT:GetType() == 'Text' and hT:GetFontScheme() == 164 then
+								local szFullMeasure = LIB.TrimString(hT:GetText())
+								local tDisplay = g_tTable.RideSubDisplay:Search(it.nDetail)
+								if tDisplay and szFullMeasure ~= tDisplay.szFullMeasure3 then
+									OutputWarningMessage('MSG_WARNING_YELLOW', Table_GetItemName(it.nUiId) .. ': ' .. szFullMeasure)
+									PlaySound(SOUND.UI_SOUND, g_sound.CloseAuction)
+								end
+								break
+							end
+						end
+						HideTip(false)
+					end
+				end
+			end
+		end)
+	else
+		LIB.RegisterEvent('SYS_MSG.MY_Force__FeedHorse', false)
+	end
+end
+
+-- check warning buff type
+function D.OnWarningDebuffChange(_, bWarningDebuff)
+	if bWarningDebuff then
+		LIB.RegisterEvent('BUFF_UPDATE.MY_Force__WarningDebuff', function()
+			-- buff update：
+			-- arg0：dwPlayerID，arg1：bDelete，arg2：nIndex，arg3：bCanCancel
+			-- arg4：dwBuffID，arg5：nStackNum，arg6：nEndFrame，arg7：？update all?
+			-- arg8：nLevel，arg9：dwSkillSrcID
+			local me = GetClientPlayer()
+			if arg0 ~= me.dwID or not O.bWarningDebuff or (not arg7 and arg3) then
+				return
+			end
+			local t, t2 = {}, {}
+			local aBuff, nCount = LIB.GetBuffList(me)
+			for i = 1, nCount do
+				local buff = aBuff[i]
+				if not buff.bCanCancel and not t2[buff.dwID] then
+					local info = GetBuffInfo(buff.dwID, buff.nLevel, {})
+					if info and info.nDetachType > 2 then
+						if not t[info.nDetachType] then
+							t[info.nDetachType] = 1
+						else
+							t[info.nDetachType] = t[info.nDetachType] + 1
+						end
+						t2[buff.dwID] = true
+					end
+				end
+			end
+			for nType, nNum in pairs(t) do
+				if nNum >= O.nDebuffNum then
+					local szText = _L('Your debuff of type [%s] reached [%d]', g_tStrings.tBuffDetachType[nType], nNum)
+					OutputWarningMessage('MSG_WARNING_GREEN', szText)
+					PlaySound(SOUND.UI_SOUND, g_sound.CloseAuction)
+				end
+			end
+		end)
+	else
+		LIB.RegisterEvent('BUFF_UPDATE.MY_Force__WarningDebuff', false)
+	end
+end
+
+-- check on wanted msg
+do
+local function OnMsgAnnounce(szMsg)
+	local _, _, sM, sN = string.find(szMsg, _L['Now somebody pay (%d+) gold to buy life of (.-)'])
+	if sM and sN == GetClientPlayer().szName then
+		local fW = function()
+			OutputWarningMessage('MSG_WARNING_RED', _L('Congratulations, you offered a reward [%s] gold!', sM))
+			PlaySound(SOUND.UI_SOUND, g_sound.CloseAuction)
+		end
+		SceneObject_SetTitleEffect(TARGET.PLAYER, UI_GetClientPlayerID(), 47)
+		fW()
+		LIB.DelayCall(2000, fW)
+		LIB.DelayCall(4000, fW)
+		O.bHasWanted = true
+	end
+end
+function D.OnAlertWantedChange(_, bAlertWanted)
+	if bAlertWanted then
+		-- 变化时更新头顶效果
+		LIB.RegisterEvent('PLAYER_STATE_UPDATE.MY_Force__AlertWanted', function()
+			if arg0 == UI_GetClientPlayerID() then
+				if O.bHasWanted then
+					SceneObject_SetTitleEffect(TARGET.PLAYER, arg0, 47)
+				end
+			end
+		end)
+		-- 重伤后删除头顶效果
+		LIB.RegisterEvent('SYS_MSG.MY_Force__AlertWanted', function()
+			if arg0 == 'UI_OME_DEATH_NOTIFY' then
+				if O.bHasWanted and arg1 == UI_GetClientPlayerID() then
+					O.bHasWanted = nil
+					SceneObject_SetTitleEffect(TARGET.PLAYER, arg1, 0)
+				end
+			end
+		end)
+		RegisterMsgMonitor(OnMsgAnnounce, {'MSG_GM_ANNOUNCE'})
+	else
+		LIB.RegisterEvent('PLAYER_STATE_UPDATE.MY_Force__AlertWanted', false)
+		LIB.RegisterEvent('SYS_MSG.MY_Force__AlertWanted', false)
+		UnRegisterMsgMonitor(OnMsgAnnounce, {'MSG_GM_ANNOUNCE'})
+	end
+end
+end
+
+LIB.RegisterEvent('LOADING_END.MY_Force', function()
+	local buff = Table_GetBuff(374, 1)
+	if buff then
+		buff.bShowTime = 1
+	end
+end)
+
+-------------------------------------
+-- 全局导出接口
+-------------------------------------
+do
+local settings = {
+	exports = {
+		{
+			fields = {
+				bAlertPet      = true,
+				bMarkPet       = true,
+				bFeedHorse     = true,
+				bWarningDebuff = true,
+				nDebuffNum     = true,
+				bAlertWanted   = true,
+			},
+			root = O,
+		},
+	},
+	imports = {
+		{
+			fields = {
+				bAlertPet      = true,
+				bMarkPet       = true,
+				bFeedHorse     = true,
+				bWarningDebuff = true,
+				nDebuffNum     = true,
+				bAlertWanted   = true,
+			},
+			triggers = {
+				bAlertPet      = D.OnAlertPetChange,
+				bMarkPet       = D.OnMarkPetChange,
+				bFeedHorse     = D.OnFeedHorseChange,
+				bWarningDebuff = D.OnWarningDebuffChange,
+				bAlertWanted   = D.OnAlertWantedChange,
+			},
+			root = O,
+		},
+	},
+}
+MY_Force = LIB.GeneGlobalNS(settings)
+end
+
+-------------------------------------
+-- 设置界面
+-------------------------------------
+local PS = {}
+function PS.OnPanelActive(frame)
+	local ui = UI(frame)
+	local X, Y = 10, 10
+	local x, y = X, Y
+	local w, h = ui:Size()
+	-- wu du
+	---------------
+	ui:Append('Text', { text = g_tStrings.tForceTitle[CONSTANT.FORCE_TYPE.WU_DU], x = x, y = y, font = 27 })
+	-- crlf
+	x = X + 10
+	y = y + 28
+	-- disappear
+	x = ui:Append('WndCheckBox', {
+		x = x, y = y,
+		text = _L['Alert when pet disappear unexpectedly (for 5D)'],
+		checked = MY_Force.bAlertPet,
+		oncheck = function(bChecked)
+			MY_Force.bAlertPet = bChecked
+		end,
+	}):AutoWidth():Pos('BOTTOMRIGHT') + 10
+	-- mark pet
+	ui:Append('WndCheckBox', {
+		x = x, y = y,
+		text = _L['Mark pet'],
+		checked = MY_Force.bMarkPet,
+		oncheck = function(bChecked)
+			MY_Force.bMarkPet = bChecked
+		end,
+	}):AutoWidth()
+	-- crlf
+	x = X + 10
+	y = y + 28
+	-- guding
+	x = ui:Append('WndCheckBox', {
+		x = x, y = y,
+		text = _L['Display GUDING of teammate, change color'],
+		checked = MY_ForceGuding.bEnable,
+		oncheck = function(bChecked)
+			MY_ForceGuding.bEnable = bChecked
+		end,
+	}):AutoWidth():Pos('BOTTOMRIGHT') + 2
+	x = ui:Append('Shadow', {
+		x = x, y = y + 2, w = 18, h = 18,
+		color = MY_ForceGuding.color,
+		onclick = function()
+			local ui = UI(this)
+			OpenColorTablePanel(function(r, g, b)
+				ui:Color(r, g, b)
+				MY_ForceGuding.color = { r, g, b }
+			end)
+		end,
+	}):Pos('BOTTOMRIGHT') + 10
+	ui:Append('WndCheckBox', {
+		x = x, y = y,
+		text = _L['Auto talk in team channel after puting GUDING'],
+		checked = MY_ForceGuding.bAutoSay,
+		autoenable = function() return MY_ForceGuding.bEnable end,
+		oncheck = function(bChecked)
+			MY_ForceGuding.bAutoSay = bChecked
+		end,
+	})
+	x = X + 10
+	y = y + 28
+	ui:Append('WndEditBox', {
+		x = x, y = y, w = w - x * 2, h = 50,
+		multiline = true, limit = 512,
+		text = MY_ForceGuding.szSay,
+		autoenable = function() return MY_ForceGuding.bAutoSay end,
+		onchange = function(szText)
+			MY_ForceGuding.szSay = szText
+		end,
+	})
+	if not LIB.IsShieldedVersion() then
+		-- crlf
+		x = X + 10
+		y = y + 54
+		x = ui:Append('WndCheckBox', {
+			x = x, y = y,
+			checked = MY_ForceGuding.bUseMana,
+			text = _L['Automatic eat GUDING when mana below '],
+			oncheck = function(bChecked)
+				MY_ForceGuding.bUseMana = bChecked
+			end,
+		}):AutoWidth():Pos('BOTTOMRIGHT') + 5
+		x = ui:Append('WndTrackbar', {
+			x = x, y = y, w = 70, h = 25,
+			range = {0, 100, 50},
+			value = MY_ForceGuding.nManaMp,
+			onchange = function(nVal) MY_ForceGuding.nManaMp = nVal end,
+			autoenable = function() return MY_ForceGuding.bUseMana end,
+		}):Pos('BOTTOMRIGHT') + 65
+		x = ui:Append('Text', {
+			x = x, y = y - 3,
+			text = _L[', or life below '],
+		}):AutoWidth():Pos('BOTTOMRIGHT') + 5
+		x = ui:Append('WndTrackbar', {
+			x = x, y = y, w = 70, h = 25,
+			range = {0, 100, 50},
+			value = MY_ForceGuding.nManaHp,
+			onchange = function(nVal) MY_ForceGuding.nManaHp = nVal end,
+			autoenable = function() return MY_ForceGuding.bUseMana end,
+		}):Pos('BOTTOMRIGHT')
+	end
+	-- crlf
+	y = y + 36
+	-- other
+	---------------
+	x = X
+	ui:Append('Text', { text = _L['Others'], x = x, y = y, font = 27 })
+	-- crlf
+	x = X + 10
+	y = y + 28
+	-- hungry
+	x = ui:Append('WndCheckBox', {
+		x = x, y = y,
+		text = _L['Alert when horse is hungry'], checked = MY_Force.bFeedHorse,
+		oncheck = function(bChecked)
+			MY_Force.bFeedHorse = bChecked
+		end,
+	}):AutoWidth():Pos('BOTTOMRIGHT') + 10
+	-- crlf
+	x = X + 10
+	y = y + 28
+	-- be wanted alert
+	ui:Append('WndCheckBox', {
+		x = x, y = y,
+		text = _L['Alert when I am wanted publishing online'],
+		checked = MY_Force.bAlertWanted,
+		oncheck = function(bChecked)
+			MY_Force.bAlertWanted = bChecked
+		end,
+	})
+	-- crlf
+	x = X + 10
+	y = y + 28
+	-- debuff type num
+	x = ui:Append('WndCheckBox', {
+		x = x, y = y,
+		text = _L['Alert when my same type of debuff reached a certain number '],
+		checked = MY_Force.bWarningDebuff,
+		oncheck = function(bChecked)
+			MY_Force.bWarningDebuff = bChecked
+		end,
+	}):AutoWidth():Pos('BOTTOMRIGHT') + 10
+	ui:Append('WndComboBox', {
+		x = x, y = y, w = 50, h = 25,
+		autoenable = function() return MY_Force.bWarningDebuff end,
+		text = tostring(MY_Force.nDebuffNum),
+		menu = function()
+			local ui = UI(this)
+			local m0 = {}
+			for i = 1, 10 do
+				insert(m0, {
+					szOption = tostring(i),
+					fnAction = function()
+						MY_Force.nDebuffNum = i
+						ui:Text(tostring(i))
+					end,
+				})
+			end
+			return m0
+		end,
+	})
+end
+LIB.RegisterPanel('MY_Force', _L['MY_Force'], _L['Target'], 327, PS)
---
+++ MY_Toolbox/src/MY_ForceGuding.lua
@@ -0,0 +1,305 @@
+--------------------------------------------------------
+-- This file is part of the JX3 Mingyi Plugin.
+-- @link     : https://jx3.derzh.com/
+-- @desc     : 五毒仙王蛊鼎显示
+-- @author   : 茗伊 @双梦镇 @追风蹑影
+-- @modifier : Emil Zhai (root@derzh.com)
+-- @copyright: Copyright (c) 2013 EMZ Kingsoft Co., Ltd.
+--------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------
+-- these global functions are accessed all the time by the event handler
+-- so caching them is worth the effort
+-----------------------------------------------------------------------------------------------------------
+local setmetatable = setmetatable
+local ipairs, pairs, next, pcall, select = ipairs, pairs, next, pcall, select
+local sub, len, format, rep = string.sub, string.len, string.format, string.rep
+local find, byte, char, gsub = string.find, string.byte, string.char, string.gsub
+local type, tonumber, tostring = type, tonumber, tostring
+local HUGE, PI, random, abs = math.huge, math.pi, math.random, math.abs
+local min, max, floor, ceil = math.min, math.max, math.floor, math.ceil
+local pow, sqrt, sin, cos, tan, atan = math.pow, math.sqrt, math.sin, math.cos, math.tan, math.atan
+local insert, remove, concat, sort = table.insert, table.remove, table.concat, table.sort
+local pack, unpack = table.pack or function(...) return {...} end, table.unpack or unpack
+-- jx3 apis caching
+local wsub, wlen, wfind, wgsub = wstring.sub, wstring.len, wstring.find, StringReplaceW
+local GetTime, GetLogicFrameCount, GetCurrentTime = GetTime, GetLogicFrameCount, GetCurrentTime
+local GetClientTeam, UI_GetClientPlayerID = GetClientTeam, UI_GetClientPlayerID
+local GetClientPlayer, GetPlayer, GetNpc, IsPlayer = GetClientPlayer, GetPlayer, GetNpc, IsPlayer
+local LIB = MY
+local UI, DEBUG_LEVEL, PATH_TYPE, PACKET_INFO = LIB.UI, LIB.DEBUG_LEVEL, LIB.PATH_TYPE, LIB.PACKET_INFO
+local spairs, spairs_r, sipairs, sipairs_r = LIB.spairs, LIB.spairs_r, LIB.sipairs, LIB.sipairs_r
+local ipairs_r, count_c, pairs_c, ipairs_c = LIB.ipairs_r, LIB.count_c, LIB.pairs_c, LIB.ipairs_c
+local IsNil, IsBoolean, IsUserdata, IsFunction = LIB.IsNil, LIB.IsBoolean, LIB.IsUserdata, LIB.IsFunction
+local IsString, IsTable, IsArray, IsDictionary = LIB.IsString, LIB.IsTable, LIB.IsArray, LIB.IsDictionary
+local IsNumber, IsHugeNumber, IsEmpty, IsEquals = LIB.IsNumber, LIB.IsHugeNumber, LIB.IsEmpty, LIB.IsEquals
+local Call, XpCall, GetTraceback, RandomChild = LIB.Call, LIB.XpCall, LIB.GetTraceback, LIB.RandomChild
+local Get, Set, Clone, GetPatch, ApplyPatch = LIB.Get, LIB.Set, LIB.Clone, LIB.GetPatch, LIB.ApplyPatch
+local EncodeLUAData, DecodeLUAData, CONSTANT = LIB.EncodeLUAData, LIB.DecodeLUAData, LIB.CONSTANT
+-----------------------------------------------------------------------------------------------------------
+local PLUGIN_NAME = 'MY_Toolbox'
+local PLUGIN_ROOT = PACKET_INFO.ROOT .. PLUGIN_NAME
+local MODULE_NAME = 'MY_Force'
+local _L = LIB.LoadLangPack(PLUGIN_ROOT .. '/lang/')
+--------------------------------------------------------------------------
+if not LIB.AssertVersion(MODULE_NAME, _L[MODULE_NAME], 0x2013900) then
+	return
+end
+--------------------------------------------------------------------------
+
+local D = {}
+local O = {
+	-- 导出设置
+	bEnable = true, -- 总开关
+	bAutoSay = false, -- 摆鼎后自动说话
+	szSay = _L['I have put the GUDING, hurry to eat if you lack of mana. *la la la*'],
+	color = { 255, 0, 128 }, -- 名称颜色，默认绿色
+	bUseMana = true, -- 路过时自动吃毒锅
+	nManaMp = 80, -- 自动吃的 MP 百分比
+	nManaHp = 80, -- 自动吃的 HP 百分比
+	-- 本地变量
+	nMaxDelay = 500, -- 释放和出现的最大时差，单位毫秒
+	nMaxTime = 60000, -- 存在的最大时间，单位毫秒
+	dwSkillID = 2234,
+	dwTemplateID = 2418,
+	tList = {}, -- 显示记录 (#ID => nTime)
+	tCast = {}, -- 技能释放记录
+	nFrame = 0, -- 上次自动吃鼎、绘制帧次
+}
+RegisterCustomData('MY_ForceGuding.bEnable')
+RegisterCustomData('MY_ForceGuding.bAutoSay')
+RegisterCustomData('MY_ForceGuding.szSay')
+RegisterCustomData('MY_ForceGuding.color')
+RegisterCustomData('MY_ForceGuding.bUseMana')
+RegisterCustomData('MY_ForceGuding.nManaMp')
+RegisterCustomData('MY_ForceGuding.nManaHp')
+
+--[[#DEBUG BEGIN]]
+-- debug
+function D.Debug(szMsg)
+	LIB.Debug(szMsg, _L['MY_ForceGuding'], DEBUG_LEVEL.LOG)
+end
+--[[#DEBUG END]]
+
+-- add to list
+function D.AddToList(tar, dwCaster, dwTime, szEvent)
+	O.tList[tar.dwID] = { dwCaster = dwCaster, dwTime = dwTime }
+	-- bg notify
+	local me = GetClientPlayer()
+	if szEvent == 'DO_SKILL_CAST' and me.IsInParty() then
+		LIB.SendBgMsg(PLAYER_TALK_CHANNEL.RAID, 'MY_GUDING_NOTIFY', tar.dwID, dwCaster)
+	end
+	if O.bAutoSay and me.dwID == dwCaster then
+		local nChannel = PLAYER_TALK_CHANNEL.RAID
+		if not me.IsInParty() then
+			nChannel = PLAYER_TALK_CHANNEL.NEARBY
+		end
+		LIB.Talk(nChannel, O.szSay)
+	end
+end
+
+-- remove record
+function D.RemoveFromList(dwID)
+	O.tList[dwID] = nil
+end
+
+-------------------------------------
+-- 事件处理函数
+-------------------------------------
+-- skill cast log
+function D.OnSkillCast(dwCaster, dwSkillID, dwLevel, szEvent)
+	local player = GetPlayer(dwCaster)
+	if player and dwSkillID == O.dwSkillID and (dwCaster == UI_GetClientPlayerID() or LIB.IsParty(dwCaster)) then
+		insert(O.tCast, { dwCaster = dwCaster, dwTime = GetTime(), szEvent = szEvent })
+		--[[#DEBUG BEGIN]]
+		D.Debug('[' .. player.szName .. '] cast [' .. LIB.GetSkillName(dwSkillID, dwLevel) .. '#' .. szEvent .. ']')
+		--[[#DEBUG END]]
+	end
+end
+
+-- doodad enter
+function D.OnDoodadEnter()
+	local tar = GetDoodad(arg0)
+	if not tar or O.tList[arg0] or tar.dwTemplateID ~= O.dwTemplateID then
+		return
+	end
+	--[[#DEBUG BEGIN]]
+	D.Debug('[' .. tar.szName .. '] enter scene')
+	--[[#DEBUG END]]
+	-- find caster
+	for k, v in ipairs(O.tCast) do
+		local nTime = GetTime() - v.dwTime
+		--[[#DEBUG BEGIN]]
+		D.Debug('checking [#' .. v.dwCaster .. '], delay [' .. nTime .. ']')
+		--[[#DEBUG END]]
+		if nTime < O.nMaxDelay then
+			remove(O.tCast, k)
+			D.AddToList(tar, v.dwCaster, v.dwTime, v.szEvent)
+			--[[#DEBUG BEGIN]]
+			D.Debug('matched [' .. tar.szName .. '] casted by [#' .. v.dwCaster .. ']')
+			--[[#DEBUG END]]
+			return
+		end
+	end
+	-- purge
+	for k, v in pairs(O.tCast) do
+		if (GetTime() - v.dwTime) > O.nMaxDelay then
+			remove(O.tCast, k)
+		end
+	end
+end
+
+-- notify
+function D.OnSkillNotify(_, nChannel, dwTalkerID, szTalkerName, bSelf, ...)
+	if not bSelf then
+		local data = {...}
+		local dwID = tonumber(data[1])
+		if not O.tList[dwID] then
+			O.tList[dwID] = { dwCaster = tonumber(data[2]), dwTime = GetTime() }
+			--[[#DEBUG BEGIN]]
+			D.Debug('received notify from [#' .. data[2] .. ']')
+			--[[#DEBUG END]]
+		end
+	end
+end
+
+function D.OnEnableChange(_, bEnable)
+	local h = UI.GetShadowHandle('MY_ForceGuding')
+	h:Clear()
+	if bEnable then
+		h:AppendItemFromString('<shadow>name="Shadow_Label"</shadow>')
+		O.pLabel = h:Lookup('Shadow_Label')
+		LIB.RegisterEvent('SYS_MSG.MY_ForceGuding', function()
+			if arg0 == 'UI_OME_SKILL_HIT_LOG' then
+				D.OnSkillCast(arg1, arg4, arg5, arg0)
+			elseif arg0 == 'UI_OME_SKILL_EFFECT_LOG' then
+				D.OnSkillCast(arg1, arg5, arg6, arg0)
+			end
+		end)
+		LIB.RegisterEvent('DO_SKILL_CAST.MY_ForceGuding', function(event)
+			D.OnSkillCast(arg0, arg1, arg2, event)
+		end)
+		LIB.RegisterEvent('DOODAD_ENTER_SCENE.MY_ForceGuding', function()
+			D.OnDoodadEnter()
+		end)
+		LIB.RegisterBgMsg('MY_GUDING_NOTIFY.MY_ForceGuding', D.OnSkillNotify)
+		LIB.BreatheCall('MY_ForceGuding', function()
+			-- skip frame
+			local nFrame = GetLogicFrameCount()
+			if nFrame >= O.nFrame and (nFrame - O.nFrame) < 8 then
+				return
+			end
+			O.nFrame = nFrame
+			-- check empty
+			local sha, me = O.pLabel, GetClientPlayer()
+			if not me or not MY_ForceGuding.bEnable or IsEmpty(O.tList) then
+				return sha:Hide()
+			end
+			-- color, alpha
+			local r, g, b = unpack(MY_ForceGuding.color)
+			local a = 200
+			local buff = LIB.GetBuff(me, 3488)
+			if buff and not buff.bCanCancel then
+				a = 120
+			end
+			-- shadow text
+			sha:SetTriangleFan(GEOMETRY_TYPE.TEXT)
+			sha:ClearTriangleFanPoint()
+			sha:Show()
+			for k, v in pairs(O.tList) do
+				local nLeft = v.dwTime + O.nMaxTime - GetTime()
+				if nLeft < 0 then
+					D.RemoveFromList(k)
+				else
+					local tar = GetDoodad(k)
+					if tar then
+						--  show name
+						local szText = _L['-'] .. math.floor(nLeft / 1000)
+						local player = GetPlayer(v.dwCaster)
+						if player then
+							szText = player.szName .. szText
+						else
+							szText = tar.szName .. szText
+						end
+						sha:AppendDoodadID(tar.dwID, r, g, b, a, 192, 199, szText, 0, 1)
+					end
+				end
+			end
+		end)
+	else
+		LIB.RegisterEvent('SYS_MSG.MY_ForceGuding', false)
+		LIB.RegisterEvent('DO_SKILL_CAST.MY_ForceGuding', false)
+		LIB.RegisterEvent('DOODAD_ENTER_SCENE.MY_ForceGuding', false)
+		LIB.RegisterBgMsg('MY_GUDING_NOTIFY.MY_ForceGuding', false)
+		LIB.BreatheCall('MY_ForceGuding', false)
+	end
+end
+
+function D.OnUseManaChange(_, bUseMana)
+	if bUseMana then
+		LIB.BreatheCall('MY_ForceGuding__UseMana', function()
+			local nFrame = GetLogicFrameCount()
+			-- check to use mana
+			if O.bUseMana and (not O.nManaFrame or O.nManaFrame <= (nFrame - 4)) then
+				local aList = O.tList
+				if not IsEmpty(aList) then
+					local me = GetClientPlayer()
+					local buff = LIB.GetBuff(me, 3448)
+					if (not buff or buff.bCanCancel)
+						and not me.bOnHorse and me.nMoveState == MOVE_STATE.ON_STAND and me.GetOTActionState() == 0
+						and  ((me.nCurrentMana / me.nMaxMana) <= (O.nManaMp / 100)
+							or (me.nCurrentLife / me.nMaxLife) <= (O.nManaHp / 100))
+					then
+						for k, _ in pairs(aList) do
+							local doo = GetDoodad(k)
+							if doo and LIB.GetDistance(doo) < 6 then
+								O.nManaFrame = GetLogicFrameCount()
+								InteractDoodad(doo.dwID)
+								LIB.Sysmsg("自动吃仙王蛊鼎")
+								break
+							end
+						end
+					end
+				end
+			end
+		end)
+	else
+		LIB.BreatheCall('MY_ForceGuding__UseMana', false)
+	end
+end
+
+-------------------------------------
+-- 全局导出接口
+-------------------------------------
+do
+local settings = {
+	exports = {
+		{
+			fields = {
+				bEnable  = true,
+				bAutoSay = true,
+				szSay    = true,
+				color    = true,
+				bUseMana = true,
+			},
+			root = O,
+		},
+	},
+	imports = {
+		{
+			fields = {
+				bEnable  = true,
+				bAutoSay = true,
+				szSay    = true,
+				color    = true,
+				bUseMana = true,
+			},
+			triggers = {
+				bEnable  = D.OnEnableChange,
+				bUseMana = D.OnUseManaChange,
+			},
+			root = O,
+		},
+	},
+}
+MY_ForceGuding = LIB.GeneGlobalNS(settings)
+end
