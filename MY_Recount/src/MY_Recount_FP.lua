ea462eb78b32acb2aa88392d0d366e72ebbca09d
Emil Zhai <root@derzh.com>
2020-02-29 04:49:53 +0800

    战斗复盘界面增加标题显示

--- MY_Recount/src/MY_Recount_FP.lua
+++ MY_Recount/src/MY_Recount_FP.lua
@@ -394,6 +394,7 @@ function D.DrawData(frame)
 	handle:FormatAllItemPos()
 	handle:SetSizeByAllItemSize()
 	hOuter:FormatAllItemPos()
+	frame:Lookup('', 'Text_Title'):SetText(_L['MY_Recount_FP'] .. ' - ' .. data[DK.BOSSNAME])
 	frame:Lookup('Wnd_Total/Wnd_Index', 'Handle_IndexCount/Text_IndexCount'):SprintfText(_L['Total %d pages'], nPageCount)
 end
 
@@ -480,7 +481,6 @@ end
 function MY_Recount_FP.OnFrameCreate()
 	this.szSortKey = 'time'
 	this.szSortOrder = 'asc'
-	this:Lookup('', 'Text_Title'):SetText(_L['MY_Recount_FP'])
 	this:Lookup('Wnd_Total/Wnd_Search/Edit_Search'):SetPlaceholderText(_L['Press enter to search ...'])
 
 	local handle = this:Lookup('Wnd_Total/Wnd_Index', 'Handle_IndexesOuter/Handle_Indexes')
