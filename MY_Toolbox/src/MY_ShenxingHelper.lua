6ffadcbdc3b0e07e979170c3c43892d386a10623
Emil Zhai <root@derzh.com>
2020-04-03 16:27:52 +0800

    重构神行助手取消每帧检查的写法降低开销

--- MY_Toolbox/src/MY_ShenxingHelper.lua
+++ MY_Toolbox/src/MY_ShenxingHelper.lua
@@ -57,22 +57,21 @@ RegisterCustomData('MY_ShenxingHelper.bAncientMap')
 RegisterCustomData('MY_ShenxingHelper.bOpenAllMap')
 RegisterCustomData('MY_ShenxingHelper.bAvoidBlackCD')
 
--- 【台服用】老地图神行
-do
-local tNonwarData = {
-	{ id =  8, x =   70, y =   5 }, -- 洛阳
-	{ id = 11, x =   15, y = -90 }, -- 天策
-	{ id = 12, x = -150, y = 110 }, -- 枫华
-	{ id = 15, x = -450, y =  65 }, -- 长安
-	{ id = 26, x =  -20, y =  90 }, -- 荻花宫
-	{ id = 32, x =   50, y =  45 }, -- 小战宝
+local NONWAR_DATA = {
+	{ id =  8, x =   70, y =    5 }, -- 洛阳
+	{ id = 11, x =  170, y = -160 }, -- 天策
+	{ id = 12, x = -150, y =  110 }, -- 枫华
+	{ id = 15, x = -450, y =   65 }, -- 长安
+	{ id = 26, x =  -20, y =   90 }, -- 荻花宫
+	{ id = 32, x =   50, y =   45 }, -- 小战宝
 }
-local function drawNonwarMap()
-	if LIB.IsShieldedVersion('MY_ShenxingHelper') then
-		return
-	end
+
+--------------------------------------------------------------------------
+-- 【台服用】老地图神行
+--------------------------------------------------------------------------
+function D.HookNonwarMap()
 	local h = Station.Lookup('Topmost1/WorldMap/Wnd_All', 'Handle_CopyBtn')
-	if not h or h.__MY_NonwarData or not h:IsVisible() then
+	if not h or h.__MY_NonwarData then
 		return
 	end
 	local me = GetClientPlayer()
@@ -84,7 +83,7 @@ local function drawNonwarMap()
 		if m and m.mapid == 160 then
 			local _w, _ = m:GetSize()
 			local fS = m.w / _w
-			for _, v in ipairs(tNonwarData) do
+			for _, v in ipairs(NONWAR_DATA) do
 				local bOpen = me.GetMapVisitFlag(v.id)
 				local szFile, nFrame = 'ui/Image/MiddleMap/MapWindow.UITex', 41
 				if bOpen then
@@ -92,6 +91,7 @@ local function drawNonwarMap()
 				end
 				h:AppendItemFromString('<image>name="mynw_' .. v.id .. '" path='..EncodeComponentsString(szFile)..' frame='..nFrame..' eventid=341</image>')
 				local img = h:Lookup(h:GetItemCount() - 1)
+				img.bMYNonwar = true
 				img.bEnable = bOpen
 				img.bSelect = bOpen and v.id ~= 26 and v.id ~= 32
 				img.x = m.x + v.x
@@ -103,6 +103,15 @@ local function drawNonwarMap()
 				img.city = img.name
 				img.button = m.button
 				img.copy = true
+				img.OnItemMouseEnter = function()
+					img:SetAlpha(255)
+					return LIB.FORMAT_WMSG_RET(true, true)
+				end
+				img.OnItemMouseLeave = function()
+					img:SetAlpha(200)
+					return LIB.FORMAT_WMSG_RET(true, true)
+				end
+				img:SetAlpha(200)
 				img:SetSize(img.w / fS, img.h / fS)
 				img:SetRelPos(img.x / fS - (img.w / fS / 2), img.y / fS - (img.h / fS / 2))
 			end
@@ -112,30 +121,51 @@ local function drawNonwarMap()
 	end
 	h.__MY_NonwarData = true
 end
-LIB.BreatheCall('MY_Toolbox#NonwarData', 130, drawNonwarMap)
-end
 
--- 【台服用】强开所有地图
-do
-local h, hList, hItem
-local function openAllMap()
-	if LIB.IsShieldedVersion('MY_ShenxingHelper') then
+function D.UnhookNonwarMap()
+	local h = Station.Lookup('Topmost1/WorldMap/Wnd_All', 'Handle_CopyBtn')
+	if not h or not h.__MY_NonwarData then
 		return
 	end
-	h = Station.Lookup('Topmost1/WorldMap/Wnd_All', '')
-	if not h or not h:IsVisible() then
+	for i = h:GetItemCount() - 1, 0, -1 do
+		local m = h:Lookup(i)
+		if m.bMYNonwar then
+			h:RemoveItem(m)
+		end
+	end
+	h.__MY_NonwarData = nil
+end
+
+function D.CheckNonwarMapEnable()
+	if O.bAncientMap and not LIB.IsShieldedVersion('MY_ShenxingHelper') then
+		D.HookNonwarMap()
+	else
+		D.UnhookNonwarMap()
+	end
+end
+LIB.RegisterFrameCreate('WorldMap.MY_ShenxingHelper__NonwarMap', D.CheckNonwarMapEnable)
+
+--------------------------------------------------------------------------
+-- 【台服用】强开所有地图
+--------------------------------------------------------------------------
+function D.HookOpenAllMap()
+	local h = Station.Lookup('Topmost1/WorldMap/Wnd_All', '')
+	if not h then
 		return
 	end
 	local me = GetClientPlayer()
 	local dwCurrMapID = me and me.GetScene().dwMapID
-	for _, szHandleName in ipairs({
-		'Handle_CityBtn',
-		'Handle_CopyBtn',
-	}) do
-		hList = h:Lookup(szHandleName)
+	for _, szHandleName in ipairs({ 'Handle_CityBtn', 'Handle_CopyBtn' }) do
+		local hList = h:Lookup(szHandleName)
 		if hList then
 			for i = 0, hList:GetItemCount() - 1 do
-				hItem = hList:Lookup(i)
+				local hItem = hList:Lookup(i)
+				if hItem.dwMYMapID == nil then
+					hItem.dwMYMapID = hItem.mapid
+				end
+				if hItem.bMYEnable == nil then
+					hItem.bMYEnable = hItem.bEnable
+				end
 				if hItem.mapid == 1 or dwCurrMapID == hItem.mapid then
 					hItem.mapid = tostring(hItem.mapid)
 				else
@@ -145,12 +175,43 @@ local function openAllMap()
 			end
 		end
 	end
-	h, hList, hItem = nil
-end
-LIB.BreatheCall('MY_Toolbox#OpenAllMap', 130, openAllMap)
 end
 
-function D.Apply()
+function D.UnhookOpenAllMap()
+	local h = Station.Lookup('Topmost1/WorldMap/Wnd_All', '')
+	if not h then
+		return
+	end
+	for _, szHandleName in ipairs({ 'Handle_CityBtn', 'Handle_CopyBtn' }) do
+		local hList = h:Lookup(szHandleName)
+		if hList then
+			for i = 0, hList:GetItemCount() - 1 do
+				local hItem = hList:Lookup(i)
+				if hItem.dwMYMapID ~= nil then
+					hItem.mapid = hItem.dwMYMapID
+					hItem.dwMYMapID = nil
+				end
+				if hItem.bMYEnable ~= nil then
+					hItem.bEnable = hItem.bMYEnable
+					hItem.bMYEnable = nil
+				end
+			end
+		end
+	end
+end
+
+function D.CheckOpenAllMapEnable()
+	if O.bOpenAllMap and not LIB.IsShieldedVersion('MY_ShenxingHelper') then
+		D.HookOpenAllMap()
+	else
+		D.UnhookOpenAllMap()
+	end
+end
+
+--------------------------------------------------------------------------
+-- 防止神行CD被黑
+--------------------------------------------------------------------------
+function D.CheckAvoidBlackShenxingEnable()
 	if O.bEnable then
 		LIB.RegisterEvent('DO_SKILL_CAST.MY_AvoidBlackShenxingCD', function()
 			local dwID, dwSkillID, dwSkillLevel = arg0, arg1, arg2
@@ -177,10 +238,35 @@ function D.Apply()
 		LIB.RegisterEvent('DO_SKILL_CAST.MY_AvoidBlackShenxingCD')
 	end
 end
-LIB.RegisterInit('MY_ShenxingHelper', D.Apply)
 
+--------------------------------------------------------------------------
+-- 模块事件监听
+--------------------------------------------------------------------------
+function D.CheckEnable()
+	D.CheckNonwarMapEnable()
+	D.CheckOpenAllMapEnable()
+	D.CheckAvoidBlackShenxingEnable()
+end
+
+function D.RemoveHook()
+	D.UnhookNonwarMap()
+	D.UnhookOpenAllMap()
+end
+
+LIB.RegisterEvent('MY_SHIELDED_VERSION.MY_ShenxingHelper', function()
+	if arg0 and arg0 ~= 'MY_ShenxingHelper' then
+		return
+	end
+	D.CheckEnable()
+end)
+LIB.RegisterInit('MY_ShenxingHelper', D.CheckEnable)
+LIB.RegisterReload('MY_ShenxingHelper', D.RemoveHook)
+
+--------------------------------------------------------------------------
+-- 设置界面
+--------------------------------------------------------------------------
 function D.OnPanelActivePartial(ui, X, Y, W, H, x, y)
-	ui:Append('WndCheckBox', {
+	x = x + ui:Append('WndCheckBox', {
 		x = x, y = y, w = 'auto',
 		text = _L['Avoid blacking shenxing cd'],
 		tip = _L['Got zhenyan wen shenxing, your shengxing will be blacked.'],
@@ -189,7 +275,31 @@ function D.OnPanelActivePartial(ui, X, Y, W, H, x, y)
 		oncheck = function(bChecked)
 			MY_ShenxingHelper.bAvoidBlackCD = bChecked
 		end,
-	})
+	}):Width() + 5
+
+	if not LIB.IsShieldedVersion('MY_ShenxingHelper') then
+		x = x + ui:Append('WndCheckBox', {
+			x = x, y = y, w = 'auto',
+			text = _L['Shenxing to ancient maps'],
+			checked = MY_ShenxingHelper.bAncientMap,
+			oncheck = function(bChecked)
+				MY_ShenxingHelper.bAncientMap = bChecked
+			end,
+		}):Width() + 5
+
+		x = x + ui:Append('WndCheckBox', {
+			x = x, y = y, w = 'auto',
+			text = _L['Force open all map shenxing'],
+			tip = _L['Shenxing can fly to undiscovered maps'],
+			tippostype = UI.TIP_POSITION.BOTTOM_TOP,
+			checked = MY_ShenxingHelper.bOpenAllMap,
+			oncheck = function(bChecked)
+				MY_ShenxingHelper.bOpenAllMap = bChecked
+			end,
+		}):Width() + 5
+	end
+
+	x = X
 	y = y + 25
 	return x, y
 end
@@ -220,9 +330,9 @@ local settings = {
 				bAvoidBlackCD = true,
 			},
 			triggers = {
-				bAncientMap = D.Apply,
-				bOpenAllMap = D.Apply,
-				bAvoidBlackCD = D.Apply,
+				bAncientMap = D.CheckNonwarMapEnable,
+				bOpenAllMap = D.CheckOpenAllMapEnable,
+				bAvoidBlackCD = D.CheckAvoidBlackShenxingEnable,
 			},
 			root = O,
 		},
