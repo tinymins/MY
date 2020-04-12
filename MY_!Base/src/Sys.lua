f46d10d79f737f2ff1335458bcc01bc66812a7e1
Emil Zhai <root@derzh.com>
2020-04-12 11:48:49 +0800

    修改自定义字体读取方式使其更自由

--- MY_!Base/src/Sys.lua
+++ MY_!Base/src/Sys.lua
@@ -764,6 +764,7 @@ function LIB.CreateDataRoot(ePathType)
 	CPath.MakeDir(LIB.FormatPath({'cache/', ePathType}))
 	CPath.MakeDir(LIB.FormatPath({'config/', ePathType}))
 	CPath.MakeDir(LIB.FormatPath({'export/', ePathType}))
+	CPath.MakeDir(LIB.FormatPath({'font/', ePathType}))
 	CPath.MakeDir(LIB.FormatPath({'userdata/', ePathType}))
 end
 end
@@ -928,34 +929,63 @@ end
 
 function LIB.GetFontList()
 	local aList, tExist = {}, {}
+	-- 插件字体包
 	if MY_FontResource then
-		aList = MY_FontResource.GetList()
-	end
-	for _, p in ipairs(aList) do
-		local szFile = p.szFile:gsub('/', '\\')
-		local szKey = szFile:lower()
-		if not tExist[szKey] then
-			tExist[szKey] = true
+		for _, p in ipairs(MY_FontResource.GetList()) do
+			local szFile = p.szFile:gsub('/', '\\')
+			local szKey = szFile:lower()
+			if not tExist[szKey] then
+				insert(aList, {
+					szName = p.szName,
+					szFile = p.szFile,
+				})
+				tExist[szKey] = true
+			end
 		end
 	end
+	-- 系统字体
 	for _, p in ipairs_r(Font.GetFontPathList() or {}) do
 		local szFile = p.szFile:gsub('/', '\\')
 		local szKey = szFile:lower()
 		if not tExist[szKey] then
-			insert(aList, 1, { szName = p.szName, szFile = szFile })
+			insert(aList, 1, {
+				szName = p.szName,
+				szFile = szFile,
+			})
 			tExist[szKey] = true
 		end
 	end
-	local CUSTOM_FONT_DIR = LIB.FormatPath({'font/', PATH_TYPE.GLOBAL}):gsub('%./', '/')
-	local CUSTOM_FONT_LIST = LIB.LoadLUAData(CUSTOM_FONT_DIR .. 'list.jx3dat', { passphrase = false }) or {}
-	for _, p in ipairs(CUSTOM_FONT_LIST) do
-		local szFile = p.szFile:gsub('^%./', CUSTOM_FONT_DIR):gsub('/', '\\')
-		local szKey = szFile:lower()
-		if not tExist[szKey] then
-			insert(aList, { szName = p.szName, szFile = szFile })
-			tExist[szKey] = true
+	-- 按照描述文件添加字体
+	local CUSTOM_FONT_DIR = LIB.FormatPath({'font/', PATH_TYPE.GLOBAL})
+	for _, szFile in ipairs(CPath.GetFileList(CUSTOM_FONT_DIR)) do
+		local info = szFile:lower():find('%.jx3dat$') and LIB.LoadLUAData(CUSTOM_FONT_DIR .. szFile, { passphrase = false })
+		if info and info.szName and info.szFile then
+			local szFontFile = info.szFile:gsub('^%./', CUSTOM_FONT_DIR):gsub('/', '\\')
+			local szKey = szFontFile:lower()
+			if not tExist[szKey] then
+				insert(aList, {
+					szName = info.szName,
+					szFile = szFontFile,
+				})
+				tExist[szKey] = true
+			end
 		end
 	end
+	-- 纯字体文件
+	for _, szFile in ipairs(CPath.GetFileList(CUSTOM_FONT_DIR)) do
+		if szFile:lower():find('%.[to]tf$') then
+			local szFontFile = (CUSTOM_FONT_DIR .. szFile):gsub('/', '\\')
+			local szKey = szFontFile:lower()
+			if not tExist[szKey] then
+				insert(aList, {
+					szName = szFile,
+					szFile = szFontFile,
+				})
+				tExist[szKey] = true
+			end
+		end
+	end
+	-- 删除不存在的字体
 	for i, p in ipairs_r(aList) do
 		if not IsFileExist(p.szFile) then
 			remove(aList, i)
