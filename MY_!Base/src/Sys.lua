f3c74f48e471f8ceaaf5215376ee028c4cabc3df
Emil Zhai <root@derzh.com>
2020-04-23 21:12:17 +0800

    增加汉字转拼音的接口

--- MY_!Base/src/Sys.lua
+++ MY_!Base/src/Sys.lua
@@ -2554,3 +2554,68 @@ function LIB.CreateCache(szNameMode, aEvent)
 	Register()
 	return setmetatable(t, mt)
 end
+
+-----------------------------------------------
+-- ºº×Ö×ªÆ´Òô
+-----------------------------------------------
+do local PINYIN, PINYIN_CONSONANT
+function LIB.Han2Pinyin(szText)
+	if not IsString(szText) then
+		return
+	end
+	if not PINYIN then
+		PINYIN = LIB.LoadLUAData(PACKET_INFO.FRAMEWORK_ROOT .. 'data/pinyin/{$lang}.jx3dat', { passphrase = false })
+		local tPinyinConsonant = {}
+		for c, v in pairs(PINYIN) do
+			local a, t = {}, {}
+			for _, s in ipairs(v) do
+				s = s:sub(1, 1)
+				if not t[s] then
+					t[s] = true
+					insert(a, s)
+				end
+			end
+			tPinyinConsonant[c] = a
+		end
+		PINYIN_CONSONANT = tPinyinConsonant
+	end
+	local aText = LIB.SplitString(szText, '')
+	local aFull, nFullCount = {''}, 1
+	local aConsonant, nConsonantCount = {''}, 1
+	for _, szChar in ipairs(aText) do
+		local aCharPinyin = PINYIN[szChar]
+		if aCharPinyin and #aCharPinyin > 0 then
+			for i = 2, #aCharPinyin do
+				for j = 1, nFullCount do
+					insert(aFull, aFull[j] .. aCharPinyin[i])
+				end
+			end
+			for j = 1, nFullCount do
+				aFull[j] = aFull[j] .. aCharPinyin[1]
+			end
+			nFullCount = nFullCount * #aCharPinyin
+		else
+			for j = 1, nFullCount do
+				aFull[j] = aFull[j] .. szChar
+			end
+		end
+		local aCharPinyinConsonant = PINYIN_CONSONANT[szChar]
+		if aCharPinyinConsonant and #aCharPinyinConsonant > 0 then
+			for i = 2, #aCharPinyinConsonant do
+				for j = 1, nConsonantCount do
+					insert(aConsonant, aConsonant[j] .. aCharPinyinConsonant[i])
+				end
+			end
+			for j = 1, nConsonantCount do
+				aConsonant[j] = aConsonant[j] .. aCharPinyinConsonant[1]
+			end
+			nConsonantCount = nConsonantCount * #aCharPinyinConsonant
+		else
+			for j = 1, nConsonantCount do
+				aConsonant[j] = aConsonant[j] .. szChar
+			end
+		end
+	end
+	return aFull, aConsonant
+end
+end
