bd47fbe6777caa642150fce63739c26fdfde915c
Emil Zhai <root@derzh.com>
2020-05-29 11:22:22 +0800

    Release V172

--- MY_!Base/src/Base.lua
+++ MY_!Base/src/Base.lua
@@ -139,8 +139,8 @@ end
 local function IsStreaming()
 	return _G.SM_IsEnable and _G.SM_IsEnable()
 end
-local _BUILD_                = '20200528'
-local _VERSION_              = 0x2017100
+local _BUILD_                = '20200601'
+local _VERSION_              = 0x2017200
 local _MENU_COLOR_           = {255, 165, 79}
 local _MAX_PLAYER_LEVEL_     = 100
 local _INTERFACE_ROOT_       = 'Interface/'
