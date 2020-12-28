812f7b7129811564c24ad2c66c5ea997f94f9364
Emil Zhai <root@derzh.com>
2020-12-28 22:32:38 +0800

    Release V198

--- MY_!Base/src/Base.lua
+++ MY_!Base/src/Base.lua
@@ -139,9 +139,9 @@ end
 local function IsStreaming()
 	return _G.SM_IsEnable and _G.SM_IsEnable()
 end
-local _BUILD_                = '20201211'
-local _NATURAL_VERSION_      = 197
-local _VERSION_              = '3.0.4'
+local _BUILD_                = '20201228'
+local _NATURAL_VERSION_      = 198
+local _VERSION_              = '3.0.5'
 local _MENU_COLOR_           = {255, 165, 79}
 local _MAX_PLAYER_LEVEL_     = 100
 local _INTERFACE_ROOT_       = 'Interface/'
