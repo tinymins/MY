b34d074db8a8ca5114c69465d7c84fe3362d87c5
Emil Zhai <root@derzh.com>
2021-04-25 00:54:40 +0800

    战斗日志增加奇穴信息

--- MY_TeamTools/src/MY_CombatLogs.lua
+++ MY_TeamTools/src/MY_CombatLogs.lua
@@ -332,7 +332,17 @@ function D.OnTargetUpdate(dwID, bForce)
 			})
 		end
 		local szGUID = LIB.GetPlayerGUID(dwID) or ''
-		D.InsertLog(LOG_TYPE.PLAYER_INFO, { dwID, szName, dwForceID, dwMountKungfuID, nEquipScore, aEquip, szGUID })
+		local aTalent = LIB.GetPlayerTalentInfo(player)
+		if aTalent then
+			for i, p in ipairs(aTalent) do
+				aTalent[i] = {
+					p.nIndex,
+					p.dwSkillID,
+					p.dwSkillLevel,
+				}
+			end
+		end
+		D.InsertLog(LOG_TYPE.PLAYER_INFO, { dwID, szName, dwForceID, dwMountKungfuID, nEquipScore, aEquip, aTalent, szGUID })
 	else
 		local npc = GetNpc(dwID)
 		if not npc then
