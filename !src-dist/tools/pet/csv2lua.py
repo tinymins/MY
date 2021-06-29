e5e4b6d8ca0a9eda6c261ed0e47f776ac56b76bc
Emil Zhai <root@derzh.com>
2021-06-29 14:49:07 +0800

    修复奇遇数据生成器脚本问题

--- !src-dist/tools/pet/csv2lua.py
+++ !src-dist/tools/pet/csv2lua.py
@@ -1,31 +1,39 @@
-import pandas, luadata, os
+import codecs, pandas, luadata, os
 
 SRC_PATH = os.path.abspath(os.path.join(__file__, '..', 'data.tsv'))
 POS_PATH = os.path.abspath(os.path.join(__file__, '..', 'pos.tsv'))
-DST_PATH = os.path.abspath(os.path.join(__file__, '..\\..\\..\\dat\\MY_RoleStatistics\\data\\serendipity\\zhcn.jx3dat'))
+DST_PATH = os.path.abspath(os.path.join(__file__, '..\\..\\..\\data\\MY_RoleStatistics\\data\\serendipity\\zhcn.jx3dat'))
 
-res = []
+def rowappend(a, p, k, v):
+	if not pandas.notna(v) or k == 'id' or k.startswith('Unnamed'):
+		return
+	if k == 'dwType' or k.startswith('a'):
+		pass
+	elif k.startswith('dw') or k.startswith('n'):
+		v = luadata.serialize(int(v))
+	else:
+		v = luadata.serialize(v, 'gbk')
+	a.append('%s%s = %s,' % (p, k, v))
 
-def serialize(key, val):
-	if type(val) == float:
-		return int(val)
-	if key == 'dwType' or key.startswith('a'):
-		return luadata.const(val)
-	return val
+res = ['return {']
 
-data = []
-for _, row in pandas.read_csv(SRC_PATH, sep='\t', encoding="utf-8", skiprows=2).iterrows():
-	data.append({k : serialize(k, v)
-					for k, v in dict(row).items()
-						if pandas.notna(v) and k != 'id' and not k.startswith('Unnamed')})
-res.append(data)
+res.append('\t{')
+for _, row in pandas.read_csv(SRC_PATH, sep='\t', encoding='utf-8', skiprows=2).iterrows():
+	res.append('\t\t{')
+	for k, v in dict(row).items():
+		rowappend(res, '\t\t\t', k, v)
+	res.append('\t\t},')
+res.append('\t},')
 
+res.append('\t{')
+for _, row in pandas.read_csv(POS_PATH, sep='\t', encoding='utf-8', skiprows=1).iterrows():
+	res.append('\t\t{')
+	for k, v in dict(row).items():
+		rowappend(res, '\t\t\t', k, v)
+	res.append('\t\t},')
+res.append('\t},')
 
-pos = []
-for _, row in pandas.read_csv(POS_PATH, sep='\t', encoding="utf-8", skiprows=1).iterrows():
-	pos.append({k : serialize(k, v)
-					for k, v in dict(row).items()
-						if pandas.notna(v) and k != 'id' and not k.startswith('Unnamed')})
-res.append(pos)
+res.append('}')
 
-luadata.write(res, DST_PATH, encoding='gbk', form = True)
+with codecs.open(DST_PATH, 'w', 'gbk') as file:
+	file.write('\n'.join(res))
