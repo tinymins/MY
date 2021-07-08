134ff773265c625c32e3cea108f3b7acd97516bf
Emil Zhai <root@derzh.com>
2021-07-09 02:14:56 +0800

    build: 编译脚本增加 --diff 参数可手动指定增量打包基础版本号

--- !src-dist/build.py
+++ !src-dist/build.py
@@ -77,7 +77,7 @@ def __compress(addon):
 		f.write(converter.convert(info_content))
 	print('Update info done...')
 
-def __get_version_info():
+def __get_version_info(diff_ver):
 	'''Get version information'''
 	# Read version from Base.lua
 	current_version = ''
@@ -85,28 +85,54 @@ def __get_version_info():
 		if line[0:16] == 'local _VERSION_ ':
 			current_version = re.sub(r'(?is)^local _VERSION_\s+=', '', line).strip()[1:-1]
 	# Read max and previous release commit
-	commit_list = filter(None, os.popen('git log --grep release: --pretty=format:"%p|%s"').read().split('\n'))
+	current_hash = os.popen('git log -n 1 --pretty=format:"%h"').read().strip()
+	commit_list = os.popen('git log --grep release: --pretty=format:"SUCCESS|%h|%s"').read().split('\n')
+	if diff_ver:
+		commit_list += os.popen('git log ' + diff_ver + ' -n 1 --pretty=format:"SUCCESS|%h|%s"').read().split('\n')
+	commit_list = filter(lambda x: x and x.startswith('SUCCESS|'), commit_list)
+	commit_list = map(lambda x: x[8:], commit_list)
 	max_version, prev_version, prev_version_message, prev_version_hash = '', '', '', ''
 	for commit in commit_list:
 		try:
 			version = re.sub(r'(?is)^\w+\|release:\s+', '', commit).strip()
 			version_message = re.sub(r'(?is)^\w+\|', '', commit).strip()
 			version_hash = re.sub(r'(?is)\|.+$', '', commit).strip()
-			if max_version == '' and semver.compare(version, '0.0.0') == 1:
-				max_version = version
-				prev_version = version
-				prev_version_message = version_message
-				prev_version_hash = version_hash
-				continue
-			if semver.compare(version, current_version) == -1 and semver.compare(version, prev_version) == 1:
-				prev_version = version
-				prev_version_message = version_message
-				prev_version_hash = version_hash
-			if semver(version, max_version) == 1:
-				max_version = version
+			if diff_ver:
+				if diff_ver == version and semver.compare(version, '0.0.0') == 1:
+					max_version = version
+					prev_version = version
+					prev_version_message = version_message
+					prev_version_hash = version_hash
+					continue
+				if diff_ver.startswith(version_hash):
+					max_version = '0.0.0'
+					prev_version = '0.0.0'
+					prev_version_message = version_message
+					prev_version_hash = version_hash
+					continue
+			else:
+				if max_version == '' and semver.compare(version, '0.0.0') == 1:
+					max_version = version
+					prev_version = version
+					prev_version_message = version_message
+					prev_version_hash = version_hash
+					continue
+				if semver.compare(version, current_version) == -1 and semver.compare(version, prev_version) == 1:
+					prev_version = version
+					prev_version_message = version_message
+					prev_version_hash = version_hash
+				if semver(version, max_version) == 1:
+					max_version = version
 		except:
 			pass
-	return { 'current': current_version, 'max': max_version, 'previous': prev_version, 'previous_message': prev_version_message, 'previous_hash': prev_version_hash }
+	return {
+		'current': current_version,
+		'current_hash': current_hash,
+		'max': max_version,
+		'previous': prev_version,
+		'previous_message': prev_version_message,
+		'previous_hash': prev_version_hash,
+	}
 
 def __7zip(file_name, base_message, base_hash):
 	cmd_suffix = ''
@@ -143,9 +169,14 @@ def __7zip(file_name, base_message, base_hash):
 	print('Url: ' + file_name)
 	print('Based on git commit "%s(%s)".' % (base_message, base_hash) if base_hash != '' else 'Full package.')
 
-def run(is_full, is_source):
-	print('> RELEASE MODE: %s, %s.' % ('full' if is_full else 'diff', 'source' if is_source else 'dist'))
-	version_info = __get_version_info()
+def run(diff_ver, is_source):
+	print('> DIFF VERSION: %s' % (diff_ver or 'auto'))
+	print('> RELEASE MODE: %s' % ('source' if is_source else 'dist'))
+	version_info = __get_version_info(diff_ver)
+
+	if diff_ver and version_info.get('previous_hash') == '':
+		print('Error: Specified diff commit not found (release: %s).' % diff_ver)
+		exit()
 
 	if not is_source:
 		# Merge master into prelease
@@ -160,7 +191,7 @@ def run(is_full, is_source):
 		# Merge prelease into stable
 		if git.get_current_branch() == 'prelease':
 			os.system('git reset master')
-			version_info = __get_version_info()
+			version_info = __get_version_info(diff_ver)
 			utils.assert_exit(version_info.get('max') == '' or semver.compare(version_info.get('current'), version_info.get('max')) == 1,
 				'Error: current version(%s) must be larger than max history version(%s)!' % (version_info.get('current'), version_info.get('max')))
 			os.system('git add * && git commit -m "release: %s"' % version_info.get('current'))
@@ -178,15 +209,29 @@ def run(is_full, is_source):
 			__compress(addon)
 
 	# Package files
-	file_name = '!src-dist/dist/%s_%s_v%s.7z' % (get_current_packet_id(), time.strftime('%Y%m%d%H%M%S', time.localtime()), version_info.get('current'))
-	fullpack_file_name = '!src-dist/dist/%s_%s_v%s.full.7z' % (get_current_packet_id(), time.strftime('%Y%m%d%H%M%S', time.localtime()), version_info.get('current'))
+	file_name = ''
+	if version_info.get('previous_hash'):
+		file_name = '!src-dist/dist/%s_%s_v%s.diff-%s-%s.7z' % (
+			get_current_packet_id(),
+			time.strftime('%Y%m%d%H%M%S', time.localtime()),
+			version_info.get('current'),
+			version_info.get('previous_hash'),
+			version_info.get('current_hash'),
+		)
 	base_message = ''
 	base_hash = ''
-	if not is_full and version_info.get('current') != '' and version_info.get('previous_hash') != '':
+	if version_info.get('current') != '' and version_info.get('previous_hash') != '':
 		base_message = version_info.get('previous_message')
 		base_hash = version_info.get('previous_hash')
-	__7zip(file_name, base_message, base_hash)
-	__7zip(fullpack_file_name, base_message, '')
+	if file_name:
+		__7zip(file_name, base_message, base_hash)
+
+	fullpack_file_name = '!src-dist/dist/%s_%s_v%s.full.7z' % (
+		get_current_packet_id(),
+		time.strftime('%Y%m%d%H%M%S', time.localtime()),
+		version_info.get('current'),
+	)
+	__7zip(fullpack_file_name, '', '')
 
 	# Revert source code modify by compressing
 	if not is_source:
@@ -197,10 +242,10 @@ def run(is_full, is_source):
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='One-key release packet product helper.')
-	parser.add_argument('--full', action='store_true', help='Package full plugin packet.')
+	parser.add_argument('--diff', type=str, help='Package diff version.')
 	parser.add_argument('--no-build', action='store_true', help='Package source code.')
 	args = parser.parse_args()
 
 	env.set_packet_as_cwd()
 
-	run(args.full, args.no_build)
+	run(args.diff, args.no_build)
