c50fe4362521277f1bc2830ca2dd79ea081ddb91
Emil Zhai <root@derzh.com>
2021-07-16 01:38:02 +0800

    release: 7.0.0

--- MY_TeamTools/src/MY_JB.PS.lua
+++ MY_TeamTools/src/MY_JB.PS.lua
@@ -50,7 +50,7 @@ local PLUGIN_ROOT = PACKET_INFO.ROOT .. PLUGIN_NAME
 local MODULE_NAME = 'MY_JBBind'
 local _L = LIB.LoadLangPack(PLUGIN_ROOT .. '/lang/jx3box/')
 --------------------------------------------------------------------------
-if not LIB.AssertVersion(MODULE_NAME, _L[MODULE_NAME], '^6.0.0') then
+if not LIB.AssertVersion(MODULE_NAME, _L[MODULE_NAME], '^7.0.0') then
 	return
 end
 --------------------------------------------------------------------------
