de004a1e025000e60d4b1da364e51cd989570ce5
Emil Zhai <root@derzh.com>
2021-08-10 13:41:36 +0800

    fix: 修复采集头顶勾选其它物品后路标等物件也会显示的问题

--- MY_GKP/src/MY_GKPDoodad.lua
+++ MY_GKP/src/MY_GKPDoodad.lua
@@ -195,6 +195,24 @@ RegisterCustomData('MY_GKPDoodad.szCustom')
 -- 本地函数和变量
 ---------------------------------------------------------------------
 local INI_SHADOW = PACKET_INFO.UICOMPONENT_ROOT .. 'Shadow.ini'
+local DOODAD_TYPE_VISIBLE = {
+	[DOODAD_KIND.INVALID     ] = false,
+	[DOODAD_KIND.NORMAL      ] = false, -- 普通的Doodad,有Tip,不能操作
+	[DOODAD_KIND.CORPSE      ] = true , -- 尸体
+	[DOODAD_KIND.QUEST       ] = true , -- 任务相关的Doodad
+	[DOODAD_KIND.READ        ] = true , -- 可以看的Doodad
+	[DOODAD_KIND.DIALOG      ] = true , -- 可以对话的Doodad
+	[DOODAD_KIND.ACCEPT_QUEST] = true , -- 可以接任务的Doodad,本质上上面3个类型是一样的,只是图标不同而已
+	[DOODAD_KIND.TREASURE    ] = true , -- 宝箱
+	[DOODAD_KIND.ORNAMENT    ] = false, -- 装饰物,不能操作
+	[DOODAD_KIND.CRAFT_TARGET] = true , -- 生活技能的采集物
+	[DOODAD_KIND.CLIENT_ONLY ] = false, -- 客户端用
+	[DOODAD_KIND.CHAIR       ] = true , -- 可以坐的Doodad
+	[DOODAD_KIND.GUIDE       ] = false, -- 路标
+	[DOODAD_KIND.DOOR        ] = false, -- 门之类有动态障碍的Doodad
+	[DOODAD_KIND.NPCDROP     ] = false, -- 使用NPC掉落模式的doodad
+	[DOODAD_KIND.SPRINT      ] = false, -- 轻功落脚点
+}
 
 local function GetDoodadTemplateName(dwID)
 	local doodad = GetDoodadTemplate(dwID)
@@ -268,6 +286,9 @@ function D.GetDoodadInfo(dwID)
 	end
 	local me = GetClientPlayer()
 	local tpl = GetDoodadTemplate(doodad.dwTemplateID)
+	if DOODAD_TYPE_VISIBLE[tpl.nKind] == false then
+		return
+	end
 	local info = {
 		dwCraftID = tpl.dwCraftID,
 	}
