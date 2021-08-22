df94cc27b9d18393f88a982ebe88a78b938f0706
Emil Zhai <root@derzh.com>
2021-08-23 03:25:52 +0800

    feat: 屏蔽怀旧版金团无法使用的发布工资按钮

--- MY_GKP/src/MY_GKP_MI.lua
+++ MY_GKP/src/MY_GKP_MI.lua
@@ -257,7 +257,7 @@ X.RegisterBgMsg('MY_GKP', function(_, data, nChannel, dwID, szName, bIsSelf)
 						local handle = ui:Append('Handle', { w = 230, h = 20, x = x + 30, y = y + 70 + 30 * n + 5, handlestyle = 3 })[1]
 						handle:AppendItemFromString(GetFormatText(_L['Total Auction:'], 41) .. D.GetMoneyTipText(nMoney))
 						handle:FormatAllItemPos()
-						if X.IsDistributer() then
+						if X.IsDistributer() and GLOBAL.GAME_BRANCH ~= 'classic' then
 							ui:Append('WndButton', {
 								w = 91, h = 26, x = x + 620, y = y + 70 + 30 * n + 5, text = _L['salary'],
 								buttonstyle = 'SKEUOMORPHISM',
