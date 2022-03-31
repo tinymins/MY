71871b78630653525dcd6595ade55e261b2a5a83
Emil Zhai <root@derzh.com>
2022-03-31 20:50:59 +0800

    fix: 修复头顶血条颜色转换问题

--- MY_LifeBar/src/MY_LifeBar_Official.lua
+++ MY_LifeBar/src/MY_LifeBar_Official.lua
@@ -89,8 +89,9 @@ local function SetCaptionColor(dwID, nR, nG, nB)
 	if tColor and tColor.nR == nR and tColor.nG == nG and tColor.nB == nB then
 		return
 	end
-	tColor = { nR = nR, nG = nG, nB = nB, dwColor = 255 * 16711680 + nR * 65536 + nG * 256 + nB }
+	tColor = { nR = nR, nG = nG, nB = nB, dwColor = 255 * 16777216 + nR * 65536 + nG * 256 + nB }
 	CACHE_CAPTION_COLOR[dwID] = tColor
+	Output('set plugin caption color ' .. dwID .. ' 1 ' .. tColor.dwColor, tColor)
 	rlcmd('set plugin caption color ' .. dwID .. ' 1 ' .. tColor.dwColor)
 end
 local function ApplyCaptionColor(dwID)
