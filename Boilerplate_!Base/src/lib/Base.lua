ec74ae8dee9d4869b98176304f5716f2ca62518c
Emil Zhai <root@derzh.com>
2022-05-25 18:29:20 +0800

    feat: 测试等级配置使用枚举键名方式便于理解

--- Boilerplate_!Base/src/lib/Base.lua
+++ Boilerplate_!Base/src/lib/Base.lua
@@ -55,8 +55,8 @@ local _LOGO_MENU_FRAME_       = 1
 local _LOGO_MENU_HOVER_FRAME_ = 2
 local _POSTER_UITEX_          = _ADDON_ROOT_ .. _NAME_SPACE_ .. '_Resource/img/Poster.UITex'
 local _POSTER_FRAME_COUNT_    = 1
-local _DEBUG_LEVEL_           = tonumber(LoadLUAData(_DATA_ROOT_ .. 'debug.level.jx3dat') or nil) or DEBUG_LEVEL.NONE
-local _LOG_LEVEL_             = math.min(tonumber(LoadLUAData(_DATA_ROOT_ .. 'log.level.jx3dat') or nil) or DEBUG_LEVEL.NONE, _DEBUG_LEVEL_)
+local _DEBUG_LEVEL_           = DEBUG_LEVEL[LoadLUAData(_DATA_ROOT_ .. 'debug.level.jx3dat') or 'NONE'] or DEBUG_LEVEL.NONE
+local _LOG_LEVEL_             = math.min(DEBUG_LEVEL[LoadLUAData(_DATA_ROOT_ .. 'log.level.jx3dat') or 'NONE'] or DEBUG_LEVEL.NONE, _DEBUG_LEVEL_)
 
 -- 创建命名空间
 local X = {
