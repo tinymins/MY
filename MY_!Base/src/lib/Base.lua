1f096a51231e8e9e2dc7f09416a60733ad6e579f
Emil Zhai <root@derzh.com>
2023-11-03 06:16:51 +0800

    release: 17.0.0

--- MY_!Base/src/lib/Base.lua
+++ MY_!Base/src/lib/Base.lua
@@ -46,8 +46,8 @@ local CODE_PAGE = {
 }
 
 local _NAME_SPACE_            = 'MY'
-local _BUILD_                 = '20231030'
-local _VERSION_               = '16.3.8'
+local _BUILD_                 = '20231103'
+local _VERSION_               = '17.0.0'
 local _MENU_COLOR_            = {255, 165, 79}
 local _INTERFACE_ROOT_        = 'Interface/'
 local _ADDON_ROOT_            = _INTERFACE_ROOT_ .. _NAME_SPACE_ .. '/'
--- MY_Logoff/src/MY_Logoff.lua
+++ MY_Logoff/src/MY_Logoff.lua
@@ -14,7 +14,7 @@ local PLUGIN_ROOT = X.PACKET_INFO.ROOT .. PLUGIN_NAME
 local MODULE_NAME = 'MY_Logoff'
 local _L = X.LoadLangPack(PLUGIN_ROOT .. '/lang/')
 --------------------------------------------------------------------------
-if not X.AssertVersion(MODULE_NAME, _L[MODULE_NAME], '^16.0.0') then
+if not X.AssertVersion(MODULE_NAME, _L[MODULE_NAME], '^17.0.0') then
 	return
 end
 --[[#DEBUG BEGIN]]X.ReportModuleLoading(MODULE_PATH, 'START')--[[#DEBUG END]]
