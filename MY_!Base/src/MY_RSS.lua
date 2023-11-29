3d98f8b902ff7afef313414bc4d58c5ce2a237a8
Emil Zhai <root@derzh.com>
2023-11-29 12:05:42 +0800

    fix: 更正推送数据基地址

--- MY_!Base/src/MY_RSS.lua
+++ MY_!Base/src/MY_RSS.lua
@@ -26,7 +26,7 @@ local RSS_DATA = X.LoadLUAData(RSS_FILE)
 local RSS_ADAPTER = {}
 local RSS_DATA_CACHE = {}
 local RSS_PULL_BASE_URL = 'https://pull.j3cx.com'
-local RSS_PUSH_BASE_URL = 'https://pull.j3cx.com'
+local RSS_PUSH_BASE_URL = 'https://push.j3cx.com'
 
 function D.Get(szKey)
 	if not RSS_DATA then
