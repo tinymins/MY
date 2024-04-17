cb82eec69b252db85e5fab7a16e5c35c68e841e8
Emil Zhai <root@derzh.com>
2024-04-17 14:32:17 +0800

    release: 19.0.3

--- MY_!Base/src/lib/Base.lua
+++ MY_!Base/src/lib/Base.lua
@@ -46,8 +46,8 @@ local CODE_PAGE = {
 }
 
 local _NAME_SPACE_            = 'MY'
-local _BUILD_                 = '20240308'
-local _VERSION_               = '19.0.2'
+local _BUILD_                 = '20240417'
+local _VERSION_               = '19.0.3'
 local _MENU_COLOR_            = {255, 165, 79}
 local _INTERFACE_ROOT_        = 'Interface/'
 local _ADDON_ROOT_            = _INTERFACE_ROOT_ .. _NAME_SPACE_ .. '/'
