b8a9cae3d50ac737fc483f20143903b53098f742
Emil Zhai <root@derzh.com>
2024-05-11 17:50:40 +0800

    release: 20.0.0

--- MY_Force/src/MY_EnergyBar.lua
+++ MY_Force/src/MY_EnergyBar.lua
@@ -15,7 +15,7 @@ local PLUGIN_ROOT = X.PACKET_INFO.ROOT .. PLUGIN_NAME
 local MODULE_NAME = 'MY_EnergyBar'
 local _L = X.LoadLangPack(PLUGIN_ROOT .. '/lang/')
 --------------------------------------------------------------------------
-if not X.AssertVersion(MODULE_NAME, _L[MODULE_NAME], '^19.0.0-alpha.0') then
+if not X.AssertVersion(MODULE_NAME, _L[MODULE_NAME], '^20.0.0') then
 	return
 end
 --[[#DEBUG BEGIN]]X.ReportModuleLoading(MODULE_PATH, 'START')--[[#DEBUG END]]
