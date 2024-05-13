3624663cae16e4f9f555a8201a7c74f87533338b
Emil Zhai <root@derzh.com>
2024-05-14 00:28:34 +0800

    feat: 增加游戏钩子库

---
+++ Boilerplate_!Base/src/lib/Game.Hook.lua
@@ -0,0 +1,15 @@
+--------------------------------------------------------------------------------
+-- This file is part of the JX3 Plugin Project.
+-- @desc     : ”Œœ∑ª∑æ≥ø‚
+-- @copyright: Copyright (c) 2009 Kingsoft Co., Ltd.
+--------------------------------------------------------------------------------
+local X = Boilerplate
+--------------------------------------------------------------------------------
+local MODULE_PATH = X.NSFormatString('{$NS}_!Base/lib/Game.Hook')
+--------------------------------------------------------------------------------
+--[[#DEBUG BEGIN]]X.ReportModuleLoading(MODULE_PATH, 'START')--[[#DEBUG END]]
+--------------------------------------------------------------------------------
+local _L = X.LoadLangPack(X.PACKET_INFO.FRAMEWORK_ROOT .. 'lang/lib/')
+--------------------------------------------------------------------------------
+
+--[[#DEBUG BEGIN]]X.ReportModuleLoading(MODULE_PATH, 'FINISH')--[[#DEBUG END]]
