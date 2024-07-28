bebbbba2f641e82a9d47eee1c9072fa39a9e6cfb
Emil Zhai <root@derzh.com>
2024-07-28 14:25:32 +0800

    refactor: 使用聊天接口重构密聊提醒模块

--- MY_Chat/src/MY_WhisperMention.lua
+++ MY_Chat/src/MY_WhisperMention.lua
@@ -28,54 +28,14 @@ local O = X.CreateUserSettingsModule('MY_WhisperMention', _L['Chat'], {
 		xDefaultValue = false,
 	},
 })
-local D = {}
+local D = {
+	bEnable = false,
+	aMentionMsg = {},
+}
 
 function D.Apply()
-	if O.bEnable then
-		X.RegisterMsgMonitor({
-			'MSG_NORMAL',
-			'MSG_PARTY',
-			'MSG_MAP',
-			'MSG_BATTLE_FILED',
-			'MSG_GUILD',
-			'MSG_GUILD_ALLIANCE',
-			'MSG_SCHOOL',
-			'MSG_WORLD',
-			'MSG_TEAM',
-			'MSG_CAMP',
-			'MSG_GROUP',
-			'MSG_SEEK_MENTOR',
-			'MSG_FRIEND',
-			'MSG_IDENTITY',
-			'MSG_SYS',
-			'MSG_NPC_NEARBY',
-			'MSG_NPC_YELL',
-			'MSG_NPC_PARTY',
-			'MSG_NPC_WHISPER',
-		}, 'MY_RedirectMentionToWhisper', function(szChannel, szMsg, nFont, bRich, r, g, b, dwTalkerID, szName)
-			local me = X.GetClientPlayer()
-			if not me or me.dwID == dwTalkerID then
-				return
-			end
-			local bEcho = false
-			local aXMLNode = X.XMLDecode(szMsg)
-			if not X.IsTable(aXMLNode) then
-				return
-			end
-			for _, node in ipairs(aXMLNode) do
-				local nodeType = X.XMLGetNodeType(node)
-				local nodeName = X.XMLGetNodeData(node, 'name') or ''
-				local nodeText = X.XMLGetNodeData(node, 'text')
-				if nodeType == 'text' and nodeName:sub(1, 8) == 'namelink' and nodeText:sub(2, -2) == me.szName then
-					bEcho = true
-					break
-				end
-			end
-			if bEcho then
-				OutputMessage('MSG_WHISPER', szMsg, bRich, nFont, {r, g, b}, dwTalkerID, szName)
-			end
-		end)
-		X.HookChatPanel('FILTER', 'MY_RedirectMentionToWhisper', function(h, szMsg, szChannel, dwTime)
+	if D.bEnable then
+		X.HookChatPanel('FILTER', 'MY_WhisperMention', function(h, szMsg, szChannel, dwTime)
 			local tInfo = MY_Chat.ParseMessageInfo(szMsg)
 			if tInfo then
 				dwTime    = tInfo.dwTime
@@ -89,26 +49,64 @@ function D.Apply()
 			return true
 		end)
 	else
-		X.HookChatPanel('FILTER', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_NORMAL', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_PARTY', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_MAP', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_BATTLE_FILED', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_GUILD', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_GUILD_ALLIANCE', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_SCHOOL', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_WORLD', 'MY_RedirectMentionToWhisper',false)
-		X.RegisterMsgMonitor('MSG_TEAM', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_CAMP', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_GROUP', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_SEEK_MENTOR', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_FRIEND', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_IDENTITY', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_SYS', 'MY_RedirectMentionToWhisper',false)
-		X.RegisterMsgMonitor('MSG_NPC_NEARBY', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_NPC_YELL', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_NPC_PARTY', 'MY_RedirectMentionToWhisper', false)
-		X.RegisterMsgMonitor('MSG_NPC_WHISPER', 'MY_RedirectMentionToWhisper',false)
+		X.HookChatPanel('FILTER', 'MY_WhisperMention', false)
+	end
+end
+
+function D.ClearMsg(szMsg)
+	return (szMsg:gsub('<null.+/null>', ''))
+end
+
+function D.OnMessageArrive(szChannel, szMsg, nFont, bRich, r, g, b, dwTalkerID, szName)
+	local me = X.GetClientPlayer()
+	if not me then
+		return
+	end
+	local bEcho = false
+	local aXMLNode = X.XMLDecode(szMsg)
+	if not X.IsTable(aXMLNode) then
+		return
+	end
+	for _, node in ipairs(aXMLNode) do
+		local nodeType = X.XMLGetNodeType(node)
+		local nodeName = X.XMLGetNodeData(node, 'name') or ''
+		local nodeText = X.XMLGetNodeData(node, 'text')
+		if nodeType == 'text' and nodeName:sub(1, 8) == 'namelink' and nodeText:sub(2, -2) == me.szName then
+			bEcho = true
+			break
+		end
+	end
+	if bEcho then
+		if D.bEnable then
+			OutputMessage('MSG_WHISPER', szMsg, bRich, nFont, {r, g, b}, dwTalkerID, szName)
+		end
+		table.insert(D.aMentionMsg, {
+			szChannel = szChannel,
+			szMsg = D.ClearMsg(szMsg),
+			dwTalkerID = dwTalkerID,
+			szName = szName,
+			dwTime = GetCurrentTime(),
+		})
+		X.DelayCall('MY_WhisperMention', 1500, D.OnMentionMsgGC)
+	end
+end
+
+function D.OnMentionMsgGC()
+	while true do
+		local tMention = D.aMentionMsg[1]
+		if not tMention or tMention.dwTime + 1 > GetCurrentTime() then
+			break
+		end
+		table.remove(D.aMentionMsg, 1)
+	end
+end
+
+function D.OnMsgFilter(szMsgType, szMsg, nFont, bRich, r, g, b, dwTalkerID, szName)
+	szMsg = D.ClearMsg(szMsg)
+	for _, v in ipairs(D.aMentionMsg) do
+		if dwTalkerID == v.dwTalkerID and szMsg == v.szMsg and v.dwTime + 1 > GetCurrentTime() then
+			return true
+		end
 	end
 end
 
@@ -148,6 +146,40 @@ end
 -- ÊÂ¼þ×¢²á
 --------------------------------------------------------------------------------
 
-X.RegisterUserSettingsInit('MY_WhisperMention', D.Apply)
+X.RegisterMsgMonitor(
+	{
+		'MSG_NORMAL',
+		'MSG_PARTY',
+		'MSG_MAP',
+		'MSG_BATTLE_FILED',
+		'MSG_GUILD',
+		'MSG_GUILD_ALLIANCE',
+		'MSG_SCHOOL',
+		'MSG_WORLD',
+		'MSG_TEAM',
+		'MSG_CAMP',
+		'MSG_GROUP',
+		'MSG_SEEK_MENTOR',
+		'MSG_FRIEND',
+		'MSG_IDENTITY',
+		'MSG_SYS',
+		'MSG_NPC_NEARBY',
+		'MSG_NPC_YELL',
+		'MSG_NPC_PARTY',
+		'MSG_NPC_WHISPER',
+	},
+	'MY_WhisperMention',
+	D.OnMessageArrive
+)
+X.RegisterMsgFilter('MSG_WHISPER', 'MY_WhisperMention', D.OnMsgFilter)
+
+X.RegisterUserSettingsInit('MY_WhisperMention', function()
+	D.bEnable = O.bEnable
+	D.Apply()
+end)
+X.RegisterUserSettingsRelease('MY_WhisperMention', function()
+	D.bEnable = false
+	D.Apply()
+end)
 
 --[[#DEBUG BEGIN]]X.ReportModuleLoading(MODULE_PATH, 'FINISH')--[[#DEBUG END]]
