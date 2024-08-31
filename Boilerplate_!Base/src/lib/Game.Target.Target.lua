fffd79895d437feb73938a586d3ee38742d4bae9
Emil Zhai <root@derzh.com>
2024-08-31 23:33:19 +0800

    chore: 调整函数顺序

--- Boilerplate_!Base/src/lib/Game.Target.Target.lua
+++ Boilerplate_!Base/src/lib/Game.Target.Target.lua
@@ -44,6 +44,112 @@ function X.GetTargetTarget(object)
 	return X.GetTarget(KTar)
 end
 
+X.RegisterRestriction('X.SET_TARGET', { ['*'] = true, intl = false })
+
+-- 根据 dwType 类型和 dwID 设置目标
+-- (void) X.SetTarget([number dwType, ]number dwID)
+-- (void) X.SetTarget([number dwType, ]string szName)
+-- dwType   -- *可选* 目标类型
+-- dwID     -- 目标 ID
+function X.SetTarget(arg0, arg1)
+	local dwType, dwID, szNames
+	if X.IsUserdata(arg0) then
+		dwType, dwID = TARGET[X.GetObjectType(arg0)], arg0.dwID
+	elseif X.IsString(arg0) then
+		szNames = arg0
+	elseif X.IsNumber(arg0) then
+		if X.IsNil(arg1) then
+			dwID = arg0
+		elseif X.IsString(arg1) then
+			dwType, szNames = arg0, arg1
+		elseif X.IsNumber(arg1) then
+			dwType, dwID = arg0, arg1
+		end
+	end
+	if not dwID and not szNames then
+		return
+	end
+	if dwID and not dwType then
+		dwType = X.IsPlayer(dwID) and TARGET.PLAYER or TARGET.NPC
+	end
+	if szNames then
+		local tTarget = {}
+		for _, szName in pairs(X.SplitString(szNames:gsub('[%[%]]', ''), '|')) do
+			tTarget[szName] = true
+		end
+		if not dwID and (not dwType or dwType == TARGET.NPC) then
+			for _, p in ipairs(X.GetNearNpc()) do
+				if tTarget[p.szName] then
+					dwType, dwID = TARGET.NPC, p.dwID
+					break
+				end
+			end
+		end
+		if not dwID and (not dwType or dwType == TARGET.PLAYER) then
+			for _, p in ipairs(X.GetNearPlayer()) do
+				if tTarget[p.szName] then
+					dwType, dwID = TARGET.PLAYER, p.dwID
+					break
+				end
+			end
+		end
+	end
+	if not dwType or not dwID then
+		return false
+	end
+	if dwType == TARGET.PLAYER then
+		if X.IsInShieldedMap() and not X.IsParty(dwID) and X.IsRestricted('X.SET_TARGET') then
+			--[[#DEBUG BEGIN]]
+			X.OutputDebugMessage('SetTarget', 'Set target to player is forbiden in current map.', X.DEBUG_LEVEL.WARNING)
+			--[[#DEBUG END]]
+			return false
+		end
+	elseif dwType == TARGET.NPC then
+		local npc = X.GetNpc(dwID)
+		if npc and not npc.IsSelectable() and X.IsRestricted('X.SET_TARGET') then
+			--[[#DEBUG BEGIN]]
+			X.OutputDebugMessage('SetTarget', 'Set target to unselectable npc.', X.DEBUG_LEVEL.WARNING)
+			--[[#DEBUG END]]
+			return false
+		end
+	elseif dwType == TARGET.DOODAD then
+		if X.IsRestricted('X.SET_TARGET') then
+			--[[#DEBUG BEGIN]]
+			X.OutputDebugMessage('SetTarget', 'Set target to doodad.', X.DEBUG_LEVEL.WARNING)
+			--[[#DEBUG END]]
+			return false
+		end
+	end
+	SetTarget(dwType, dwID)
+	return true
+end
+
+do
+local CALLBACK_LIST
+-- 获取到当前角色并执行函数
+-- @param {function} callback 回调函数
+function X.WithClientPlayer(callback)
+	local me = X.GetClientPlayer()
+	if me then
+		X.SafeCall(callback, me)
+	elseif CALLBACK_LIST then
+		table.insert(CALLBACK_LIST, callback)
+	else
+		CALLBACK_LIST = {callback}
+		X.BreatheCall(X.NSFormatString('{$NS}.WithClientPlayer'), function()
+			local me = X.GetClientPlayer()
+			if me then
+				for _, callback in ipairs(CALLBACK_LIST) do
+					X.SafeCall(callback, me)
+				end
+				CALLBACK_LIST = nil
+				X.BreatheCall(X.NSFormatString('{$NS}.WithClientPlayer'), false)
+			end
+		end)
+	end
+end
+end
+
 -- 获取指定名字的右键菜单
 function X.InsertPlayerContextMenu(t, szName, dwID, szGlobalID)
 	-- 数据库获取 dwID, szGlobalID 补全信息
@@ -155,112 +261,6 @@ function X.InsertPlayerContextMenu(t, szName, dwID, szGlobalID)
 	end
 
 	return t
-end
-
-X.RegisterRestriction('X.SET_TARGET', { ['*'] = true, intl = false })
-
--- 根据 dwType 类型和 dwID 设置目标
--- (void) X.SetTarget([number dwType, ]number dwID)
--- (void) X.SetTarget([number dwType, ]string szName)
--- dwType   -- *可选* 目标类型
--- dwID     -- 目标 ID
-function X.SetTarget(arg0, arg1)
-	local dwType, dwID, szNames
-	if X.IsUserdata(arg0) then
-		dwType, dwID = TARGET[X.GetObjectType(arg0)], arg0.dwID
-	elseif X.IsString(arg0) then
-		szNames = arg0
-	elseif X.IsNumber(arg0) then
-		if X.IsNil(arg1) then
-			dwID = arg0
-		elseif X.IsString(arg1) then
-			dwType, szNames = arg0, arg1
-		elseif X.IsNumber(arg1) then
-			dwType, dwID = arg0, arg1
-		end
-	end
-	if not dwID and not szNames then
-		return
-	end
-	if dwID and not dwType then
-		dwType = X.IsPlayer(dwID) and TARGET.PLAYER or TARGET.NPC
-	end
-	if szNames then
-		local tTarget = {}
-		for _, szName in pairs(X.SplitString(szNames:gsub('[%[%]]', ''), '|')) do
-			tTarget[szName] = true
-		end
-		if not dwID and (not dwType or dwType == TARGET.NPC) then
-			for _, p in ipairs(X.GetNearNpc()) do
-				if tTarget[p.szName] then
-					dwType, dwID = TARGET.NPC, p.dwID
-					break
-				end
-			end
-		end
-		if not dwID and (not dwType or dwType == TARGET.PLAYER) then
-			for _, p in ipairs(X.GetNearPlayer()) do
-				if tTarget[p.szName] then
-					dwType, dwID = TARGET.PLAYER, p.dwID
-					break
-				end
-			end
-		end
-	end
-	if not dwType or not dwID then
-		return false
-	end
-	if dwType == TARGET.PLAYER then
-		if X.IsInShieldedMap() and not X.IsParty(dwID) and X.IsRestricted('X.SET_TARGET') then
-			--[[#DEBUG BEGIN]]
-			X.OutputDebugMessage('SetTarget', 'Set target to player is forbiden in current map.', X.DEBUG_LEVEL.WARNING)
-			--[[#DEBUG END]]
-			return false
-		end
-	elseif dwType == TARGET.NPC then
-		local npc = X.GetNpc(dwID)
-		if npc and not npc.IsSelectable() and X.IsRestricted('X.SET_TARGET') then
-			--[[#DEBUG BEGIN]]
-			X.OutputDebugMessage('SetTarget', 'Set target to unselectable npc.', X.DEBUG_LEVEL.WARNING)
-			--[[#DEBUG END]]
-			return false
-		end
-	elseif dwType == TARGET.DOODAD then
-		if X.IsRestricted('X.SET_TARGET') then
-			--[[#DEBUG BEGIN]]
-			X.OutputDebugMessage('SetTarget', 'Set target to doodad.', X.DEBUG_LEVEL.WARNING)
-			--[[#DEBUG END]]
-			return false
-		end
-	end
-	SetTarget(dwType, dwID)
-	return true
-end
-
-do
-local CALLBACK_LIST
--- 获取到当前角色并执行函数
--- @param {function} callback 回调函数
-function X.WithClientPlayer(callback)
-	local me = X.GetClientPlayer()
-	if me then
-		X.SafeCall(callback, me)
-	elseif CALLBACK_LIST then
-		table.insert(CALLBACK_LIST, callback)
-	else
-		CALLBACK_LIST = {callback}
-		X.BreatheCall(X.NSFormatString('{$NS}.WithClientPlayer'), function()
-			local me = X.GetClientPlayer()
-			if me then
-				for _, callback in ipairs(CALLBACK_LIST) do
-					X.SafeCall(callback, me)
-				end
-				CALLBACK_LIST = nil
-				X.BreatheCall(X.NSFormatString('{$NS}.WithClientPlayer'), false)
-			end
-		end)
-	end
-end
 end
 
 --[[#DEBUG BEGIN]]X.ReportModuleLoading(MODULE_PATH, 'FINISH')--[[#DEBUG END]]
