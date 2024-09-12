e607c25b9f68a15a860fc914606fbaccac419d6c
Emil Zhai <root@derzh.com>
2024-09-12 11:02:01 +0800

    fix: 修复背包助手设置项无法导入导出的问题

--- MY_BagEx/src/MY_BagEx_Bank.lua
+++ MY_BagEx/src/MY_BagEx_Bank.lua
@@ -35,7 +35,7 @@ local O = X.CreateUserSettingsModule(MODULE_NAME, _L['General'], {
 	},
 	tLock = {
 		ePathType = X.PATH_TYPE.ROLE,
-		bUserData = true,
+		bPersonal = true,
 		szLabel = _L['MY_BagEx'],
 		xSchema = X.Schema.Map(X.Schema.String, X.Schema.Boolean),
 		xDefaultValue = {},
