8160f14ae018d133d3eac1bc72c5b0ee71ebb792
Emil Zhai <root@derzh.com>
2024-10-12 17:24:26 +0800

    feat: 界面库创建面板支持传入关闭回调

--- Boilerplate_!Base/src/lib/UI.lua
+++ Boilerplate_!Base/src/lib/UI.lua
@@ -6440,6 +6440,9 @@ function X.UI.CreateFrame(szName, opt)
 		end
 		return false
 	end
+	if X.IsFunction(opt.close) then
+		X.UI(frm):Remove(opt.close)
+	end
 	-- init frame
 	if opt.esc then
 		X.RegisterEsc(
