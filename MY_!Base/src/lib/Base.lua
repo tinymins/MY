82f27529b6f17498ac84271328c7016a895021ea
Zohar Lee <leafvmaple@gmail.com>
2025-06-15 15:23:10 +0800

    release: 27.1.11

--- MY_!Base/src/lib/Base.lua
+++ MY_!Base/src/lib/Base.lua
@@ -51,8 +51,8 @@ local CODE_PAGE = {
 }
 
 local _NAME_SPACE_            = 'MY'
-local _BUILD_                 = '20250416'
-local _VERSION_               = '27.1.10'
+local _BUILD_                 = '20250615'
+local _VERSION_               = '27.1.11'
 local _MENU_COLOR_            = {255, 165, 79}
 local _INTERFACE_ROOT_        = 'Interface/'
 local _ADDON_ROOT_            = _INTERFACE_ROOT_ .. _NAME_SPACE_ .. '/'
