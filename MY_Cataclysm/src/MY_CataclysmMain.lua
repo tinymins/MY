201c88ea72193ba638388d7eadc89aa42b172d80
ZXXX1999 <1511137684@qq.com>
2026-01-09 21:12:13 +0800

    feat: 优化自定义名称缓存字段（重新干净提交）

--- MY_Cataclysm/src/MY_CataclysmMain.lua
+++ MY_Cataclysm/src/MY_CataclysmMain.lua
@@ -111,6 +111,7 @@ local function UpdateTeamMonData()
 							v.szDisplayName = X.RenderTemplateString(data.szDisplayName, nil, -1, false, false)
 						end
 						v.nIcon = data.nIcon
+						v.szDisplayName = v.szDisplayName and X.RenderTemplateString(v.szDisplayName, nil, -1, false, false)
 						table.insert(aBuff, v)
 					end
 				end
